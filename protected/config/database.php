ab3b6515344823f1248a1e1010c5c6748706faf1
osy <ymxyt7207@163.com>
2015-01-18 01:08:37 +0800

    db

--- protected/config/database.php
+++ protected/config/database.php
@@ -5,9 +5,9 @@ return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
 
-	'connectionString' => 'mysql:host=localhost;dbname=newdb',
+	'connectionString' => 'mysql:host=121.42.12.97;dbname=nb_wymenu',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '123456',
+	'password' => 'MYmenu123',
 	'charset' => 'utf8',
 );
--- protected/config/main.php
+++ protected/config/main.php
@@ -60,7 +60,7 @@ return array(
 		// uncomment the following to enable URLs in path-format
 		
 		'urlManager'=>array(
-                        //'showScriptName'=>false,
+            'showScriptName'=>false,
 			'urlFormat'=>'path',
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
