6a83363859ba6883f755fb67b0fd90d7e995ff53
yqs <yqs01221@163.com>
2015-02-11 21:24:19 +0800

    layout

--- protected/modules/admin/views/layouts/loginLayout.php
+++ protected/modules/admin/views/layouts/loginLayout.php
@@ -67,7 +67,6 @@
 	<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/plugins/select2/select2.min.js');?>
 	<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/scripts/app.js');?>
 	<?php Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/scripts/login-soft.js');?>
-        //branck
 	<script>
 		jQuery(document).ready(function() {     
 		  App.init();
