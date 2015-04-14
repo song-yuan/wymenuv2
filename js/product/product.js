3a87168d5a2568fa44502926bb1dc3862c9d97c8
dys <daiyongsheng.good@163.com>
2015-04-14 22:49:29 +0800

    dys0414

--- js/product/product.js
+++ js/product/product.js
@@ -101,6 +101,16 @@ $(document).ready(function(){
  	 				}
  	 				$('.flicking_con .flicking_inner').html(dotstr);
  	 				$('.main_image ul').html(str);
+ 	 				$(".main_image").touchSlider({
+ 	 					flexible : true,
+ 	 					speed : 200,
+ 	 					btn_prev : $("#btn_prev"),
+ 	 					btn_next : $("#btn_next"),
+ 	 					paging : $(".flicking_con a"),
+ 	 					counter : function (e) {
+ 	 						$(".flicking_con a").removeClass("on").eq(e.current-1).addClass("on");
+ 	 					}
+ 	 				});
  				}
  			},
  		});
@@ -115,16 +125,6 @@ $(document).ready(function(){
 		$("#btn_prev,#btn_next").fadeOut()
 		})
 	$dragBln = false;
-	$(".main_image").touchSlider({
-		flexible : true,
-		speed : 200,
-		btn_prev : $("#btn_prev"),
-		btn_next : $("#btn_next"),
-		paging : $(".flicking_con a"),
-		counter : function (e) {
-			$(".flicking_con a").removeClass("on").eq(e.current-1).addClass("on");
-		}
-	});
 	$(".main_image").bind("mousedown", function() {
 		$dragBln = false;
 	})
