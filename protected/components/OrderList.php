0f53c6ece237436e9e57bd374c79ccacf7315f8f
dys <daiyongsheng.good@163.com>
2015-05-04 22:28:51 +0800

    dys0504

--- protected/components/OrderList.php
+++ protected/components/OrderList.php
@@ -83,7 +83,7 @@ class OrderList
 	//获取该套餐的产品 array(1=>array(,),2=>array(,)) 1,2表示group_no
 	public function GetSetProduct($setId){
 		$result = array();
-		$sql = 'select t.*, t1.product_name, t1.main_picture from nb_product_set_detail t left join nb_product t1 on t.product_id=t1.lid where t.set_id=:setId and t.dpid=:dpid and t1.dpid=:dpid and t.delete_flag=0 order by is_select desc';
+		$sql = 'select t.*, t1.product_name, t1.main_picture from nb_product_set_detail t left join nb_product t1 on t.product_id=t1.lid where t.set_id=:setId and t.dpid=t1.dpid and t.dpid=:dpid and t.delete_flag=0 order by is_select desc';
 		$conn = $this->db->createCommand($sql);
 		$conn->bindValue(':setId',$setId);
 		$conn->bindValue(':dpid',$this->siteNo['dpid']);
