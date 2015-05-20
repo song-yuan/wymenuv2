5652ca65ad0ff82022b77d9ed8cb6e87b3ba8bb0
dys <daiyongsheng.good@163.com>
2015-05-20 17:34:03 +0800

    dys0520

--- protected/controllers/ProductController.php
+++ protected/controllers/ProductController.php
@@ -192,7 +192,7 @@ class ProductController extends Controller
 	public function actionConfirmPadOrder(){
 		$goodsIds = isset($_POST) ?$_POST :array();
 		$padOrder = json_encode(array('status'=>false,'msg'=>'订单为空'));
-	 	if(!empty($goodIds)){
+	 	if(!empty($goodsIds)){
 	 		var_dump($this->companyId);
 	 		var_dump($goodsIds);
 	 		var_dump($this->padId);exit;
