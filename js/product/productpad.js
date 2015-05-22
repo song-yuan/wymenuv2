c4459f4439059f8609a2399c87b939f721ee94e2
dys <daiyongsheng.good@163.com>
2015-05-22 17:52:48 +0800

    dys0522

--- js/product/productpad.js
+++ js/product/productpad.js
@@ -210,7 +210,7 @@ $(document).ready(function(){
                 }
  			},
  		});
- 		//alert($("#gallery").outerHeight());
+ 		alert($("#gallery").outerHeight());
 		var top = 0;
 		if( ($('.large-pic').height() - $("#gallery").outerHeight()) > 0){
 			top = ($('.large-pic').height() - $("#gallery").outerHeight())/2 ;
