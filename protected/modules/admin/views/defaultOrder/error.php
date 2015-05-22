f8a6bafb0054ccca6390c7eafd28b28abd32ba2e
osy <ymxyt7207@163.com>
2015-05-22 11:12:39 +0800

    osy234

--- protected/modules/admin/views/defaultOrder/error.php
+++ protected/modules/admin/views/defaultOrder/error.php
@@ -1,20 +1,29 @@
+
 <div class="page-content">
-	<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->               
 	
 	<!-- /.modal -->
 	<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
-	<div class="col-md-12">
+	<!-- BEGIN PAGE HEADER-->
+	
+	<!-- END PAGE HEADER-->
+	<!-- BEGIN PAGE CONTENT-->
+	<div class="row">
+            		<div class="col-md-12">
 			<!-- BEGIN EXAMPLE TABLE PORTLET-->
-		<div class="col-md-12">
-			<!-- BEGIN PAGE TITLE & BREADCRUMB-->			
-			<h3 class="page-title">
-				<small>'<?php echo $title;?>'</small>
-			</h3>
-                        <a href="<?php echo $backurl;?>" class="btn-over btn green msg_sure_btn" ><i class="fa fa-check"></i>确认</a>
-			<!-- END PAGE TITLE & BREADCRUMB-->
+			<div class="portlet box">
+				<div class="portlet-title">
+					<div class="caption"><i class="fa fa-globe"></i>错误提示</div>
+					<h3 class="page-title">
+                                                <small>'<?php echo $title;?>'</small>
+                                        </h3>
+                                        <a href="<?php echo $backurl;?>" class="btn-over btn green msg_sure_btn" ><i class="fa fa-check"></i>确认</a>
+			
+				</div>
+				
+			</div>
+			<!-- END EXAMPLE TABLE PORTLET-->
 		</div>
-			<!-- END EXAMPLE TABLE PORTLET-->		
+            
 	</div>
-</div>
 	<!-- END PAGE CONTENT-->
 	
