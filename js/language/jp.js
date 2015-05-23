4d1238cc6e6e2d03fb5fc1ac82563bd4ac4eaec5
osy <ymxyt7207@163.com>
2015-05-23 11:57:17 +0800

    osy1233

---
+++ js/language/jp.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var language_diancai="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;注文&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
+var language_jiancai="減らす";
+var language_renqi="人気：";
+var language_duotuliulan="&nbsp;&nbsp;画面大&nbsp;&nbsp;";
+
