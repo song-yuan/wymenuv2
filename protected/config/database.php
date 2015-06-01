ae93dd725989dac7dec6c39198d2198b1603bccb
osy <ymxyt7207@163.com>
2015-06-01 13:18:35 +0800

    osy234

--- protected/config/database.php
+++ protected/config/database.php
@@ -4,7 +4,7 @@
 return array(
 	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-
+        //'connectionString' => 'mysql:host=52.68.233.6;dbname=nb_wymenu',
 	'connectionString' => 'mysql:host=121.42.12.97;dbname=nb_wymenu',
 	'emulatePrepare' => true,
 	'username' => 'root',
