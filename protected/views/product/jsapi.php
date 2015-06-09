f5e89504d8230818104d5c61c079007c02721a23
dys <daiyongsheng.good@163.com>
2015-06-09 16:43:32 +0800

    dys0609

--- protected/views/product/jsapi.php
+++ protected/views/product/jsapi.php
@@ -12,7 +12,7 @@ function printf_info($data)
 //①、获取用户openid
 $tools = new JsApiPay();
 $openId = $tools->GetOpenid();
-
+echo $openId;exit;
 //②、统一下单
 $input = new WxPayUnifiedOrder();
 $input->SetBody("test");
