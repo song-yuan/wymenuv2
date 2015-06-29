517f952f1d5a99b965a7e578a2f6f83aba4e7dd2
osy <ymxyt7207@163.com>
2015-06-29 15:28:44 +0800

    dsf

--- themes/pad/views/product/product.php
+++ themes/pad/views/product/product.php
@@ -150,8 +150,9 @@
        //{"company_id":"0000000001","do_id":"sell_off",//
        //num <0 无数量限制
        function sell_off(do_data) {
-            alert(do_data);
+            //alert(do_data);
             var data = eval('(' + do_data + ')');
+            alert(data[0]['product_id']);
             //
        }
        
