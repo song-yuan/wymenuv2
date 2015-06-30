5b18f17787efaa6e5bcda97e0f1883287e0f1df5
dys <daiyongsheng.good@163.com>
2015-06-30 15:29:30 +0800

    dys0630

--- themes/pad/views/product/product.php
+++ themes/pad/views/product/product.php
@@ -150,12 +150,9 @@
               //{"company_id":"0000000001","do_id":"sell_off",
 	       //num <0 无数量限制
       function sell_off(do_data) {
-           alert(do_data);
             var data = eval('(' + do_data + ')');
             	//for(var item in data.do_data){
             	for(var item in data){
-                    alert(data[item].product_id);
-                    alert(data[item].num);
                     $('div.blockCategory[product-id="'+data[item].product_id+'"]').attr('store',data[item].num);
             	}             
        }       
