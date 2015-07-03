3dbeae94d1b04363046d657d4a075c93c2991119
osy <ymxyt7207@163.com>
2015-07-03 15:20:54 +0800

    osf

--- protected/modules/admin/views/orderManagement/orderDaliyCollect.php
+++ protected/modules/admin/views/orderManagement/orderDaliyCollect.php
@@ -34,19 +34,19 @@
 			<div class="portlet box purple">
 				<div class="portlet-title">
 				
-				 <div class="caption"><i class="fa fa-globe"></i><?php echo yii::t('app','汇总列表'); ?></div>
+				 <div class="caption"><i class="fa fa-globe"></i><?php echo yii::t('app','汇总列表');?></div>
 					 <div class="actions">
                         <div class="btn-group">
             
 						   <div class="input-group input-large date-picker input-daterange" data-date="10/11/2012" data-date-format="mm/dd/yyyy">
-								<input type="text" class="form-control" name="begtime" id="begin_time" placeholder=<?php echo yii::t('app','起始时间'); ?> value="<?php echo $begin_time; ?>" onfocus=this.blur()>  
+								<input type="text" class="form-control" name="begtime" id="begin_time" placeholder="<?php echo yii::t('app','起始时间');?>" value="<?php echo $begin_time; ?>" onfocus=this.blur()>  
 								<span class="input-group-addon">~</span>
-							    <input type="text" class="form-control" name="endtime" id="end_time" placeholder=<?php echo yii::t('app','终止时间'); ?>  value="<?php echo $end_time;?>" onfocus=this.blur()>           
+							    <input type="text" class="form-control" name="endtime" id="end_time" placeholder="<?php echo yii::t('app','终止时间');?>"  value="<?php echo $end_time;?>" onfocus=this.blur()>           
 						   </div>  
 					    </div>
 					   
 					      <div class="btn-group">
-							    <button type="submit" id="btn_time_query" class="btn red" ><i class="fa fa-pencial"></i><?php echo yii::t('app','日 结'); ?></button>
+							    <button type="submit" id="btn_time_query" class="btn red" ><i class="fa fa-pencial"></i><?php echo yii::t('app','日 结');?></button>
 				  	      </div>
 				  	  </div>
 				</div>
@@ -56,16 +56,16 @@
 						<thead>
 							<tr>
 								
-								<th width=100px;><?php echo yii::t('app','序号'); ?></th>
-						        <th><?php echo yii::t('app','店铺'); ?></th>
-                                <th><?php echo yii::t('app','支付方式'); ?></th>
-                                <th><?php echo yii::t('app','金额'); ?></th>                                                                
-                                <th><?php echo yii::t('app','备注'); ?></th>
+								<th width=100px;><?php echo yii::t('app','序号');?></th>
+						        <th><?php echo yii::t('app','店铺');?></th>
+                                <th><?php echo yii::t('app','支付方式');?></th>
+                                <th><?php echo yii::t('app','金额');?></th>                                                                
+                                <th><?php echo yii::t('app','备注');?></th>
 								
 							</tr>
 						</thead>
 						<tbody>
-						
+				 
 						<!--foreach-->
 					<?php $a=1;?>
 						<!--  <?php foreach ($models as $model):?>   -->
@@ -73,24 +73,30 @@
 						
 						
 								<tr class="odd gradeX">
-								<td><?php echo $a;?></td>
-								<td><?php echo $model['company_name']; ?></td>
-								<td><?php if($model['payment_method_id'] == '0000000000') echo yii::t('app','现金支付'); else echo $model['name'];?></td>
-								<td><?php echo $model['should_all'];?></td>
+								<td><?php echo ($pages->getCurrentPage())*10+$a;?></td>
+								<td><?php echo $model->company->company_name; ?></td>
+								<td><?php if($model->payment_method_id!='0000000000') echo $model->paymentMethod->name.yii::t('app','(后台)'); else switch($model->paytype) {case 0: echo  yii::t('app','现金支付');break; case 1: echo  yii::t('app','微信支付');break; case 2: echo  yii::t('app','支付宝支付');break; case 3: echo  yii::t('app','后台手动支付');break;  default :echo ''; }?></td>								<td><?php echo $model->should_all;?></td>
 								<td></td>
 								</tr>
-						<?php $a=$a+1;?>
+						<?php $a++;?>
 						<!-- <?php endforeach;?>	  -->
 						<!-- end foreach-->
 						
 						</tbody>
 					</table>
+					<div class="form-actions fluid">
+					   <div class="col-md-offset-3 col-md-9">
+						  <!-- <button type="submit" class="btn blue">确定</button>    -->
+						  <a href="<?php echo $this->createUrl('orderManagement/notPay' , array('companyId' => $this->companyId));?>/begin_time/<?php echo $begin_time;?>/end_time/<?php echo $end_time;?>/page/" class="btn default"><?php echo yii::t('app','返回');?></a>                              
+					   </div>
+				    </div>
 						<?php if($pages->getItemCount()):?>
 						<div class="row">
 							<div class="col-md-5 col-sm-12">
 								<div class="dataTables_info">
-									<?php echo yii::t('app','共'); ?> <?php echo $pages->getPageCount();?> <?php echo yii::t('app','页'); ?>  , <?php echo $pages->getItemCount();?> <?php echo yii::t('app','条数据'); ?> , <?php echo yii::t('app','当前是第'); ?> <?php echo $pages->getCurrentPage()+1;?> <?php echo yii::t('app','页'); ?>
+									<?php echo yii::t('app','共 ');?> <?php echo $pages->getPageCount();?> <?php echo yii::t('app','页');?>  , <?php echo $pages->getItemCount();?> <?php echo yii::t('app','条数据');?> ,  <?php echo yii::t('app','当前是第');?> <?php echo $pages->getCurrentPage()+1;?> <?php echo yii::t('app','页');?>
 								</div>
+							</div>
 							</div>
 							<div class="col-md-7 col-sm-12">
 								<div class="dataTables_paginate paging_bootstrap">
