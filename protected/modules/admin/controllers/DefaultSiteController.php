a8dec8939a7fb2e0f47c6846192d86097ebc9c10
osy <ymxyt7207@163.com>
2015-07-31 13:18:10 +0800

    sdf

--- protected/modules/admin/controllers/DefaultSiteController.php
+++ protected/modules/admin/controllers/DefaultSiteController.php
@@ -64,7 +64,12 @@ class DefaultSiteController extends BackendController
                 $status = Yii::app()->request->getParam('status','0');
                 $istemp = Yii::app()->request->getParam('istemp','0');
                 $typeId = Yii::app()->request->getParam('typeId','0');
-              
+                $criteria2 = new CDbCriteria;
+                $criteria2->condition =  't.status in ("1","2","3") and t.dpid='.$this->companyId.' and t.site_id='.$sid.' and t.is_temp='.$istemp ;
+                $criteria2->order = ' t.lid desc ';
+                $siteNo = SiteNo::model()->find($criteria2);
+                $status=$siteNo->status;
+//                var_dump($siteNo);exit;
 		$model=array();
 		$this->renderPartial('button' , array(
 				'model' => $model,
