bd951770b44fd0f2b4373e9ff47c1e0734c3edde
dys <daiyongsheng.good@163.com>
2015-08-11 18:06:32 +0800

    dys

---
+++ protected/modules/admin/views/member/_form.php
@@ -0,0 +1,23 @@
+							<?php $form=$this->beginWidget('CActiveForm', array(
+									'id' => 'taste-form',
+									'errorMessageCssClass' => 'help-block',
+									'htmlOptions' => array(
+										'class' => 'form-horizontal',
+										'enctype' => 'multipart/form-data'
+									),
+							)); ?>
+								<div class="form-body">
+									<div class="form-group">
+										<?php echo $form->label($model, 'name',array('class' => 'col-md-3 control-label'));?>
+										<div class="col-md-4">
+											<?php echo $form->textField($model, 'name',array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('name')));?>
+											<?php echo $form->error($model, 'name' )?>
+										</div>
+									</div>
+									<div class="form-actions fluid">
+										<div class="col-md-offset-3 col-md-9">
+											<button type="submit" class="btn blue"><?php echo yii::t('app','确定');?></button>
+											<a href="<?php echo $this->createUrl('taste/index' , array('companyId' => $model->dpid,'type'=>$type));?>" class="btn default"><?php echo yii::t('app','返回');?></a>                              
+										</div>
+									</div>
+							<?php $this->endWidget(); ?>
