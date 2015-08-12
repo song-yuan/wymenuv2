5ede438d09ddddef639e39453c7a4510bb1201d3
dys <daiyongsheng.good@163.com>
2015-08-12 11:15:54 +0800

    dys

--- protected/modules/admin/views/layouts/sidebar.php
+++ protected/modules/admin/views/layouts/sidebar.php
@@ -131,7 +131,7 @@
 					<span class="title"><?php echo yii::t('app','会员管理');?></span>					
 					</a>
 					<ul class="sub-menu">
-						<li class="<?php if(Yii::app()->controller->id == 'member' && in_array($this->getAction()->getId(),'index','create','update')) echo 'active';?>"><a href="<?php echo $this->createUrl('member/index',array('companyId' => $this->companyId));?>"><?php echo yii::t('app','会员列表');?></a></li>
+						<li class="<?php if(Yii::app()->controller->id == 'member' && in_array($this->getAction()->getId(),array('index','create','update'))) echo 'active';?>"><a href="<?php echo $this->createUrl('member/index',array('companyId' => $this->companyId));?>"><?php echo yii::t('app','会员列表');?></a></li>
 					</ul>
 				</li>
 				<!--
