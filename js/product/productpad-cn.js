9883a652ff1476c758c252c42ee819e850cc0951
dys <daiyongsheng.good@163.com>
2015-08-16 10:47:40 +0800

    dys

--- js/product/productpad-cn.js
+++ js/product/productpad-cn.js
@@ -377,8 +377,8 @@ $(document).ready(function(){
                 //alert(msg);
 	                $('.large-pic').css('display','block');
 	                $('.large-pic').html(msg);
-	                var left = ($('.large-pic').width() - $("#gallery").outerWidth())/2;
-	                var top = ($('.large-pic').height() - $("#gallery").outerHeight())/2;
+	                var left = ($(window).width() - $("#gallery").outerWidth())/2;
+	                var top = ($(window).height() - $("#gallery").outerHeight())/2;
 	                if(left < 0){
 	                	left = 0;
 	                }
