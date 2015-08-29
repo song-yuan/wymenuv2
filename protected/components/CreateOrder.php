6e6780653b9c46c4671322fe44613dfca15acf97
dys <daiyongsheng.good@163.com>
2015-08-29 19:17:48 +0800

    dys

--- protected/components/CreateOrder.php
+++ protected/components/CreateOrder.php
@@ -494,9 +494,7 @@ class CreateOrder
 			$db->createCommand($sql)->execute();
 			
 			//厨打
-			$order = new Order;
-            $siteNo = new SiteNo;
-            $site = new Site;
+			
             if($orderId !='0')
             {
                 $order = Order::model()->with('company')->find(' t.lid=:lid and t.dpid=:dpid and t.order_status in(1,2,3)' , array(':lid'=>$orderId,':dpid'=>$dpid));
