ff450f7791647e309c58905d9af19b860f196add
dys <daiyongsheng.good@163.com>
2015-10-13 10:48:52 +0800

    dys

--- js/product/productpad-cn.js
+++ js/product/productpad-cn.js
@@ -30,28 +30,7 @@ function addToCart() {
 }
 $(document).ready(function(){
 	var language = $('input[name="language"]').val();
-	//更换套餐明细
-    $('#forum_list').on('click','.productsetpad .item',function(){
-   	var blockCategory = $(this).parents('.blockCategory');
-	   	var productId = blockCategory.find('a.product-pic').attr('lid');//套餐 ID
-	   	
-	   	var productsetDetailLid = $(this).attr('productset-detail-id');
-   	var productsetGroup = $(this).parents('.productset-group');
-   	var groupNo = productsetGroup.attr('group-no');
-   	
-   	if(!$(this).hasClass('active')){
-   		var str = '<input type="hidden" name="'+productId+'['+groupNo+']['+productsetDetailLid+']'+'" value="1"/>';
-   		$('#padOrderForm').append(str);
-   		$(this).addClass('active');
-   		$(this).siblings().each(function(){
-   			if($(this).hasClass('active')){
-		    		var detailId = $(this).attr('productset-detail-id');
-		    		$('input[name="'+productId+'['+groupNo+']['+detailId+']'+'"]').remove();
-		    		$(this).removeClass('active');
-		    	}
-   		});
-   	}
-   });
+	
     $('#forum_list').on(event_clicktouchstart,'.addCart',function(){
         var istemp=$('#id_client_is_temp').val();
         if(istemp=="1")
@@ -723,6 +702,28 @@ $(document).ready(function(){
                     }
                  }	
      });
+     //更换套餐明细
+	    $('#forum_list').on('click','.productsetpad .item',function(){
+		   	var blockCategory = $(this).parents('.blockCategory');
+			   	var productId = blockCategory.find('a.product-pic').attr('lid');//套餐 ID
+			   	
+			   	var productsetDetailLid = $(this).attr('productset-detail-id');
+		   	var productsetGroup = $(this).parents('.productset-group');
+		   	var groupNo = productsetGroup.attr('group-no');
+		   	
+		   	if(!$(this).hasClass('active')){
+		   		var str = '<input type="hidden" name="'+productId+'['+groupNo+']['+productsetDetailLid+']'+'" value="1"/>';
+		   		$('#padOrderForm').append(str);
+		   		$(this).addClass('active');
+		   		$(this).siblings().each(function(){
+		   			if($(this).hasClass('active')){
+				    		var detailId = $(this).attr('productset-detail-id');
+				    		$('input[name="'+productId+'['+groupNo+']['+detailId+']'+'"]').remove();
+				    		$(this).removeClass('active');
+				    	}
+		   		});
+		   	}
+	   });
      //套餐取消
       $('body').on(event_clicktouchstart,'.productset-cancel',function(){
 	      	var _this = $(this);
