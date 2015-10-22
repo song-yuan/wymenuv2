92b80032ab5ac3f17a5833c5395cce3c4640e289
dys <daiyongsheng.good@163.com>
2015-10-22 15:21:50 +0800

    dys

--- protected/components/ProductClass.php
+++ protected/components/ProductClass.php
@@ -310,7 +310,7 @@ class ProductClass
 		return CHtml::listData($categories, 'lid', 'category_name');
 	}
     public static function getSaleOffProducts($companyId){
-         $sql = 'select lid,dpid,store_number from nb_product where dpid=:dpid and is_show=1 and delete_flag=0';
+         $sql = 'select lid,dpid,store_number from nb_product where dpid=:dpid and store_number >=0 and is_show=1 and delete_flag=0';
          $result = Yii::app()->db->createCommand($sql)->bindValue(':dpid',$companyId)->queryAll();
          return $result;
     }
