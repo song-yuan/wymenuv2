8f8848f107e53d0063099c52a92d2b4dc6a711c0
caofang <962460422@qq.com>
2015-11-10 09:21:28 +0800

    cf

--- protected/modules/admin/views/orderManagement/orderDaliyCollect.php
+++ protected/modules/admin/views/orderManagement/orderDaliyCollect.php
@@ -81,9 +81,9 @@
                                                                 <td><?php switch($model->paytype) {case 0: echo  yii::t('app','现金支付');break; 
                                                                 case 1: echo  yii::t('app','微信支付');break; 
                                                                 case 2: echo  yii::t('app','支付宝支付');break; 
-                                                                case 3: if ($model->payment_method_id){echo  $model->paymentMethod->name;}else echo '';break;  
                                                                 case 4: echo  yii::t('app','会员卡支付');break;  
-                                                                case 5: echo  yii::t('app','银联卡支付');break;  
+                                                                case 5: echo  yii::t('app','银联卡支付');break; 
+                                                                case 3: if ($model->payment_method_id){echo  $model->paymentMethod->name;}else echo '';break;
                                                                 default :echo ''; }?></td>
 								<td><?php echo $model->should_all;?></td>
 								<td></td>
@@ -103,7 +103,7 @@
                                                     <tr class="odd gradeX">
                                                     <td></td>
                                                     <td>合计：</td>
-                                                    <td><?php echo $sumall;?></td>
+                                                    <td><?php echo $sumall+$money;?></td>
                                                     <td></td>
                                                     </tr>
 						</tbody>
