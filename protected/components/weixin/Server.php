cda7bf70a4ad43696e9f3dfdfac8045272a04d79
dys <daiyongsheng.good@163.com>
2015-11-16 21:17:17 +0800

    dys

--- protected/components/weixin/Server.php
+++ protected/components/weixin/Server.php
@@ -221,9 +221,9 @@ class Server {
 			$time = time();
 			$se = new Sequence("scene_scan_log");
             $lid = $se->nextval();
-            $this->text('欢迎关注我要点单官方微信！'.$this->userId.'dd'.$lid);
 			$sql = 'INSERT INTO nb_scene_scan_log(lid, dpid, create_at, update_at, scene_id, user_id) VALUES(' . $lid . ','.$this->brandId.', "'.date('Y-m-d H:i:s',$time).'","'.date('Y-m-d H:i:s',$time).'",'.$this->sceneId.' , '.$this->userId.')';
 			Yii::app()->db->createCommand($sql)->execute();
+			 $this->text('欢迎关注我要点单官方微信！'.$this->userId.'dd'.$lid);
 		}
 	}
     
