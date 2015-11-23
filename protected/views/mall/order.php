eb024dd8c38d532d76fc454312fbca41755e3188
dys <daiyongsheng.good@163.com>
2015-11-23 14:42:31 +0800

    dys

--- protected/views/mall/order.php
+++ protected/views/mall/order.php
@@ -7,7 +7,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/order.css">
 <script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/Adaptive.js"></script>
 <script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/jquery-1.9.1.min.js"></script>
-
+<div class="order-title">订单详情</div>
 <div class="order-info">
 	<?php foreach($orderProducts as $product):?>
 	<div class="item">
