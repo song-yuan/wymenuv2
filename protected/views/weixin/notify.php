e8b4d988bfda60548cf282ea39a96664a951d8b0
dys <daiyongsheng.good@163.com>
2015-11-25 15:37:06 +0800

    dys

--- protected/views/weixin/notify.php
+++ protected/views/weixin/notify.php
@@ -1,5 +1,10 @@
 <?php
 
+$myfile = fopen("notify.txt", "w") or die("Unable to open file!");
+fwrite($myfile, 'aaa');
+fwrite($myfile, 'bbbbbbb');
+fwrite($myfile, 'cccc');
+fclose($myfile);
 $notify = new Notify();
 $notify->Handle(false);
 
