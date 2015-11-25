f1bfd0c0395e0bc55b303201db213f265a4a715c
dys <daiyongsheng.good@163.com>
2015-11-25 14:35:51 +0800

    dys

--- protected/controllers/MallController.php
+++ protected/controllers/MallController.php
@@ -26,6 +26,7 @@ class MallController extends Controller
 					$newBrandUser = new NewBrandUser($openid, $this->companyId);
 		    		$this->brandUser = $newBrandUser->brandUser;
 				}
+				var_dump($this->brandUser);exit;
 				$userId = $this->brandUser['lid'];
 				Yii::app()->session['userId'] = $userId;
 				Yii::app()->session['qrcode-'.$userId] = 0000000000;
