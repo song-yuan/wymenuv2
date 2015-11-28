70762245ff24d2dc5d7ba51352be468b5de85ba7
dys <daiyongsheng.good@163.com>
2015-11-28 09:34:13 +0800

    dys

--- protected/controllers/MallController.php
+++ protected/controllers/MallController.php
@@ -39,6 +39,10 @@ class MallController extends Controller
 				Yii::app()->session['userId'] = $userId;
 				if($this->type==1){
 					//堂吃
+					$scaned = WxScanLog::get($this->companyId,$userId);
+					if(!empty($scaned)){
+						Yii::app()->session['qrcode-'.$userId] = $scaned['scene_id'];
+					}
 					if(!isset(Yii::app()->session['qrcode-'.$userId])){
 						Yii::app()->session['qrcode-'.$userId] = -1;//通过扫描二维码 添加session
 					}
