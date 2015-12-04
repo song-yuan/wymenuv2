15cd0c235380661603cb78bad31df651b7db7755
dys <daiyongsheng.good@163.com>
2015-12-04 11:08:33 +0800

    dys

--- protected/components/mall/WxPromotion.php
+++ protected/components/mall/WxPromotion.php
@@ -30,6 +30,7 @@ class WxPromotion
 			$sql = 'select m.*,n.num from (select t.product_id,t.is_set,t.is_discount,t.promotion_money,t.promotion_discount,t.order_num,t1.product_name,t1.main_picture,t1.original_price from nb_private_promotion_detail t,nb_product t1 where t.product_id=t1.lid and t.dpid=t1.dpid and t.dpid=:dpid and t.private_promotion_id=:promotionId and t.delete_flag=0)m left join nb_cart n on m.product_id=n.product_id and n.user_id=:userId and privation_promotion_id=:promotionId';
 			$products = Yii::app()->db->createCommand($sql)->bindValue(':dpid',$this->dpid)->bindValue(':userId',$this->user['lid'])->bindValue(':promotionId',$result['private_promotion_id'])->queryAll();
 			foreach($products as $j=>$product){
+//				$products[$j]['price'] = self::getPromotionPrice($this->dpid,$this->userId,$product['product_id'],$result['private_promotion_id'],$result['to_group']);
 				if($product['is_discount']==0){
 					$products[$j]['price'] = ($product['original_price'] - $product['promotion_money']) > 0 ? $product['original_price'] - $product['promotion_money'] : 0;
 				}else{
