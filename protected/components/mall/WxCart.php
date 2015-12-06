f5635c2ad1275a9ce806b2ed4de6d5f2f51cc023
dys <daiyongsheng.good@163.com>
2015-12-06 21:08:08 +0800

    dys

--- protected/components/mall/WxCart.php
+++ protected/components/mall/WxCart.php
@@ -61,6 +61,9 @@ class WxCart
 					}
 				}
 			}
+			if(($resulta['count'] >= $result['order_num'])){
+				return array('status'=>false,'msg'=>'超过活动商品数量!');
+			}
 			if(($result['order_num']!=0 && $resulta['count'] >= $result['order_num']) || $result['product_num']!=0 && (isset($this->cart['num'])?$this->cart['num']:0) >= $result['product_num']){
 				return array('status'=>false,'msg'=>'超过活动商品数量!');
 			}
