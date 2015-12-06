3c3d6a110788bb48613960f64d641e18ea2e9f35
dys <daiyongsheng.good@163.com>
2015-12-06 20:59:39 +0800

    dys

--- protected/views/mall/order.php
+++ protected/views/mall/order.php
@@ -33,7 +33,7 @@
 	</div>
 	<?php if($order['reality_total'] - $order['should_total']):?>
 	<div class="item">
-		<div class="lt">优惠金额:</div><div class="rt">￥<?php echo $order['reality_total'] - $order['should_total'];?></div>
+		<div class="lt">优惠金额:</div><div class="rt">￥<?php echo number_format($order['reality_total'] - $order['should_total'],2);?></div>
 		<div class="clear"></div>
 	</div>
 	<?php endif;?>
