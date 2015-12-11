fe35d6ab314f6228a3a2b9920cd35fb7976b7cc4
dys <daiyongsheng.good@163.com>
2015-12-11 16:00:07 +0800

    dys

--- protected/views/mall/getwxcard.php
+++ protected/views/mall/getwxcard.php
@@ -1,15 +1,15 @@
-<?php $this->beginContent('//layouts/brandlayouts.php'); ?>
 <style>
 	.card{padding:10px;height:150px;}
 	.card .inner{width:100%;height:100%;position:relative;}
 	.card .inner .inner-top{height:100px;width:100%;}
 	.card .inner .inner-bottom{height:50px;width:100%;position:absolute;left:0;bottom:0;color:white;background-color:rgba(255,255,255,0.25);}
-	.inner-top .left{width:60px;height:60px;margin:20px;display:inline}
+	.inner-top .left{width:60px;height:60px;margin:20px;float:left;}
 	.inner-top .left img {border-radius:100%;width:60px;height:60px;}
-	.inner-top .right{font-size:30px;text-align:center;height:70px;width:-moz-calc(100% - 100px);width:-webkit-calc(100% - 100px);width:calc(100% - 100px);margin-top:30px;display:inline;color:white;}
+	.inner-top .right{font-size:30px;text-align:left;height:70px;width:-moz-calc(100% - 100px);width:-webkit-calc(100% - 100px);width:calc(100% - 100px);margin-top:30px;float:left;color:white;}
 	.inner-bottom .info{float:left;line-height:50px;}
 	.l{width:-moz-calc(45% - 5px);width:-webkit-calc(45% - 5px);width:calc(45% - 5px);height:50px;text-align:left;padding-left:5px;}
 	.r{width:-moz-calc(55% - 5px);width:-webkit-calc(55% - 5px);width:calc(55% - 5px);height:50px;text-align:right;padding-right:5px;}
+	.clear{clear:both;}
 </style>
 <?php 
 	$this->setPageTitle('卡券列表');
@@ -33,7 +33,7 @@
 			?>
 			<div class="card" id="batchAddCard<?php echo $key;?>">
 				<div class="inner" style="background-color:<?php echo $wxCard['color'];?>">
-					<div class="inner-top"><div class="left"><img src="<?php echo $wxCard['logo'];?>" /></div><div class="right"><?php echo $wxCard['title'];?></div></div>
+					<div class="inner-top"><div class="left"><img src="<?php echo $wxCard['logo'];?>" /></div><div class="right"><?php echo $wxCard['title'];?></div><div class="clear"></div></div>
 					<div class="inner-bottom"><div class="info l"><?php echo $wxCard['brand_name'];?></div><div class="info r"><?php if($wxCard['date_info_type']==1){ echo '有效期至:'.date('Y-m-d',$wxCard['end_timestamp']);}else{ echo '有效期至:'. date('Y-m-d',time() + ($wxCard['fixed_begin_term']+$wxCard['fixed_begin_term'])*24*3600);}?></div></div>
 				</div>
 			</div>
@@ -55,5 +55,3 @@
 		<?php }
 		}?>
 
-
-<?php $this->endContent(); ?>
