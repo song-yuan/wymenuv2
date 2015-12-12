8c9c2eee32d69094df8810b3cfeb1c92ca4f85e2
dys <daiyongsheng.good@163.com>
2015-12-12 15:50:56 +0800

    dys

--- protected/components/mall/WxPromotionActivity.php
+++ protected/components/mall/WxPromotionActivity.php
@@ -164,7 +164,7 @@ class WxPromotionActivity
 		if($type==1){
 			$sql = 'select * from nb_private_branduser where dpid='.$dpid.' and ((brand_user_lid='.$userId.' and to_group=3) or (to_group=2)) and private_promotion_id='.$promotionId;
 		}elseif($type==2){
-			$sql = 'select * from nb_cupon_branduser where dpid='.$dpid.' and brand_user_lid='.$userId.' and cupon_id='.$promotionId;
+			$sql = 'select * from nb_cupon_branduser where dpid='.$dpid.' and ((brand_user_lid='.$userId.' and to_group=3) or (to_group=2)) and cupon_id='.$promotionId;
 		}
 		$result = Yii::app()->db->createCommand($sql)->queryRow();
 	}
