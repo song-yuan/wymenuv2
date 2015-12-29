68e904b39754395866fc6cfa71792c0bd986134d
caofang <962460422@qq.com>
2015-12-29 13:32:49 +0800

    cf

--- protected/components/Helper.php
+++ protected/components/Helper.php
@@ -1414,7 +1414,7 @@ class Helper
            	$printlenn=(strlen($number) + mb_strlen($number,'UTF8')) / 2;
            	$printlenp=(strlen($price) + mb_strlen($price,'UTF8')) / 2;
            	$printlenr=(strlen($reaprice) + mb_strlen($reaprice,'UTF8')) / 2;
-           	array_push($listData,"00".$payname.str_pad("", 13-$printlen," ").$time.str_pad("", 9-$printlent," ").$sitenum.str_pad("", 8-$printlens," ").$number.str_pad("", 6-$printlenn," ").$price.str_pad("", 4-$printlenp," ").$reaprice);
+           	array_push($listData,"00".$payname.str_pad("", 13-$printlen," ").$time.str_pad("", 10-$printlent," ").$sitenum.str_pad("", 8-$printlens," ").$number.str_pad("", 6-$printlenn," ").$price.str_pad("", 6-$printlenp," ").$reaprice);
            	array_push($listData,"br");
            	array_push($listData,"00".str_pad('',48,'-'));
            	$a=1;//return array('status'=>false,'msg'=>$products);
@@ -1424,7 +1424,7 @@ class Helper
            		//$time=date('m-d H:i',$model->update_at);
            		$date=$model->update_at; // 数据库读取出来的时间
            		$time = strtotime($date);
-           		//$sitenum = Helper::getSiteName($model->lid);
+           		$sitenum = Helper::getSiteName($model->lid);
            		$number = $model->all_number;
            		$price = sprintf("%.2f",Helper::getOriginalMoney($model->account_no));
            		$reaprice = sprintf("%.2f",Helper::getAccountMoney($model->account_no));
@@ -1435,7 +1435,7 @@ class Helper
            		$printlenp=(strlen($price) + mb_strlen($price,'UTF8')) / 2;
            		$printlenr=(strlen($reaprice) + mb_strlen($reaprice,'UTF8')) / 2;
            		$a++;
-           		array_push($listData,"00".$payname.str_pad("", 13-$printlen," ").date("d"."日"."H:i",$time).str_pad("", 9-$printlent," ").$sitenum.str_pad("", 8-$printlens," ").$number.str_pad("", 6-$printlenn," ").$price.str_pad("", 4-$printlenp," ").$reaprice);
+           		array_push($listData,"00".$payname.str_pad("", 13-$printlen," ").date("d"."日"."H:i",$time).str_pad("", 10-$printlent," ").$sitenum.str_pad("", 8-$printlens," ").$number.str_pad("", 4-$printlenn," ").$price.str_pad("", 8-$printlenp," ").$reaprice);
            		array_push($listData,"br");
            		//                 	$payname="数量/金额";
            		//                 	$printlen=(strlen($payname) + mb_strlen($payname,'UTF8')) / 2;
@@ -1531,7 +1531,7 @@ class Helper
 		return $originalMoney;
 	}
 	//获取座位信息
-	public function getSiteName($orderId){
+public function getSiteName($orderId){
 		$sitename="";
 		$sitetype="";
 	
@@ -1547,9 +1547,9 @@ class Helper
 		if($site['site_id'] && $site['dpid'] ){
 			//	echo 'ABC';
 			$sitelevel = $site['site_level'];
-			$sitename = $site['simplecode'];
+			$sitename = $site['name'];
 			$sitetype = $site['serial'];
-			$retsite = $sitename.":".$sitetype;
+			$retsite=$sitelevel.":".$sitename.":".$sitetype;
 		}
 		//if($siteId && $dpid){
 		//$sql = 'select order.site_id, order.dpid,site.type_id, site.serial, site_type.name from nb_order, nb_site, nb_site_type where order.site_id = site.lid and order.dpid = site.dpid';
