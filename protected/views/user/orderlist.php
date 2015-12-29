2521b4feb1488966f5dcbeb2c912f1d313b8fba2
dys <daiyongsheng.good@163.com>
2015-12-29 10:00:27 +0800

    dys

--- protected/views/user/orderlist.php
+++ protected/views/user/orderlist.php
@@ -10,7 +10,7 @@
 	<ul class="orderList">
 		<li class="mar-bm-2"><div class="item col-4">时间</div><div class="item col-2">类别</div><div class="item col-2">金额</div><div class="item col-2">状态</div><div class="clear"></div></li>
 		<?php foreach($models as $model):?>
-		<?php if($model['order_status'] < 2):?>
+		<?php if($model['order_type']==1&&$model['order_status'] < 2):?>
 		<li class="mar-bm-1"><a href="<?php echo $this->createUrl('/mall/order',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>"><div class="item col-4"><?php echo $model['create_at'];?></div><div class="item col-2"><?php if($model['order_type']==1) echo '堂吃';else echo '外卖';?></div><div class="item col-2"><?php echo $model['should_total'];?>元</div><div class="item col-2"><?php if($model['order_status'] < 3) echo '<span class="ispay">待支付</span>';elseif($model['order_status'] == 3) echo '已支付';else echo '已完成';?></div><div class="clear"></div></a></li>
 		<?php else:?>
 		<li class="mar-bm-1"><a href="<?php echo $this->createUrl('/user/orderInfo',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>"><div class="item col-4"><?php echo $model['create_at'];?></div><div class="item col-2"><?php if($model['order_type']==1) echo '堂吃';else echo '外卖';?></div><div class="item col-2"><?php echo $model['should_total'];?>元</div><div class="item col-2"><?php if($model['order_status'] < 3) echo '<span class="ispay">待支付</span>';elseif($model['order_status'] == 3) echo '已支付';else echo '已完成';?></div><div class="clear"></div></a></li>
