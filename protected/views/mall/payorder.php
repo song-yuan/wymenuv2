89510de03e5f0085b8de8d29c04bd2ec1ea2d984
dys <daiyongsheng.good@163.com>
2015-12-29 09:44:38 +0800

    dys

--- protected/views/mall/payorder.php
+++ protected/views/mall/payorder.php
@@ -42,6 +42,12 @@
 <script type="text/javascript" src="<?php echo $baseUrl.'/js/layer/layer.js';?>"></script>
 
 <div class="order-title">订单详情</div>
+<?php if($address):?>
+<div class="location">
+	<span>收货人：<?php echo $address['name'];?>   <?php echo $address['mobile'];?></span><br>
+	<span class="add">收货地址：<?php echo $address['province'].$address['city'].$address['area'].$address['street'];?></span>
+</div>
+<?php endif;?>
 <div class="order-info">
 	<?php foreach($orderProducts as $product):?>
 	<div class="item">
