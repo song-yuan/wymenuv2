efce5e8b45cebb3deecffb4b37f2404448d6613f
dys <daiyongsheng.good@163.com>
2016-01-06 17:13:47 +0800

    dys

--- protected/views/user/orderlist.php
+++ protected/views/user/orderlist.php
@@ -4,17 +4,50 @@
 ?>
 
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/reset.css">
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/user.css">
-<script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/jquery-1.9.1.min.js"></script>
-<section class="prolist">
-	<ul class="orderList">
-		<li class="mar-bm-2"><div class="item col-4">时间</div><div class="item col-2">类别</div><div class="item col-2">金额</div><div class="item col-2">状态</div><div class="clear"></div></li>
-		<?php foreach($models as $model):?>
-		<?php if($model['order_type']==1&&$model['order_status'] < 2):?>
-		<li class="mar-bm-1"><a href="<?php echo $this->createUrl('/mall/order',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>"><div class="item col-4"><?php echo $model['create_at'];?></div><div class="item col-2"><?php if($model['order_type']==1) echo '堂吃';elseif($model['order_type']==2) echo '外卖';else echo '预约';?></div><div class="item col-2"><?php echo $model['should_total'];?>元</div><div class="item col-2"><?php if($model['order_status'] < 3) echo '<span class="ispay">待支付</span>';elseif($model['order_status'] == 3) echo '已支付';else echo '已完成';?></div><div class="clear"></div></a></li>
-		<?php else:?>
-		<li class="mar-bm-1"><a href="<?php echo $this->createUrl('/user/orderInfo',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>"><div class="item col-4"><?php echo $model['create_at'];?></div><div class="item col-2"><?php if($model['order_type']==1) echo '堂吃';elseif($model['order_type']==2) echo '外卖';else echo '预约';?></div><div class="item col-2"><?php echo $model['should_total'];?>元</div><div class="item col-2"><?php if($model['order_status'] < 3) echo '<span class="ispay">待支付</span>';elseif($model['order_status'] == 3) echo '已支付';else echo '已完成';?></div><div class="clear"></div></a></li>
-		<?php endif;?>
-		<?php endforeach;?>
-	</ul>
-</section>
+<link href='<?php echo $baseUrl;?>/css/mall/common.css' rel='stylesheet' type='text/css'>
+<body class="gift_exchange bg_lgrey2">
+	<div id="topnav">
+		<ul>
+			<li class="all <?php if($type==0) echo 'current';?>"><a href="<?php echo $this->createUrl('/user/orderList',array('companyId'=>$this->companyId));?>"><span>全部</span></a></li>
+			<li class="for_delivery <?php if($type==1) echo 'current';?>"><a href="<?php echo $this->createUrl('/user/orderList',array('companyId'=>$this->companyId,'t'=>1));?>"><span>待付款</span></a></li>
+			<li class="for_confirm <?php if($type==2) echo 'current';?>"><a href="<?php echo $this->createUrl('/user/orderList',array('companyId'=>$this->companyId,'t'=>2));?>"><span>已付款</span></a></li>
+		</ul>
+	</div>
+	<div class="orderlist with_topbar">
+		<!-- 全部 -->
+		<ul id="all">
+			<?php foreach($models as $model):?>
+			<li class="bg_white">
+				<?php if($model['order_type']==1&&$model['order_status'] < 2):?>
+				<a href="<?php echo $this->createUrl('/mall/order',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>">
+				<?php else:?>
+				<a href="<?php echo $this->createUrl('/user/orderInfo',array('companyId'=>$this->companyId,'orderId'=>$model['lid']));?>">
+				<?php endif;?>
+				<div class="headinfo colclear bottom_dash pad_10">
+					<div class="left small font_l"><?php echo $model['create_at'];?></div>
+					<?php if($model['order_status']< 3):?><div class="right small font_red">待付款</div><?php else:?> <div class="right small font_org">已支付</div><?php endif;?>
+				</div>
+					<!-- 商品简要情况 -->
+					<div class="shortinfo2 noborder bottom_dash">
+						<div class="maininfo">
+						<div class="left">
+							<img src="<?php echo $baseUrl;?>/img/house.jpg" class="normal">
+						</div>
+						<div class="right">
+						<h2>类型 : <?php if($model['order_type']==1) echo '堂吃';elseif($model['order_type']==2) echo '外卖';else echo '预约';?></h2><br />
+						<div class="nooverflow">
+							<span class="pts left">合计 ：￥<?php echo $model['should_total'];?></span>
+							<span class="num small right"><?php echo $model['order_num'];?>份</span>
+						</div>
+						</div>
+						</div>
+					</div>	
+					<!-- 商品简要情况 -->
+					</a>
+			</li>
+			<?php endforeach;?>
+		</ul>
+		<!-- 全部 -->
+
+	</div>
+</body>
