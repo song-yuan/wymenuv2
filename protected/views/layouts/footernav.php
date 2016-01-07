3b8a849d14f576dc89980c6359a04d099c38ab14
dys <daiyongsheng.good@163.com>
2016-01-07 18:20:06 +0800

    dys

---
+++ protected/views/layouts/footernav.php
@@ -0,0 +1,14 @@
+<?php 
+ $baseUrl = $baseUrl = Yii::app()->baseUrl;
+?>
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/footernav.css">
+<!-- 固定按钮 -->
+	<div class="bttnbar bg_dgrey2">
+		<ul>
+			<li><a href="#"><div><img src="<?php echo $baseUrl;?>/img/mall/navmain.png" alt=""></div><div class="name">首页</div></a></li>
+			<li><a href="#"><div><img src="<?php echo $baseUrl;?>/img/mall/navmall.png" alt=""></div><div class="name">订单</div></a></li>
+			<li><a href="#"><div><img src="<?php echo $baseUrl;?>/img/mall/navuser.png" alt=""></div><div class="name">我</div></a></li>
+		</ul>
+		
+	</div>
+<!-- 固定按钮 -->
