bae98503b5f798d6ee4a52153beb54dd6ce45d4a
dys <daiyongsheng.good@163.com>
2016-01-12 14:41:48 +0800

    dys

---
+++ protected/views/layouts/screenmain.php
@@ -0,0 +1,17 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="initial-scale=1, width=device-width, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <title><?php echo CHtml::encode($this->pageTitle); ?></title>  
+</head>
+<body>
+    <?php echo $content ?>
+</body>
+</html>
