89c8a9d8f71a59d9e959250834b09466b4d2bb81
dys <daiyongsheng.good@163.com>
2016-01-20 17:03:16 +0800

    dys

---
+++ protected/controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+
+class TestController extends Controller
+{
+	public function actionIndex()
+	{
+		$companyId = Yii::app()->request->getParam('companyId');
+		$order = WxOrder::getOrder(4778,6);
+		
+		new WxMessageTpl($order['dpid'],$order['user_id'],0,$order);
+		exit;
+	}
+	
+}
