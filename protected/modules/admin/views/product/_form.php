962c2e6060ac0c72b83d396c824089f42c8ec0e4
caofang <962460422@qq.com>
2016-01-27 16:32:08 +0800

    cf

--- protected/modules/admin/views/product/_form.php
+++ protected/modules/admin/views/product/_form.php
@@ -57,6 +57,13 @@
 											<?php echo $form->error($model, 'rank' )?>
 										</div>
 									</div>
+									<div class="form-group" <?php if($model->hasErrors('spicy')) echo 'has-error';?>>
+										<?php echo $form->label($model, 'spicy',array('class' => 'col-md-3 control-label'));?>
+										<div class="col-md-4">
+											<?php echo $form->dropDownList($model, 'spicy', array('0' => '0' , '1' => '1' , '2' => '2' , '3' => '3' ) , array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('spicy')));?>
+											<?php echo $form->error($model, 'spicy' )?>
+										</div>
+									</div>
                                                                         <div class="form-group">
 										<?php echo $form->label($model, 'is_discount',array('class' => 'col-md-3 control-label'));?>
 										<div class="col-md-4">
