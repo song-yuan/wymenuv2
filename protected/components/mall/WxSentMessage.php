0670ba8b65f041c75609541400e74fda87df11e8
dys <daiyongsheng.good@163.com>
2016-01-28 18:34:11 +0800

    dys

--- protected/components/mall/WxSentMessage.php
+++ protected/components/mall/WxSentMessage.php
@@ -13,14 +13,10 @@ class WxSentMessage
 	 * 发送手机短信
 	 * 
 	 */
-	public static function sentMessage($mobile,$type = 0){
-		$code = rand(1000,9999);
-		$contentTpl = array('您的验证码是：'.$code.'【物易科技】','');
-		
+	public static function sentMessage($mobile,$content = ''){
 		$userid = '';
 		$account = 'jl01';
 		$password = 'ab123456';
-		$content = $contentTpl[$type];
 		$url = 'http://sh2.ipyy.com/smsJson.aspx?action=send&userid='.$userid.'&account='.$account.'&password='.$password.'&mobile='.trim($mobile).'&content='.$content.'&sendTime=&extno=';
 		$result = Curl::httpsRequest($url);
 		return $result;
