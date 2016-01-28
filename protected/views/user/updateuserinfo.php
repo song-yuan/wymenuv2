95789bb8efec11b46aa1b1f62d20b4a4c88e9a97
dys <daiyongsheng.good@163.com>
2016-01-28 16:17:55 +0800

    dys

--- protected/views/user/updateuserinfo.php
+++ protected/views/user/updateuserinfo.php
@@ -11,7 +11,7 @@
 
 <script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/jquery-1.9.1.min.js"></script>
 <body class="add_address bg_lgrey2">
-<form action="<?php echo Yii::app()->createUrl('/user/setUserInfo',array('companyId'=>$this->companyId));?>" method="post" onsubmit="return validate()">
+<form action="<?php echo Yii::app()->createUrl('/user/saveUserInfo',array('companyId'=>$this->companyId));?>" method="post" onsubmit="return validate()">
 
 <div class="page cell">
 	<div class="weui_cells_title">完善个人资料</div>
@@ -19,20 +19,20 @@
    		<div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">姓名</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input class="weui_input" id="name" name="user[user_name]" type="tel" placeholder="请输入姓名"/>
+                <input class="weui_input" id="name" name="user[user_name]" type="tel" placeholder="请输入姓名" value="<?php echo $user['user_name'];?>"/>
             </div>
         </div>
        <div class="weui_cell">
             <div class="weui_cell_hd"><label class="weui_label">手机</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input class="weui_input" id="mobile" name="user[mobile_num]" type="tel" placeholder="请输入联系方式"/>
+                <input class="weui_input" id="mobile" name="user[mobile_num]" type="tel" placeholder="请输入联系方式" value="<?php echo $user['mobile_num'];?>"/>
             </div>
         </div>
         
         <div class="weui_cell">
             <div class="weui_cell_hd"><label for="" class="weui_label">生日</label></div>
             <div class="weui_cell_bd weui_cell_primary">
-                <input class="weui_input" id="birthday" name="user[user_birthday]" type="date" value=""/>
+                <input class="weui_input" id="birthday" name="user[user_birthday]" type="date" value="<?php echo $user['user_birthday'];?>"/>
             </div>
         </div>
     </div>
@@ -41,7 +41,7 @@
 	<button class="bttn_black2 bttn_large" type="button"><a href="<?php echo $this->createUrl('/user/index',array('companyId'=>$this->companyId));?>">取消</a></button>
 	<button class="bttn_black2 bttn_large" type="submit">保存</button>
 </div>
-
+<input type="hidden" name="user[lid]" value="<?php echo $user['lid'];?>"/>
 </form>
 <div class="weui_dialog_alert" id="dialog2" style="display: none;">
 	<div class="weui_mask"></div>
