e344c26ae5366d4a9b5f371fcb3cd2f0b48c1619
dys <daiyongsheng.good@163.com>
2016-03-01 09:24:28 +0800

    dys

--- protected/views/mall/checkorder.php
+++ protected/views/mall/checkorder.php
@@ -243,7 +243,7 @@ $(document).ready(function(){
 			$('.seatingFee').find('.num').html(parseInt(number)-1);
 			$('.seatingFee').find('.price').html((parseInt(number)-1)*seatFee);
 			
-			var totalFee = parseFloat(total) + (parseInt(number)-1)*seatFee;
+			var totalFee = parseFloat(total) - seatFee;
 			totalFee =  totalFee.toFixed(2);
 			
 			$('#total').html(totalFee);
@@ -260,7 +260,7 @@ $(document).ready(function(){
 		$('.seatingFee').find('.num').html(parseInt(number)+1);
 		$('.seatingFee').find('.price').html((parseInt(number)+1)*seatFee);
 		
-		var totalFee = parseFloat(total) + (parseInt(number)+1)*seatFee;
+		var totalFee = parseFloat(total) + seatFee;
 		totalFee =  totalFee.toFixed(2);
 		
 		$('#total').html(totalFee);
