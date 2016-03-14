4fbed3c1ddfaaa5303a73fb20f4b709f4d97028b
dys <daiyongsheng.good@163.com>
2016-03-14 13:44:51 +0800

    dys

--- protected/views/alipay/mobileweb.php
+++ protected/views/alipay/mobileweb.php
@@ -4,7 +4,7 @@
 ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/weui.min.css">
 <script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/jquery-1.9.1.min.js"></script>
-
+ <div class="weui_toptips weui_warn js_tooltips">请点击右上角，选择在浏览器中打开完成支付</div>
 <?php
     echo $htmlText;
 ?>
