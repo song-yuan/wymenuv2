f4f4d7188fb2cea62e388381f811a7ca3e4606b9
dys <daiyongsheng.good@163.com>
2016-03-14 13:54:45 +0800

    dys

--- protected/views/alipay/mobileweb.php
+++ protected/views/alipay/mobileweb.php
@@ -4,12 +4,15 @@
 ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/weui.min.css">
 <script type="text/javascript" src="<?php echo $baseUrl;?>/js/mall/jquery-1.9.1.min.js"></script>
- <div class="weui_toptips weui_warn js_tooltips">请点击右上角，选择在浏览器中打开完成支付</div>
+ <style type="text/css">
+.ali_toptips{background:#04BE02;color:#FFF;text-align: center;font-size: 18px;line-height:2.33333333}
+</style>
+ <div class="ali_toptips">请点击右上角，选择在浏览器中打开完成支付</div>
 <?php
     echo $htmlText;
 ?>
 <script type="text/javascript">
 $(document).ready(function(){
-	$('#alipaysubmit').submit();
+//	$('#alipaysubmit').submit();
 });
 </script>
