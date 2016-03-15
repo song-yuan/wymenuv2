d61da65e4e2f9b9838ef65bbc2053bb54679ab2c
dys <daiyongsheng.good@163.com>
2016-03-15 15:51:13 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -189,7 +189,8 @@ $(document).ready(function(){
         	var id = $(this).attr('id');
             var top = $(this).offset().top;
             var height = $(this).outerHeight();
-            if(top == 0){
+            alert(top);
+            if(top < 0){
             	$(this).find('.prt-title').addClass('top');
             }
             if(top < 0 && (parseInt(top) + parseInt(height)) > 5){
