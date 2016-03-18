4003bd3e58442685fb6d6cade7e1ce6068bb454f
dys <daiyongsheng.good@163.com>
2016-03-18 13:28:51 +0800

    dys

--- protected/components/mall/WxStatistic.php
+++ protected/components/mall/WxStatistic.php
@@ -18,7 +18,7 @@ class WxStatistic
 	    return $statistic;
 	}
 	public static function getStatisticByOrderPayType($dpid,$start,$end){
-		$sql = 'select t.paytype, sum(t1.pay_amount) as total from nb_order t right join nb_order_pay t1 on t.lid = t1.order_id and t.dpid=t1.dpid where t.dpid=:dpid and t.order_status in (3,4) and  t.create_at > :start and t.create_at < :end group by t.paytype';
+		$sql = 'select t1.paytype, sum(t1.pay_amount) as total from nb_order t right join nb_order_pay t1 on t.lid = t1.order_id and t.dpid=t1.dpid where t.dpid=:dpid and t.order_status in (3,4) and  t.create_at > :start and t.create_at < :end group by t1.paytype';
 		$statistic = Yii::app()->db->createCommand($sql)
 				  ->bindValue(':dpid',$dpid)
 				  ->bindValue(':start',$start)
