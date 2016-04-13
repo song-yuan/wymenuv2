6b45d18fc4357a5e015f20a3d47138ca9f43675f
dys <daiyongsheng.good@163.com>
2016-04-13 10:12:22 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -339,7 +339,7 @@ $(document).ready(function(){
         	dataType:'json'
         });
    });
-    $("#container").on('click','.lt-lt',function(){
+    $("body").on('click','.lt-lt',function(){
     	var str = $(this).html();
     	layer.open({
 		    type: 1,
