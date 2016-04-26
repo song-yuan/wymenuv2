3f0008e4046a65f89bed539b17c772b23f3f94a0
dys <daiyongsheng.good@163.com>
2016-04-26 17:06:04 +0800

    dys

--- protected/views/mall/payorderbyqrcode.php
+++ protected/views/mall/payorderbyqrcode.php
@@ -35,7 +35,7 @@
 
 		$result = $notify->GetPayUrl($input);
 		$url2 = $result["code_url"];
-		echo $url2;exit;
+		echo $url2;
 		$code=new QRCode($url2);
 		$code->create();
 	}catch(Exception $e){
