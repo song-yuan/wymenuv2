ed273d1eb451f7a779f19201bc9c10c1f57dcc92
dys <daiyongsheng.good@163.com>
2016-05-26 15:02:12 +0800

    dys

--- protected/views/mall/paybill.php
+++ protected/views/mall/paybill.php
@@ -70,7 +70,7 @@
 			function(res){
 				 if(res.err_msg == "get_brand_wcpay_request:ok" ) {
 				 	// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
-				 	location.href = '<?php echo $this->createUrl('/user/orderInfo',array('companyId'=>$this->companyId,'orderId'=>$order['lid']));?>';
+
 				 }else{
 				 	//支付失败或取消支付
 				 	
