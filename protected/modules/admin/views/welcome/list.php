fd503f247c2774aed49475e3ef3076539e014b40
caofang <962460422@qq.com>
2016-06-24 11:07:11 +0800

    cf

--- protected/modules/admin/views/welcome/list.php
+++ protected/modules/admin/views/welcome/list.php
@@ -3,15 +3,15 @@
 
 		.bg-welcome{
 			height: 760px;
-			background-image:url(../../../../img/waiter/icon-welcome.png);
-			background-position: 150px 10px;
-			background-size: 70%;
+			background-image:url(../../../../img/waiter/icon-welcome2.png);
+			background-position: 15px 10px;
+			background-size: 100%;
     		background-repeat: no-repeat;
 		}		
 	</style>
 <div class="page-content">
 	<div class="bg-welcome"></div>
-1
+2
 	</div>
 	<!-- END PAGE CONTENT-->
 	<script>
