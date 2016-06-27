56faa42307eef177d1561e0bca70731f914e6457
caofang <962460422@qq.com>
2016-06-27 12:39:47 +0800

    cf

--- protected/modules/admin/views/welcome/list.php
+++ protected/modules/admin/views/welcome/list.php
@@ -2,7 +2,7 @@
 <style>
 
 		.bg-welcome{
-			height: 760px;
+			min-height: 760px;
 			background-image:url(../../../../img/waiter/bg-welcome3.jpg);
 			background-size: 100%;
     		background-repeat: no-repeat;
@@ -14,19 +14,30 @@
     		background-repeat: no-repeat;
 		}
 		.page-content2{
-			min-height: 760px;
-			margin-left: 55px !important;
-		}		
+			padding: 0px !important;
+			margin-bottom: 25px;
+		}	
+		.portlet2{
+			margin-bottom: 0px !important; 	
+		}
+		.purple2{
+			background-color: #9D48BE;
+		}
 	</style>
-<div class="page-content2 bg-welcome">
-	<div style="height: 160px;"></div>
-	<div class="bg-logo">
-	
-	</div>
-	<div style="height: 100px;"></div>
-	<div style="height: 40px;text-align: center;font-size: 16px;"><SPAN style="font-size: 20px;font-weight: bold;">壹点吃</SPAN>致力于为用户提供SaaS+智能硬件+移动互联网的智慧餐饮物联网解决方案</div>
-	<div style="height: 16px;text-align: center;">上海物易网络科技有限公司</div>
-</div>
+<div class="page-content page-content2">
+	 <div class="row">
+		<div class="col-md-12">
+			<div class="portlet portlet2 purple2  bg-welcome">
+			
+				<div style="height: 160px;"></div>
+				<div class="bg-logo"></div>
+				<div style="height: 100px;"></div>
+				<div style="height: 40px;text-align: center;font-size: 16px;"><SPAN style="font-size: 20px;font-weight: bold;">壹点吃</SPAN>致力于为用户提供SaaS+智能硬件+移动互联网的智慧餐饮物联网解决方案</div>
+				<div style="height: 16px;text-align: center;">上海物易网络科技有限公司</div>
+			</div>
+		</div>
+	</div> 
+
 	<!-- END PAGE CONTENT-->
 	<script>
         $(document).ready(function() {
