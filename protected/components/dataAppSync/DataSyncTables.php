20a8320f06295d807a0e7fdc7baeb9690dc85941
dys <daiyongsheng.good@163.com>
2016-08-28 16:32:40 +0800

    dys

--- protected/components/dataAppSync/DataSyncTables.php
+++ protected/components/dataAppSync/DataSyncTables.php
@@ -271,6 +271,7 @@ class DataSyncTables
                     "  `main_picture` varchar(255) NOT NULL,".
                     "  `description` text NOT NULL,".
                     "  `rank` tinyint NOT NULL DEFAULT '3',".
+        			"  `sort` int(4) NOT NULL DEFAULT '50',".
                     "  `spicy` tinyint NOT NULL DEFAULT '0',".
                     "  `is_temp_price` char(1) NOT NULL DEFAULT '0',".
                     "  `is_member_discount` char(1) NOT NULL DEFAULT '0',".
@@ -311,6 +312,7 @@ class DataSyncTables
 				"  `category_name` varchar(50) NOT NULL,".
 				"  `chs_code` varchar(12) NOT NULL,".
 				"  `type` varchar(3) NOT NULL DEFAULT '0',".
+        		"  `cate_type` tinyint NOT NULL DEFAULT '0',".
 				"  `main_picture` varchar(255) NOT NULL,".
 				"  `order_num` int(4) NOT NULL DEFAULT '0',".
 				"  `delete_flag` char(1) NOT NULL DEFAULT '0',".
