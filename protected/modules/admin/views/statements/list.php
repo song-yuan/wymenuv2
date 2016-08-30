ce38808969100b4a9944a6a4bd3ba40035efe55a
caofang <962460422@qq.com>
2016-08-30 19:19:17 +0800

    cf

--- protected/modules/admin/views/statements/list.php
+++ protected/modules/admin/views/statements/list.php
@@ -177,7 +177,13 @@
 					<a href="<?php echo $this->createUrl('statements/ceshiproductReport',array('companyId' => $this->companyId,'text'=>'3','setid'=>'1','ordertype'=>'0','begin_time'=>date('Y-m-d',time()),'end_time'=>date('Y-m-d',time()),'page'=>1));?>">
 						<div class="pull-left margin-left-right">
 							<div class="ku-item ku-grey cpxs"></div>
-							<div class="ku-item-info">产品销售</div>
+							<div class="ku-item-info">单品销售</div>
+						</div>
+					</a>
+					<a href="<?php echo $this->createUrl('statements/ceshiproductsetReport',array('companyId' => $this->companyId,'text'=>'3','setid'=>'1','ordertype'=>'0','begin_time'=>date('Y-m-d',time()),'end_time'=>date('Y-m-d',time()),'page'=>1));?>">
+						<div class="pull-left margin-left-right">
+							<div class="ku-item ku-grey cpxs"></div>
+							<div class="ku-item-info">套餐销售</div>
 						</div>
 					</a>
 					<a href="<?php echo $this->createUrl('statements/orderdetail',array('companyId' => $this->companyId,'text'=>'3','begin_time'=>date('Y-m-d',time()),'end_time'=>date('Y-m-d',time()),'page'=>1));?>">
