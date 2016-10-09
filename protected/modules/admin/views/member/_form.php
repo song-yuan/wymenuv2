929e0ee4322ae940c9e62f5f2a07933603fe750e
caofang <962460422@qq.com>
2016-10-09 18:33:30 +0800

    cf

--- protected/modules/admin/views/member/_form.php
+++ protected/modules/admin/views/member/_form.php
@@ -47,7 +47,7 @@
 			<div class="form-group" <?php if($model->hasErrors('level_id')) echo 'has-error';?>>
 				<?php echo $form->label($model, 'level_id',array('class' => 'col-md-3 control-label'));?>
 				<div class="col-md-4">
-					<?php echo $form->dropDownList($model, 'level_id',array('0' => yii::t('app','-- 请选择 --')) +Helper::getCardLevel() ,array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('level_id')));?>
+					<?php echo $form->dropDownList($model, 'level_id',array('0' => yii::t('app','-- 请选择 --')) +Helper::getCardLevels() ,array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('level_id')));?>
 					<?php echo $form->error($model, 'level_id' )?>
 				</div>
 			</div>
