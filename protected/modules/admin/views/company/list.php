2ae37755630426ff69ceacfb068a638bd50388ee
caofang <962460422@qq.com>
2016-11-10 19:14:26 +0800

    cf

--- protected/modules/admin/views/company/list.php
+++ protected/modules/admin/views/company/list.php
@@ -1,27 +1,4 @@
-<link href="../../../../css/jxcgl.css" rel="stylesheet" type="text/css">
-<div class="page-content">
-	<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->               
-	<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
-					<h4 class="modal-title">关系图</h4>
-				</div>
-				<div class="modal-body">
-					<img alt="" src="">
-				</div>
-				<div class="modal-footer">
-					<!--  
-					<button type="button" class="btn blue">Save changes</button>
-					-->
-					<button type="button" class="btn default" data-dismiss="modal">确定</button>
-				</div>
-			</div>
-			<!-- /.modal-content -->
-		</div>
-		<!-- /.modal-dialog -->
-	</div>
+
 	<!-- /.modal -->
 	<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
 	<style>
@@ -99,6 +76,29 @@
 			margin-right:10px;
 		}
 	</style>
+	<div class="page-content">
+		<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->               
+		<div class="modal fade" id="portlet-config" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true"></button>
+						<h4 class="modal-title">关系图</h4>
+					</div>
+					<div class="modal-body">
+						<img alt="" src="">
+					</div>
+					<div class="modal-footer">
+						<!--  
+						<button type="button" class="btn blue">Save changes</button>
+						-->
+						<button type="button" class="btn default" data-dismiss="modal">确定</button>
+					</div>
+				</div>
+				<!-- /.modal-content -->
+			</div>
+			<!-- /.modal-dialog -->
+		</div>
 	<!-- BEGIN PAGE CONTENT-->
 		<?php $this->widget('application.modules.admin.components.widgets.PageHeader', array('head'=>yii::t('app','店铺管理'),'subhead'=>yii::t('app','店铺管理'),'breadcrumbs'=>array(array('word'=>yii::t('app','店铺管理'),'url'=>''))));?>
 	<div class="row">
@@ -131,6 +131,12 @@
 						<div class="pull-left margin-left-right">
 							<div class="ku-item ku-purple syjsz"></div>
 							<div class="ku-item-info">收银机设置</div>
+						</div>
+					</a>
+					<a href="<?php echo $this->createUrl('doubleScreen/index',array('companyId'=>$this->companyId));?>">
+						<div class="pull-left margin-left-right">
+							<div class="ku-item ku-purple syjsz"></div>
+							<div class="ku-item-info">双屏设置</div>
 						</div>
 					</a>
 					<?php endif;?>
