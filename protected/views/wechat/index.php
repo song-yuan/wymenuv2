48f1dfced79ff30f3f457312663d84598c3b03f5
caofang <962460422@qq.com>
2016-12-15 10:29:27 +0800

    cf

--- protected/views/wechat/index.php
+++ protected/views/wechat/index.php
@@ -1,9 +1,9 @@
-<?php// Yii::app()->clientScript->registerScriptFile( Yii::app()->request->baseUrl.'/js/wechat/zepto.min.js');?>
+<?php Yii::app()->clientScript->registerScriptFile( Yii::app()->request->baseUrl.'/js/wechat/zepto.min.js');?>
 <?php Yii::app()->clientScript->registerScriptFile( Yii::app()->request->baseUrl.'/js/wechat/example.js');?>
 <?php Yii::app()->clientScript->registerCssFile( Yii::app()->request->baseUrl.'/css/wechat/weui.css');?>
 <?php Yii::app()->clientScript->registerCssFile( Yii::app()->request->baseUrl.'/css/wechat/example.css');?>
 
-<html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
@@ -19,34 +19,33 @@
             </div>
             <div class="bd">
                 <div class="weui_cells weui_cells_access global_navs">
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="button">
-                        <span class="weui_cell_hd "><img src="img/icon_nav_button.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="money">
+                        <span class="weui_cell_hd "><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdcz.png" class="icon_nav" alt=""/></span>
                         
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>我的储值</p>
                         </div>
                         <div class="weui_cell_ft">
-                        </div>
-                           
+                        </div>         
                     </a>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="cell">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_cell.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="point">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdjf.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>我的积分</p>
                         </div>
                         <div class="weui_cell_ft">
                         </div>
                     </a>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="toast">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_toast.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="ticket">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdq.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>我的券</p>
                         </div>
                         <div class="weui_cell_ft">
                         </div>
                     </a>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="dialog">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_dialog.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="code">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdewm.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>我的二维码</p>
                         </div>
@@ -54,8 +53,8 @@
                         </div>
                     </a>
                     <div class="empty1"></div>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="progress">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_button.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="privilege">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdzxtq.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>我的专享特权</p>
                         </div>
@@ -63,16 +62,16 @@
                         </div>
                     </a>
                     <div class="empty2">商家优惠活动</div>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="msg">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_msg.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="sale1">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdyh.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>满减</p>
                         </div>
                         <div class="weui_cell_ft">
                         </div>
                     </a>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="article">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_article.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="sale2">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdyh.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>满送</p>
                         </div>
@@ -80,16 +79,16 @@
                         </div>
                     </a>
                     <div class="empty1"></div>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="actionSheet">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_actionSheet.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="info">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-grxx.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>个人信息</p>
                         </div>
                         <div class="weui_cell_ft">
                         </div>
                     </a>
-                    <a class="weui_cell js_cell" href="javascript:;" data-id="icons">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_icons.png" class="icon_nav" alt=""></span>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="bill">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-zd.png" class="icon_nav" alt=""></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>账单</p>
                         </div>
@@ -97,8 +96,8 @@
                         </div>
                     </a>
                     <div class="empty1"></div>
-                     <a class="weui_cell js_cell" href="javascript:;" data-id="icons">
-                        <span class="weui_cell_hd"><img src="img/icon_nav_icons.png" class="icon_nav" alt=""></span>
+                     <a class="weui_cell js_cell" href="javascript:;" data-id="tel">
+                        <span class="weui_cell_hd"></span>
                         <div class="weui_cell_bd weui_cell_primary">
                             <p>门店及电话</p>
                         </div>
@@ -115,5 +114,94 @@
            
         </div>
     </div>
+    
+     <script type="text/html" id="tpl_money">
+       <div class="page">
+           <div class="money1">
+               我的储值余额(元)
+           </div>
+           <div class="money2">
+               0.00
+           </div>
+           <div class="empty2">储值有优惠<span>(暂不支持网上支付，请店内充值)</span></div>
+           <div class="bd"> 
+               <div class="weui_cells weui_cells_access global_navs">
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="money">
+                        <span class="weui_cell_hd "><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdcz.png" class="icon_nav" alt=""/></span>
+                        
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>充值600元，送50元</p>
+                        </div>
+                        <div class="weui_cell_ft">
+                        </div>         
+                    </a>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="point">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdcz.png" class="icon_nav" alt=""></span>
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>充值1000元，送100元</p>
+                        </div>
+                        <div class="weui_cell_ft">
+                        </div>
+                    </a>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="ticket">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdcz.png" class="icon_nav" alt=""></span>
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>充值2000元，送240元</p>
+                        </div>
+                        <div class="weui_cell_ft">
+                        </div>
+                    </a>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="code">
+                        <span class="weui_cell_hd"><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat/icon-wdcz.png" class="icon_nav" alt=""></span>
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>充值5000元，送750元</p>
+                        </div>
+                        <div class="weui_cell_ft">
+                        </div>
+                    </a>
+                   
+                 </div>
+           </div> 
+        </div>   
+    </script>
+    <script type="text/html" id="tpl_point">
+       <div class="page">
+           
+           <div class="point_style">
+                <div class="point1">
+                    <span> 我的积分(元)</span>
+                </div>
+                <div class="point2">
+                    <span>0</span>
+                </div>
+           </div>
+           <div class="bd"> 
+               <div class="weui_cells weui_cells_access global_navs">
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="money">
+                        <span class="weui_cell_hd "></span>
+                       
+                            <div class="weui_cell_bd weui_cell_primary">
+                                <p>积分换礼</p>
+                            </div>
+                            <div class="weui_cell_ft">
+                            </div> 
+                       
+                    </a>
+                    <a class="weui_cell js_cell" href="javascript:;" data-id="point">
+                        <span class="weui_cell_hd"></span>
+                        <div class="weui_cell_bd weui_cell_primary">
+                            <p>积分记录</p>
+                        </div>
+                        <div class="weui_cell_ft">
+                        </div>
+                    </a>
+   
+                 </div>
+           </div> 
+        </div>   
+    </script>
+ </body>        
+    
+
+    
  
-</body>>
