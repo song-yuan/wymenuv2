4c308bb118ffb70aafe824262466632c4db4929f
dys <daiyongsheng.good@163.com>
2016-12-28 14:34:28 +0800

    dys

---
+++ protected/components/MicroPayModel.php
@@ -0,0 +1,34 @@
+<?php
+class MicroPayModel
+{
+	public static function insert($data){
+		$time = time();
+		$dpid = $data['dpid'];
+		$payType = $data['pay_type'];
+		$outTradeNo = $data['out_trade_no'];
+		$totalFee = $data['total_fee'];
+		$se = new Sequence ( "micro_pay" );
+		$microPayId = $se->nextval ();
+		$microPayData = array (
+				'lid' => $microPayId,
+				'dpid' => $dpid,
+				'create_at' => date ( 'Y-m-d H:i:s', $time ),
+				'update_at' => date ( 'Y-m-d H:i:s', $time ),
+				'pay_type' => $payType,
+				'out_trade_no' => $outTradeNo,
+				'total_fee' => $totalFee,
+				'is_sync' => $isSync
+		);
+		$reslut = Yii::app ()->db->createCommand ()->insert ( 'nb_micro_pay', $microPayData );
+		if($reslut){
+			$msg = array('status'=>true);
+		}else{
+			$msg = array('status'=>false);
+		}
+		return $msg;
+	}
+	public static function update($dpid,$out_trade_no,$transactionId,$result){
+		$sql = "update nb_micro_pay set transaction_id=".$transactionId.", pay_result='".$result."' where dpid=".$dpid." and out_trade_no=".$out_trade_no;
+		Yii::app ()->db->createCommand ($sql)->execute();
+	}
+}
