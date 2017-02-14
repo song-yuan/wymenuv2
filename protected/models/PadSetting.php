0487f5b4094517cb5e427d96996306be84b0ef37
dys <daiyongsheng.good@163.com>
2017-02-14 14:04:43 +0800

    dys

--- protected/models/PadSetting.php
+++ protected/models/PadSetting.php
@@ -59,7 +59,7 @@ class PadSetting extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
-				
+				'detail'=>array(self::HAS_MANY,'PadSettingDetail','','on'=>'t.lid=detail.pad_setting_id')
 		);
 	}
 
