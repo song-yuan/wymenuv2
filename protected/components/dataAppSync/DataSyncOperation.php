5df25cdcdc1d3cb21b00e4d255f994b4187b3a66
dys <daiyongsheng.good@163.com>
2017-03-01 17:42:51 +0800

    dys

--- protected/components/dataAppSync/DataSyncOperation.php
+++ protected/components/dataAppSync/DataSyncOperation.php
@@ -1563,7 +1563,7 @@ class DataSyncOperation {
 	public static function dealWxHykPay($data) {
 		var_dump($data);exit;
 		$cardId = $data['card_id'];
-		$cupons = $data['cupon'];
+		$cupons = isset($data['cupon'])?$data['cupon']:array();
 		$yue = $data['yue'];
 		$points = $data['points'];
 		$user = WxBrandUser::getFromCardId($cardId);
