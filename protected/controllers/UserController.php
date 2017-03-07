4c5e67496358372b294d861407beffb479e4889e
dys <daiyongsheng.good@163.com>
2017-03-07 16:13:46 +0800

    dys

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -28,7 +28,6 @@ class UserController extends Controller
 					$newBrandUser = new NewBrandUser($openid, $this->weixinServiceAccount['appid']);
 		    		$this->brandUser = $newBrandUser->brandUser;
 				}
-				var_dump($this->brandUser);exit;
 				$userId = $this->brandUser['lid'];
 				Yii::app()->session['userId'] = $userId;
 			}else{
