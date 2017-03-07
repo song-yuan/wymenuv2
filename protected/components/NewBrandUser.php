2af3347b7ac077359c965b90e8fd16afa21d5f39
dys <daiyongsheng.good@163.com>
2017-03-07 17:02:12 +0800

    dys

--- protected/components/NewBrandUser.php
+++ protected/components/NewBrandUser.php
@@ -63,7 +63,7 @@ class NewBrandUser {
        
     }
     public function brandUserLevelId() {
-    	$sql = 'SELECT 	lid FROM nb_brand_user WHERE dpid = ' . $this->brandId .' and level_type=1 and delete_flag=0 order by level_discount desc limit 1';
+    	$sql = 'SELECT	lid FROM nb_brand_user_level WHERE dpid = ' . $this->brandId .' and level_type=1 and delete_flag=0 order by level_discount desc limit 1';
     	$result = Yii::app()->db->createCommand($sql)->queryRow();
     	if($result){
     		return $result['lid'];
