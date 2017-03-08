bb980522b80d5e4eb81d9f5ec5dbaca551b8dc08
zhangxue <zhangxue@gmail.com>
2017-03-08 15:27:04 +0800

    xuexue

--- protected/views/user/ticket.php
+++ protected/views/user/ticket.php
@@ -1,3 +1,7 @@
+<?php
+	$baseUrl = Yii::app()->baseUrl;
+	$this->setPageTitle('我的券');
+?>
 <?php Yii::app()->clientScript->registerCssFile( Yii::app()->request->baseUrl.'/css/wechat_css/weui.css');?>
 <?php Yii::app()->clientScript->registerCssFile( Yii::app()->request->baseUrl.'/css/wechat_css/example.css');?>
 <?php Yii::app()->clientScript->registerScriptFile( Yii::app()->request->baseUrl.'/js/jquery-1.7.1.min.js');?>
