4b3748e39d7d4e03f4f0d77ab2582bced9c53071
dys <daiyongsheng.good@163.com>
2017-03-16 16:35:33 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -239,7 +239,7 @@ $(document).ready(function(){
             var top = $(this).offset().top;
             var height = $(this).outerHeight();
             layer.msg(top+'-'+ptHeight+'-'+height);
-            if(top < ptHeight && (parseInt(top) + parseInt(height) - parseInt(ptHeight)) >= 0){
+            if(top < ptHeight && (parseInt(top) + parseInt(height) - 2*parseInt(ptHeight)) >= 0){
             	$(this).find('.prt-title').addClass('top');
 	    		$('a[href=#'+id+']').parents('ul').find('li').removeClass('current');
 	        	$('a[href=#'+id+']').parent('li').addClass('current');
