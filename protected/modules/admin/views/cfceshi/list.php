9b9347a50435e0eebd160b5bf8a2f274d6cb29c6
caofang <962460422@qq.com>
2017-03-19 13:55:26 +0800

    cf

---
+++ protected/modules/admin/views/cfceshi/list.php
@@ -0,0 +1,252 @@
+
+<div class="page-content">
+	<!-- BEGIN SAMPLE PORTLET CONFIGURATION MODAL FORM-->               
+
+	<!-- /.modal -->
+	<!-- END SAMPLE PORTLET CONFIGURATION MODAL FORM-->
+	
+	<!-- BEGIN PAGE CONTENT-->
+	<?php if($type==0):?>
+		<?php $this->widget('application.modules.admin.components.widgets.PageHeader', array('breadcrumbs'=>array(array('word'=>yii::t('app','测试'),'url'=>''))));?>
+	<?php endif;?>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="portlet purple box">
+				<div class="portlet-title">
+					<div class="caption"><i class=" fa fa-edit"></i><a href=""><span class="tab tab-active"><?php echo yii::t('app','收钱吧对接测试');?></span></a></div>
+					<div class="actions">
+						
+					</div>
+				</div>
+				<div style="min-height: 30px;display:none" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">Code:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="code" class="form-control" value="14599168">
+					</div>
+				</div>
+				<div style="min-height: 30px;display:none" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">设备号:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="device_id" class="form-control" value="015658444456">
+					</div>
+				</div>
+				<div style="min-height: 30px;display:none" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">终端号:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="terminal_sn" class="form-control" value="100000880001181872">
+					</div>
+				</div>
+				<div style="min-height: 30px;display:none" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">终端秘钥:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="terminal_key" class="form-control" value="71a81c361faef0d7c94b5daa1343ffcf">
+					</div>
+				</div>
+				<div style="min-height: 30px;" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">价格:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="price" class="form-control" value="0.01">
+					</div>
+				</div>
+				<div style="min-height: 30px;" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">订单号:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="clientSn" class="form-control" value="1489738207-0000000027-922">
+					</div>
+				</div>
+				<div style="min-height: 30px;display:" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">支付流水号:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="sn" class="form-control" value="7895259476357545">
+					</div>
+				</div>
+				<div style="min-height: 30px;" class="form-group">
+	                <lable style="font-size: 16px;margin-top: 10px;text-align: right; " class="col-md-3 control-label">扫条码:</lable>
+					<div style="margin-top: 5px;" class="col-md-4">
+						<input id="dynamicId" class="form-control" value="">
+					</div>
+				</div>
+				<div class="form-actions fluid">
+						<div class="col-md-offset-9 col-md-3">
+							<button type="button" class="btn green" id="stocktaking">激活</button> 
+							<button type="button" class="btn green" id="stockCheck">签到</button> 
+							<button type="button" class="btn green" id="stockOrder">下单</button>   
+							<button type="button" class="btn green" id="stockPay">支付</button>  
+							<button type="button" class="btn green" id="stockRefund">退款</button>   
+							<button type="button" class="btn green" id="stockFind">查询</button>                        
+						</div>
+					</div>
+			</div>
+		</div>
+	</div>
+	<!-- END PAGE CONTENT-->
+	<script>
+	$("#stocktaking").on("click",function(){
+		
+        var code = $("#code").val();
+        var device_id = $("#device_id").val();
+       
+        $.ajax({
+            type:'POST',
+			url:"<?php echo $this->createUrl('cfceshi/sqbactivate',array('companyId'=>$this->companyId,));?>",
+			async: false,
+			//data:'{"clientSn":'+'"1234567890"'+',"totalAmount":'+'"0.01"'+',"payType":'+'"3"'+',"dynamicId":"'+price+'","abstract":"ceshi"'+',"userName":'+'"CF"}',
+            data: {
+            	code: code,
+            	device_id: device_id,
+            },
+            cache:false,
+            dataType:'json',
+			success:function(msg){
+	            //alert(msg.status);
+	            if(msg.status=="success")
+	            {            
+		            
+			        layer.msg("激活成功！");
+			          
+		            location.reload();
+	            }else{
+		            layer.mag("11");
+		            location.reload();
+	            }
+			},
+            error:function(){
+				layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+			},
+		});
+        
+		});
+	$("#stockPay").on("click",function(){
+
+		var device_id = $("#device_id").val();
+        var price = $("#price").val();
+        var dynamicId = $("#dynamicId").val();
+        var username = '<?php echo Yii::app()->user->username?>';
+        $.ajax({
+            type:'GET',
+			url:"<?php echo $this->createUrl('../alipay/barpay',array('companyId'=>$this->companyId,));?>/pay_price/"+price+"/auth_code/"+dynamicId+"/poscode/"+device_id+"/username/"+username,
+			async: false,
+            cache:false,
+            dataType:'json',
+			success:function(msg){
+	            layer.msg(msg.status);
+			},
+            error:function(){
+				layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+			},
+		});
+        
+	});
+	$("#stockFind").on("click",function(){
+
+		var terminal_sn = $("#terminal_sn").val();
+		var terminal_key = $("#terminal_key").val();
+		var clientSn = $("#clientSn").val();
+		var sn = $("#sn").val();;
+        var username = '<?php echo Yii::app()->user->username?>';
+        $.ajax({
+            type:'GET',
+			url:"<?php echo $this->createUrl('cfceshi/sqbquery',array('companyId'=>$this->companyId,));?>/terminal_sn/"+terminal_sn+"/terminal_key/"+terminal_key+"/sn/"+sn+"/clientSn/"+clientSn,
+			async: false,
+            cache:false,
+            dataType:'json',
+			success:function(msg){
+	            layer.msg(msg.status);
+			},
+            error:function(){
+				layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+			},
+		});
+        
+	});
+	$("#stockCheck").on("click",function(){
+    $.ajax({
+        type:'POST',
+		url:"<?php echo $this->createUrl('cfceshi/sqbcheck',array('companyId'=>$this->companyId,));?>",
+		async: false,
+		data: {device_id:'011686776456'},
+        cache:false,
+        dataType:'json',
+		success:function(msg){
+            //alert(msg.status);
+            if(msg.status=="success")
+            {            
+	            
+		        layer.msg("盘点成功！");
+		          
+	            location.reload();
+            }else{
+	            layer.mag("11");
+	            location.reload();
+            }
+		},
+        error:function(){
+			layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+		},
+	});
+    
+	});
+	$("#stockRefund").on("click",function(){
+
+		var device_id = $("#device_id").val();
+        var refund_fee = total_fee = $("#price").val();
+	    var out_trade_no = $("#clientSn").val();
+	    var admin_id = '0000000178';
+	    //layer.msg(admin_id);return false;
+	    $.ajax({
+	        type:'GET',
+			url:"<?php echo $this->createUrl('../weixin/refund',array('companyId'=>$this->companyId,));?>/poscode/"+device_id+"/admin_id/"+admin_id+"/out_trade_no/"+out_trade_no+"/total_fee/"+total_fee+"/refund_fee/"+refund_fee,
+			async: false,
+			
+	        cache:false,
+	        dataType:'json',
+			success:function(msg){
+	            //alert(msg.status);
+	            if(msg.status)
+	            {            
+		            
+			        layer.msg("退款成功！");
+			          
+		            //location.reload();
+	            }else{
+		            layer.msg("退款失败！");
+		            //location.reload();
+	            }
+			},
+	        error:function(){
+				layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+			},
+		});
+	    
+	});
+	$("#stockOrder").on("click",function(){
+	
+	    var device_id = $("#device_id").val();
+	    $.ajax({
+	        type:'POST',
+			url:"<?php echo $this->createUrl('cfceshi/sqbprecreate',array('companyId'=>$this->companyId,));?>",
+			async: false,
+			data: {device_id: device_id},
+	        cache:false,
+	        dataType:'json',
+			success:function(msg){
+	            //alert(msg.status);
+	            if(msg.status=="success")
+	            {            
+		            
+			        layer.msg("盘点成功！");
+			          
+		            location.reload();
+	            }else{
+		            layer.mag("11");
+		            location.reload();
+	            }
+			},
+	        error:function(){
+				layer.msg("<?php echo yii::t('app','失败'); ?>"+"2");                                
+			},
+		});
+	    
+	});
+
+	</script>
--- protected/views/alipay/barpay.php
+++ protected/views/alipay/barpay.php
@@ -14,83 +14,119 @@ $data = array(
 $result = MicroPayModel::insert($data);
 
 if($authCode!=''&&$result['status']){
-	$subject = $company['company_name']."-扫码";
 	
-	$undiscountableAmount = "0.01";
-	$sellerId = $this->alipay_config['seller_id'];
-	$body = "购买商品共花费".$totalAmount."元";
-	
-	//商户操作员编号，添加此参数可以为商户操作员做销售统计
-	$operatorId = '';
-	
-	// (必填) 商户门店编号
-	$storeId = "wy_".$this->companyId;
-	
-	$goodsDetailList = array();
-	if($goodStr!=''){
-		$goodsArr = json_decode($goodStr);
-		foreach ($goodsArr as $goods){
-			$goodsDetai = new GoodsDetail();
-			$goodsDetai->setGoodsId($goods[0]);
-			$goodsDetai->setGoodsName($goods[1]);
-			$goodsDetai->setPrice($goods[2]);
-			$goodsDetai->setQuantity($goods[3]);
-			//得到商品1明细数组
-			array_push($goodsDetailList,$goodsDetai->getGoodsDetail());
+	$compaychannel = WxCompany::getpaychannel($dpid);
+	if($compaychannel['pay_channel']=='2'){
+		$result = SqbPay::pay(array(
+				'type'=>'1',
+				'device_id'=>$poscode,
+				'dynamicId'=>$auth_code,
+				'totalAmount'=>''.$totalAmount*100,
+				'clientSn'=>$outTradeNo,
+				'dpid'=>$dpid,
+				'subject'=>$company['company_name'],
+				'operator'=>$username,
+		));
+		if($result){
+			if($result["return_code"] == "SUCCESS" && $result["result_code"] == "SUCCESS"){
+				$transactionId = $result["transaction_id"];
+				MicroPayModel::update($dpid, $orderId, $transactionId, json_encode($result));
+				echo json_encode(array('status'=>true, 'result'=>true, 'trade_no'=>$outTradeNo));
+				exit;
+			}elseif($result["return_code"] == "SUCCESS" && $result["result_code"] == "CANCEL"){
+				echo json_encode(array('status'=>true, 'result'=>false, 'trade_no'=>$outTradeNo));
+				exit;
+			}elseif($result["return_code"] == "SUCCESS" && $result["result_code"] == "CANCEL_SUCCESS"){
+				echo json_encode(array('status'=>true, 'result'=>false, 'trade_no'=>$outTradeNo));
+				exit;
+			}else{
+				echo json_encode(array('status'=>false));
+				exit;
+			}
+		}else{
+			echo json_encode(array('status'=>false));
+			exit;
 		}
-	}
-
-	// 支付宝的店铺编号
-	$alipayStoreId = "";
-	// 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)
-	$providerId = ""; //系统商pid,作为系统商返佣数据提取的依据 
-	$extendParams = new ExtendParams();
-	$extendParams->setSysServiceProviderId($providerId);
-	$extendParamsArr = $extendParams->getExtendParams();
-
-	// 支付超时，线下扫码交易定义为5分钟
-	$timeExpress = "5m";
+		
+	}else{
+		$subject = $company['company_name']."-扫码";
+		
+		$undiscountableAmount = "0.01";
+		$sellerId = $this->alipay_config['seller_id'];
+		$body = "购买商品共花费".$totalAmount."元";
+		
+		//商户操作员编号，添加此参数可以为商户操作员做销售统计
+		$operatorId = '';
+		
+		// (必填) 商户门店编号
+		$storeId = "wy_".$this->companyId;
+		
+		$goodsDetailList = array();
+		if($goodStr!=''){
+			$goodsArr = json_decode($goodStr);
+			foreach ($goodsArr as $goods){
+				$goodsDetai = new GoodsDetail();
+				$goodsDetai->setGoodsId($goods[0]);
+				$goodsDetai->setGoodsName($goods[1]);
+				$goodsDetai->setPrice($goods[2]);
+				$goodsDetai->setQuantity($goods[3]);
+				//得到商品1明细数组
+				array_push($goodsDetailList,$goodsDetai->getGoodsDetail());
+			}
+		}
 	
-	//第三方应用授权令牌,商户授权系统商开发模式下使用
-	$appAuthToken = ""; //201701BBda91f2d7e6964c37b616687e75858C86
+		// 支付宝的店铺编号
+		$alipayStoreId = "";
+		// 业务扩展参数，目前可添加由支付宝分配的系统商编号(通过setSysServiceProviderId方法)
+		$providerId = ""; //系统商pid,作为系统商返佣数据提取的依据 
+		$extendParams = new ExtendParams();
+		$extendParams->setSysServiceProviderId($providerId);
+		$extendParamsArr = $extendParams->getExtendParams();
 	
-	// 创建请求builder，设置请求参数
-	$barPayRequestBuilder = new AlipayTradePayContentBuilder();
-	$barPayRequestBuilder->setOutTradeNo($outTradeNo);
-	$barPayRequestBuilder->setTotalAmount($totalAmount);
-	$barPayRequestBuilder->setAuthCode($authCode);
-	$barPayRequestBuilder->setTimeExpress($timeExpress);
-	$barPayRequestBuilder->setSubject($subject);
-	$barPayRequestBuilder->setBody($body);
-	$barPayRequestBuilder->setUndiscountableAmount($undiscountableAmount);
-	$barPayRequestBuilder->setExtendParams($extendParamsArr);
-	$barPayRequestBuilder->setGoodsDetailList($goodsDetailList);
-	$barPayRequestBuilder->setStoreId($storeId);
-	$barPayRequestBuilder->setOperatorId($operatorId);
-	$barPayRequestBuilder->setAlipayStoreId($alipayStoreId);
-	
-	$barPayRequestBuilder->setAppAuthToken($appAuthToken);
-	
-	// 调用barPay方法获取当面付应答
-	$barPay = new AlipayTradeService($this->f2fpay_config);
-	$barPayResult = $barPay->barPay($barPayRequestBuilder);
-	
-	$response = $barPayResult->getResponse();
-	switch ($barPayResult->getTradeStatus()) {
-		case "SUCCESS":
-			$transactionId = $response->trade_no;
-			MicroPayModel::update($this->companyId, $outTradeNo, $transactionId, json_encode($response));
-			echo json_encode(array('status'=>true, 'result'=>true, 'trade_no'=>$outTradeNo));
-			break;
-		case "FAILED":
-			echo json_encode(array('status'=>false));
-			break;
-		case "UNKNOWN":
-			echo json_encode(array('status'=>true, 'result'=>false, 'trade_no'=>$outTradeNo));
-			break;
-		default:
-			echo json_encode(array('status'=>false));
-			break;
+		// 支付超时，线下扫码交易定义为5分钟
+		$timeExpress = "5m";
+		
+		//第三方应用授权令牌,商户授权系统商开发模式下使用
+		$appAuthToken = ""; //201701BBda91f2d7e6964c37b616687e75858C86
+		
+		// 创建请求builder，设置请求参数
+		$barPayRequestBuilder = new AlipayTradePayContentBuilder();
+		$barPayRequestBuilder->setOutTradeNo($outTradeNo);
+		$barPayRequestBuilder->setTotalAmount($totalAmount);
+		$barPayRequestBuilder->setAuthCode($authCode);
+		$barPayRequestBuilder->setTimeExpress($timeExpress);
+		$barPayRequestBuilder->setSubject($subject);
+		$barPayRequestBuilder->setBody($body);
+		$barPayRequestBuilder->setUndiscountableAmount($undiscountableAmount);
+		$barPayRequestBuilder->setExtendParams($extendParamsArr);
+		$barPayRequestBuilder->setGoodsDetailList($goodsDetailList);
+		$barPayRequestBuilder->setStoreId($storeId);
+		$barPayRequestBuilder->setOperatorId($operatorId);
+		$barPayRequestBuilder->setAlipayStoreId($alipayStoreId);
+		
+		$barPayRequestBuilder->setAppAuthToken($appAuthToken);
+		
+		// 调用barPay方法获取当面付应答
+		$barPay = new AlipayTradeService($this->f2fpay_config);
+		$barPayResult = $barPay->barPay($barPayRequestBuilder);
+		
+		$response = $barPayResult->getResponse();
+		switch ($barPayResult->getTradeStatus()) {
+			case "SUCCESS":
+				$transactionId = $response->trade_no;
+				MicroPayModel::update($this->companyId, $outTradeNo, $transactionId, json_encode($response));
+				echo json_encode(array('status'=>true, 'result'=>true, 'trade_no'=>$outTradeNo));
+				break;
+			case "FAILED":
+				echo json_encode(array('status'=>false));
+				break;
+			case "UNKNOWN":
+				echo json_encode(array('status'=>true, 'result'=>false, 'trade_no'=>$outTradeNo));
+				break;
+			default:
+				echo json_encode(array('status'=>false));
+				break;
+		}
 	}
 	exit;
 }
