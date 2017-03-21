dff4a1d8fd8115435db60d9cf3b36094c0d6864f
caofang <962460422@qq.com>
2017-03-21 15:16:23 +0800

    cf

--- protected/components/shouqianba/SqbConfig.php
+++ protected/components/shouqianba/SqbConfig.php
@@ -8,7 +8,6 @@
  */
 class SqbConfig{
 	const SQB_DOMAIN = 'https://api.shouqianba.com';
-	const APPID = '2017031400000088';
 	const VENDER_SN = '91800112';
 	const VENDER_KEY = 'cb262ef4113ec96626a47b96efe642e6';
 }
