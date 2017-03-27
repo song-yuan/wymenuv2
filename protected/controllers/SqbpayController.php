61db12d485f8e4d496342cb60830acb1d32654da
caofang <962460422@qq.com>
2017-03-27 11:24:01 +0800

    cf

--- protected/controllers/SqbpayController.php
+++ protected/controllers/SqbpayController.php
@@ -4,12 +4,27 @@ class SqbpayController extends Controller
 {
 	public function actionWappayresultceshi(){
 		$dpid = '0000000027';
+		
+		$now = time();
+		$rand = rand(100,999);
+		$orderId = $now.'-'.$dpid.'-'.$rand;
+		
+		$company = WxCompany::get($dpid);
+		$data = array(
+				'dpid' => $dpid,
+				'pay_type' => 0,
+				'out_trade_no' => $orderId,
+				'total_fee' => '0.01',
+		);
+		$result = MicroPayModel::insert($data);
+		
+		var_dump($result);exit;
 		$site_id = '0000';
 		$is_temp = 1;
 		$orderid = '0000016830';
 		$result = SqbPay::preOrder(array(
 				'dpid'=>$dpid,
-				'client_sn'=>'170320026772',
+				'client_sn'=>$orderId,
 				'total_amount'=>'0.01',
 				'payway'=>'3',
 				'subject'=>'wymenu',
@@ -31,6 +46,15 @@ class SqbpayController extends Controller
 			var_dump($error_code);
 			echo '###';
 			var_dump($error_message);
+		}else{
+			$terminal_sn = Yii::app()->request->getParam('terminal_sn');
+			$sn = Yii::app()->request->getParam('sn');
+			$trade_no = Yii::app()->request->getParam('trade_no');
+			$client_sn = Yii::app()->request->getParam('client_sn');
+			$status = Yii::app()->request->getParam('status');
+			$reflect = Yii::app()->request->getParam('reflect');
+			$sign = Yii::app()->request->getParam('sign');
+			echo $trade_no;
 		}
 		
 		echo '&&&';
