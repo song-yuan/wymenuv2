5853e37a3d8ab81050ebef711b60f0140e718afe
caofang <962460422@qq.com>
2017-03-28 13:39:12 +0800

    cf

--- protected/controllers/SqbpayController.php
+++ protected/controllers/SqbpayController.php
@@ -26,8 +26,8 @@ class SqbpayController extends Controller
 					'payway'=>'3',
 					'subject'=>'wymenu',
 					'operator'=>'admin',
-					'notify_url'=>'http://menu.wymenu.com/wymenuv2/sqbpay/wappayreturn',
-					'return_url'=>'http://menu.wymenu.com/wymenuv2/sqbpay/wappayresult',
+					'notify_url'=>'http://menu.wymenu.com/wymenuv2/sqbpay/wappayreturn/companyId/0000000027',
+					'return_url'=>'http://menu.wymenu.com/wymenuv2/sqbpay/wappayresult/companyId/0000000027',
 			));
 		}else{
 			echo 'error';
@@ -65,37 +65,121 @@ class SqbpayController extends Controller
 		
 	}
 	public function actionWappayreturn(){
-		$is_post_success = Yii::app()->request->getPost('is_success');
+		$companyId = Yii::app()->request->getParam('companyId');
+		
+		//收钱吧异步回调数据接收及解析...
 		$xml = $GLOBALS['HTTP_RAW_POST_DATA'];
-		Helper::writeLog('XXX'.$xml);
-		$sn = $_POST['sn'];
-		Helper::writeLog('###'.$sn);
-		$is_success = Yii::app()->request->getParam('is_success');
-		if($is_success == 'F'){
-			$error_code = Yii::app()->request->getParam('error_code');
-			$error_message = Yii::app()->request->getParam('error_message');
-			$status = Yii::app()->request->getParam('status');
-			$client_sn = Yii::app()->request->getParam('client_sn');
-			$trade_no = Yii::app()->request->getParam('trade_no');
-			
-			$data = '{"from":"returnfalse";"is_success":"'.$is_success.'";"client_sn":"'.$client_sn.'";"trade_no":"'.$trade_no.'";"status":"'.$status.'";"error_message":"'.$error_message.'";}';
-			Helper::writeLog($data);
-		}elseif($is_success == 'T'){
-			$terminal_sn = Yii::app()->request->getParam('terminal_sn');
-			$sn = Yii::app()->request->getParam('sn');
-			$trade_no = Yii::app()->request->getParam('trade_no');
-			$client_sn = Yii::app()->request->getParam('client_sn');
-			$status = Yii::app()->request->getParam('status');
-			$reflect = Yii::app()->request->getParam('reflect');
-			$sign = Yii::app()->request->getParam('sign');
-			
-			$result_code = Yii::app()->request->getParam('result_code');
-			$result_message = Yii::app()->request->getParam('result_message');
-			
-			$data = '{"from":"returntrue";"is_success":"'.$is_success.'";"client_sn":"'.$client_sn.'";"trade_no":"'.$trade_no.'";"status":"'.$status.'";"result_code":"'.$result_code.'";}';
-			Helper::writeLog($data);
+		/*$mxl如下：
+		 * {
+		 * "sn":"7895259485469125",*
+		 * "client_sn":"1490611690-0000000027-409",*
+		 * "client_tsn":"1490611690-0000000027-409",
+		 * "ctime":"1490611690929",*
+		 * "status":"FAIL_CANCELED",*
+		 * "payway":"3",*
+		 * "sub_payway":"3",*
+		 * "order_status":"PAY_CANCELED",*
+		 * "total_amount":"1",*
+		 * "net_amount":"0",*
+		 * "finish_time":"1490611957891",*
+		 * "subject":"wymenu",*
+		 * "store_id":"f35d19cb-a316-499f-b43d-76b882d7caf5",*
+		 * "terminal_id":"1cfcd666-6aa8-42fc-b031-b3eadbf2c9ed",*
+		 * "operator":"admin"*
+		 * }
+		 * 
+		 * */
+		
+		$obj = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
+		$sn = $obj->sn;
+		$client_sn = $obj->client_sn;
+		$client_tsn = $obj->client_tsn;
+		$ctime = $obj->ctime;
+		$status = $obj->status;
+		$payway = $obj->payway;
+		$sub_payway = $obj->sub_payway;
+		$order_status = $obj->order_status;
+		$total_amount = $obj->total_amount;
+		$net_amount = $obj->net_amount;
+		$finish_time = $obj->finish_time;
+		$subject = $obj->subject;
+		$store_id = $obj->store_id;
+		$terminal_id = $obj->terminal_id;
+		$operator = $obj->operator;
+		
+		Helper::writeLog('进入方法'.$sn.';店铺:'.$companyId);
+		
+		$sql = 'select * from nb_notify_wxwap where dpid ='.$companyId.' and sn='.$sn;
+		$notify = Yii::app()->db->createCommand($sql)
+		->queryRow();
+		if(!empty($notify)){
+			if($order_status == $notify['order_status']){
+				Helper::writeLog('相同的'.$sn);
+			}else{
+
+				//像微信公众号支付记录表插入记录...
+				$se = new Sequence ( "notify_wxwap" );
+				$notifyWxwapId = $se->nextval ();
+				$notifyWxwapData = array (
+						'lid' => $notifyWxwapId,
+						'dpid' => $dpid,
+						'create_at' => date ( 'Y-m-d H:i:s', $time ),
+						'update_at' => date ( 'Y-m-d H:i:s', $time ),
+						'sn' => $sn,
+						'client_sn' => $client_sn,
+						'client_tsn' => $client_tsn,
+						'ctime' => $ctime,
+						'status' => $status,
+						'payway' => $payway,
+						'sub_payway' => $sub_payway,
+						'order_status' => $order_status,
+						'total_amount' => $total_amount,
+						'net_amount' => $net_amount,
+						'finish_time' => $finish_time,
+						'subject' => $subject,
+						'store_id' => $store_id,
+						'terminal_id' => $terminal_id,
+						'operator' => $operator,
+				);
+				$result = Yii::app ()->db->createCommand ()->insert ( 'nb_notify_wxwap', $notifyWxwapData );
+				Helper::writeLog('不同的'.$sn);
+			}
 		}else{
-			Helper::writeLog('   ');
+
+			//像微信公众号支付记录表插入记录...
+			$se = new Sequence ( "notify_wxwap" );
+			$notifyWxwapId = $se->nextval ();
+			$notifyWxwapData = array (
+					'lid' => $notifyWxwapId,
+					'dpid' => $dpid,
+					'create_at' => date ( 'Y-m-d H:i:s', $time ),
+					'update_at' => date ( 'Y-m-d H:i:s', $time ),
+					'sn' => $sn,
+					'client_sn' => $client_sn,
+					'client_tsn' => $client_tsn,
+					'ctime' => $ctime,
+					'status' => $status,
+					'payway' => $payway,
+					'sub_payway' => $sub_payway,
+					'order_status' => $order_status,
+					'total_amount' => $total_amount,
+					'net_amount' => $net_amount,
+					'finish_time' => $finish_time,
+					'subject' => $subject,
+					'store_id' => $store_id,
+					'terminal_id' => $terminal_id,
+					'operator' => $operator,
+			);
+			$result = Yii::app ()->db->createCommand ()->insert ( 'nb_notify_wxwap', $notifyWxwapData );
+			Helper::writeLog('第一次'.$sn);
 		}
+		
+		if($order_status == 'PAID'){
+			//订单成功支付...
+		}elseif($order_status == 'PAY_CANCELED'){
+			//订单支付失败...
+		}
+		
+		
 	}
 }
