70e7e39ecaa76527d28382d2c6e87a262d02a68d
dys <daiyongsheng.good@163.com>
2017-04-05 10:37:32 +0800

    dys

--- protected/views/mall/shoplist.php
+++ protected/views/mall/shoplist.php
@@ -38,7 +38,6 @@
 		var init = function() {
 		    geocoder = new qq.maps.Geocoder({
 		        complete : function(result){
-			        alert(JSON.stringify(result));
 		          	$('#current_position').html(result.detail.address);
 		        }
 		    });
@@ -139,7 +138,6 @@
 	    	wx.getLocation({
 			    type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 			    success: function (res) {
-				    alert(JSON.stringify(res));
 			        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
 			        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 			        var speed = res.speed; // 速度，以米/每秒计
