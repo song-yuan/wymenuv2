3a2baf45d6b13f65142789a389c9487c15f02063
caofang <962460422@qq.com>
2017-04-14 15:44:50 +0800

    cf

--- protected/modules/admin/views/purchaseOrder/_form.php
+++ protected/modules/admin/views/purchaseOrder/_form.php
@@ -67,7 +67,7 @@ $organizations = Helper::genOrgCompany($this->companyId);
 		<div class="form-actions fluid">
 			<div class="col-md-offset-3 col-md-9">
 				<button type="submit" class="btn blue"><?php echo yii::t('app','确定');?></button>
-				<a href="<?php echo $this->createUrl('purchaseOrder/index' , array('companyId' => $model->dpid));?>" class="btn default"><?php echo yii::t('app','返回');?></a>                              
+				<!-- <a href="<?php echo $this->createUrl('purchaseOrder/index' , array('companyId' => $model->dpid));?>" class="btn default"><?php echo yii::t('app','返回');?></a>  -->                             
 			</div>
 		</div>
 </div>
