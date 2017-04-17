361855e9c67e661e89d195ce7ce5e2e27982d578
dys <daiyongsheng.good@163.com>
2017-04-17 16:15:22 +0800

    dys

--- protected/views/user/money.php
+++ protected/views/user/money.php
@@ -32,15 +32,15 @@
         <div class="money1 clearfix"><span class="left">我的储值余额(元)</span><a href="<?php echo $this->createUrl('/mall/reCharge',array('companyId'=>$this->companyId));?>"><span class="right">去充值 ></span></a></div>
         <div class="money2"><?php echo number_format($remainMoey,2);?></div>
      </div>
-     <?php if(!empty($recharges)):?>
-     <div class="empty2">充值有优惠</div>
+     <?php if(!empty($comments)):?>
+     <div class="empty2">储值说明</div>
      <div class="bd"> 
         <div class="weui_cells weui_cells_access global_navs">
-           	 <?php foreach ($recharges as $recharge):?>
+           	 <?php foreach ($comments as $comment):?>
              <a class="weui_cell js_cell" href="javascript:;" >
                  <span class="weui_cell_hd "><img src="<?php echo Yii::app()->request->baseUrl;?>/img/wechat_img/icon-wdcz.png" class="icon_nav" alt=""/></span>
                  <div class="weui_cell_bd weui_cell_primary">
-                     <p>充<?php if($recharge['recharge_money']-(int)$recharge['recharge_money'] > 0){ echo $recharge['recharge_money'];}else{echo (int)$recharge['recharge_money'];}?>元,返<?php echo $recharge['recharge_cashback'];?>元</p>
+                     <p><?php echo $comment['content'];?></p>
                   </div>
               </a>
               <?php endforeach;?>
