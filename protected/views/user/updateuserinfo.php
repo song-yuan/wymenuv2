8c322c55467588fb2fbebf6bb740c7783178976f
dys <daiyongsheng.good@163.com>
2017-04-20 14:43:43 +0800

    dys

--- protected/views/user/updateuserinfo.php
+++ protected/views/user/updateuserinfo.php
@@ -275,7 +275,7 @@ $('document').ready(function(){
         theme: 'android-holo-light',
         lang: 'zh',
         display: 'center',
-        startYear: 1970, //开始年份
+        startYear: 1971, //开始年份
     });
     $("#birthday[data = 'true']").click(function(){
         $('#no_revise').css('display','block');
