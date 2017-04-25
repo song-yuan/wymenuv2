0f04f46be00e5698ff7fc7a023050c49f2222a64
dys <daiyongsheng.good@163.com>
2017-04-25 16:26:10 +0800

    dys

--- protected/views/user/updateuserinfo.php
+++ protected/views/user/updateuserinfo.php
@@ -233,7 +233,7 @@ $('document').ready(function(){
 
         var mobile = $('#mobile').val();
 
-        var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/; 
+        var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/; 
         if(!myreg.test(mobile)){ 
             alert('请输入有效的手机号码！'); 
             return false; 
