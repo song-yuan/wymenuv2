10ea2101d0cb050b6a6323242e08ed44348b2135
caofang <962460422@qq.com>
2017-04-25 18:47:32 +0800

    cf

--- protected/controllers/ElemeController.php
+++ protected/controllers/ElemeController.php
@@ -4,9 +4,7 @@ class ElemeController extends Controller
 {
 	public function actionElemeceshi(){
 		$dpid = '0000000027';
-		$xml = $GLOBALS['HTTP_RAW_POST_DATA'];
+		$xml = $GLOBALS;
 		var_dump($xml);
-		$obj = json_decode($xml,true);
-		var_dump($obj);
 	}
 }
