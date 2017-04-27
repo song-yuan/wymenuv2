3131cc8b80c0fde40835d3113992ed3eac84f642
dys <daiyongsheng.good@163.com>
2017-04-27 15:45:14 +0800

    dys

--- protected/components/mall/WxCompany.php
+++ protected/components/mall/WxCompany.php
@@ -43,7 +43,7 @@ class WxCompany
 		if($keyword!=''){
 			$sql .= ' and t.company_name like "%'.$keyword.'%"';
 		}
-		$sql .= ' limit ' . $page*10 . ', 10 order by juli asc';
+		$sql .= '  order by juli asc limit ' . $page*10 . ', 10';
 		$companys = Yii::app()->db->createCommand($sql)
 		->bindValue(':dpid',$dpid)
 		->queryAll();
