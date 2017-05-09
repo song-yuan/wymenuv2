e0f72e6c4c08dc389e9443c403a57a0663165629
dys <daiyongsheng.good@163.com>
2017-05-09 10:37:34 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -17,8 +17,8 @@
 		}
 	}
 ?>
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css?_=20170509">
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=20170509">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css?_=201705091036">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=201705091036">
 <style type="text/css">
 .layui-layer-content img {
 	width: 100%;
