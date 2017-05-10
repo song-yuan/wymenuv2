fa9d8313a166471b09d6a0b91c8c3998651b7080
dys <daiyongsheng.good@163.com>
2017-05-10 15:17:26 +0800

    dys

--- protected/controllers/ElemeController.php
+++ protected/controllers/ElemeController.php
@@ -2,9 +2,7 @@
 
 class ElemeController extends Controller
 {
-	public function actionElemeceshi(){
-		$dpid = '0000000027';
-		$xml = $GLOBALS;
-		var_dump($xml);
+	public function actionIndex(){
+		$this->render('index');
 	}
 }
