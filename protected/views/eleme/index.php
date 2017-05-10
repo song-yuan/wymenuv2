685df639a785a340d5213713933d6f32fc5ae25b
dys <daiyongsheng.good@163.com>
2017-05-10 15:59:46 +0800

    dys

--- protected/views/eleme/index.php
+++ protected/views/eleme/index.php
@@ -1,10 +1,10 @@
 <?php 
 $xmlStr = file_get_contents('php://input');
-if($xmlStr){
-// 	$data = XML::convertToArr($xmlStr);
-	Helper::writeLog($xmlStr);
-}else{
+if(!$xmlStr){
 	echo '{"data":"success"}';
+}else{
+	// 	$data = XML::convertToArr($xmlStr);
+	Helper::writeLog($xmlStr);
 }
 exit;
 ?>
