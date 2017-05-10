682442c74cc69a2a881a5008d37953a50eacff63
dys <daiyongsheng.good@163.com>
2017-05-10 15:48:42 +0800

    dys

--- protected/views/eleme/index.php
+++ protected/views/eleme/index.php
@@ -1,4 +1,10 @@
 <?php 
-echo '{"data":"success"}';
+$xmlStr = file_get_contents('php://input');
+if(isset($xmlStr)&&!empty($xmlStr)){
+	$data = XML::convertToArr($xmlStr);
+	Helper::writeLog(json_encode($data));
+}else{
+	echo '{"data":"success"}';
+}
 exit;
 ?>
