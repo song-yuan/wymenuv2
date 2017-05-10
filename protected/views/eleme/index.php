8d96d71e1777e6c57289f5d2f17d7055f5d2ae38
dys <daiyongsheng.good@163.com>
2017-05-10 15:53:59 +0800

    dys

--- protected/views/eleme/index.php
+++ protected/views/eleme/index.php
@@ -1,8 +1,8 @@
 <?php 
 $xmlStr = file_get_contents('php://input');
 if(!$xmlStr){
-	$data = XML::convertToArr($xmlStr);
-	Helper::writeLog(json_encode($data));
+// 	$data = XML::convertToArr($xmlStr);
+	Helper::writeLog($xmlStr);
 }else{
 	echo '{"data":"success"}';
 }
