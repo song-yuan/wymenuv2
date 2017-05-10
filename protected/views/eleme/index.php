40b9ac3143b6e0aea950033ca100588527285fd3
dys <daiyongsheng.good@163.com>
2017-05-10 16:16:45 +0800

    dys

--- protected/views/eleme/index.php
+++ protected/views/eleme/index.php
@@ -1,10 +1,6 @@
 <?php 
 $xmlStr = file_get_contents('php://input');
-if(!$xmlStr){
-	echo '{"data":"success"}';
-}else{
-	// 	$data = XML::convertToArr($xmlStr);
-	Helper::writeLog($xmlStr);
-}
+Helper::writeLog($xmlStr);
+echo '{"data":"success"}';
 exit;
 ?>
