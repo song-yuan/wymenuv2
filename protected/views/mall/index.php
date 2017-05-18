b1142d2441c20d37f768a41161fe2b4401d2ac30
dys <daiyongsheng.good@163.com>
2017-05-18 19:20:41 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -1,5 +1,9 @@
 <?php
 	$baseUrl = Yii::app()->baseUrl;
+	$title = '微信点单';
+	$link = $this->createAbsoluteUrl('/mall/index',array('companyId'=>$this->companyId,'type'=>$this->type));
+	$desc = '自助点餐,点餐不排队';
+	$imgUrl = Yii::app()->request->getHostInfo().$this->company['logo'];
 	if($this->type==2){
 		$this->setPageTitle('外卖点单');
 	}elseif($this->type==6){
@@ -392,6 +396,33 @@ function getProduct(){
 		},
 	});
 }
+wx.ready(function(){
+	// 分享朋友圈
+	wx.onMenuShareTimeline({
+	    title: '', // 分享标题
+	    link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+	    imgUrl: '', // 分享图标
+	    success: function () { 
+	        // 用户确认分享后执行的回调函数
+	    },
+	    cancel: function () { 
+	        // 用户取消分享后执行的回调函数
+	    }
+	});
+	// 分享朋友
+	wx.onMenuShareAppMessage({
+	    title: '', // 分享标题
+	    desc: '', // 分享描述
+	    link: '', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+	    imgUrl: '', // 分享图标
+	    success: function () { 
+	        // 用户确认分享后执行的回调函数
+	    },
+	    cancel: function () { 
+	        // 用户取消分享后执行的回调函数
+	    }
+	});
+});
 $(document).ready(function(){ 
 	var i = 0;
 	var j = 0;
