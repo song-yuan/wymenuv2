d4c3b2961cff428e57587e1768bc961e6c249413
dys <daiyongsheng.good@163.com>
2017-06-26 15:01:35 +0800

    dys

--- protected/components/dataAppSync/DataSyncTables.php
+++ protected/components/dataAppSync/DataSyncTables.php
@@ -124,8 +124,8 @@ class DataSyncTables
         		"update_at TIMESTAMP NOT NULL default (datetime('now', 'localtime')),".
         		"jobid int(10) NOT NULL DEFAULT '0',".
 	        	"pad_code varchar(50) NOT NULL,".
-				"pad_sales_type varchar(2) NOT NULL,".
-        		"screen_type varchar(2) NOT NULL,".
+				"pad_sales_type varchar(2) NOT NULL DEFAULT '0',".
+        		"screen_type varchar(2) NOT NULL DEFAULT '0',".
         		"item_count varchar(2) NOT NULL DEFAULT '4',".
 				"pad_type varchar(2) NOT NULL,".
 				"pad_ip varchar(20) NOT NULL,".
