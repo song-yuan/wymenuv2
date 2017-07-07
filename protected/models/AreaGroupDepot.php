7cd5b2d7e61f7d4d3d1aa804345c6e20d1e7f480
caofang <962460422@qq.com>
2017-07-07 17:27:51 +0800

    cf

---
+++ protected/models/AreaGroupDepot.php
@@ -0,0 +1,118 @@
+<?php
+
+/**
+ * This is the model class for table "nb_area_group_depot".
+ *
+ * The followings are the available columns in table 'nb_area_group_depot':
+ * @property string $lid
+ * @property string $dpid
+ * @property string $create_at
+ * @property string $update_at
+ * @property string $area_group_id
+ * @property string $depot_id
+ * @property string $is_selected
+ * @property string $delete_flag
+ * @property string $is_sync
+ */
+class AreaGroupDepot extends CActiveRecord
+{
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName()
+    {
+        return 'nb_area_group_depot';
+    }
+
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules()
+    {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('update_at, area_group_id, depot_id', 'required'),
+            array('lid, dpid, area_group_id, depot_id', 'length', 'max'=>10),
+            array('is_selected, delete_flag', 'length', 'max'=>2),
+            array('is_sync', 'length', 'max'=>50),
+            array('create_at', 'safe'),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('lid, dpid, create_at, update_at, area_group_id, depot_id, is_selected, delete_flag, is_sync', 'safe', 'on'=>'search'),
+        );
+    }
+
+    /**
+     * @return array relational rules.
+     */
+    public function relations()
+    {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+        );
+    }
+
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels()
+    {
+        return array(
+            'lid' => 'Lid',
+            'dpid' => '店铺id',
+            'create_at' => 'Create At',
+            'update_at' => '最近一次更新时间',
+            'area_group_id' => '地区分组表的lid',
+            'depot_id' => '仓库表的lid',
+            'is_selected' => '是否是默认仓库,1默认,0非默认',
+            'delete_flag' => '0表示存在，1表示删除',
+            'is_sync' => '同步标志',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search()
+    {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria=new CDbCriteria;
+
+        $criteria->compare('lid',$this->lid,true);
+        $criteria->compare('dpid',$this->dpid,true);
+        $criteria->compare('create_at',$this->create_at,true);
+        $criteria->compare('update_at',$this->update_at,true);
+        $criteria->compare('area_group_id',$this->area_group_id,true);
+        $criteria->compare('depot_id',$this->depot_id,true);
+        $criteria->compare('is_selected',$this->is_selected,true);
+        $criteria->compare('delete_flag',$this->delete_flag,true);
+        $criteria->compare('is_sync',$this->is_sync,true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria'=>$criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return AreaGroupDepot the static model class
+     */
+    public static function model($className=__CLASS__)
+    {
+        return parent::model($className);
+    }
+}
