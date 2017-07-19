8fef4044249ada7e11a5b43eab9ed377fa552958
dys <daiyongsheng.good@163.com>
2017-07-19 09:32:52 +0800

    dys

---
+++ protected/modules/admin/views/eleme/dpsq.php
@@ -0,0 +1,3 @@
+<div class="page-content">
+	<iframe frameborder="0" width= 100% height= 700px src="https://open-api-sandbox.shop.ele.me/authorize?response_type=code&client_id=57MzkUXe1y&redirect_uri=<?php echo $url;?>&state=<?php echo $companyId;?>&scope=all"></iframe>
+</div>
