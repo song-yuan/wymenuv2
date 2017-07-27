3c220f69311200c60324b1c9455a4ea8d5ba6a3e
dys <daiyongsheng.good@163.com>
2017-07-27 17:20:20 +0800

    dys

--- protected/controllers/MallController.php
+++ protected/controllers/MallController.php
@@ -64,7 +64,7 @@ class MallController extends Controller
 			}
 		}else{
 			//pc 浏览
-			$userId = 2122;
+			$userId = 2146;
 			$this->brandUser = WxBrandUser::get($userId, $this->companyId);
 			$userId = $this->brandUser['lid'];
 			$userDpid = $this->brandUser['dpid'];
