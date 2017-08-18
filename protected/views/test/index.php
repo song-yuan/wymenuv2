6fdf014e407dad6813534445f81ab9836e8d1879
dys <daiyongsheng.good@163.com>
2017-08-18 17:33:10 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -5,7 +5,7 @@
 		$sql = 'select * from nb_pad_setting_detail where pad_setting_id='.$result['lid'].' and dpid='.$result['dpid'].' order by lid desc limit 1';
 		$res = Yii::app ()->db->createCommand ( $sql )->queryRow();
 		if($res){
-			$sql = 'update nb_pad_setting_status set used_at="'.$res['create_at'].'" where dpid='.$result['dpid'].' and pad_setting_id='.$result['lid'];
+			$sql = 'update nb_pad_setting_status set use_status=1, used_at="'.$res['create_at'].'" where dpid='.$result['dpid'].' and pad_setting_id='.$result['lid'];
 			Yii::app ()->db->createCommand ( $sql )->execute();
 		}
 	}
