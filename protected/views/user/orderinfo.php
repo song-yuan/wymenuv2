20f0f8e4f9bbc77589a1b6bb97706d49e0cd8870
dys <daiyongsheng.good@163.com>
2017-09-29 10:31:08 +0800

    dys

--- protected/views/user/orderinfo.php
+++ protected/views/user/orderinfo.php
@@ -12,7 +12,11 @@
 
 
 <div class="order-title">取餐号: <?php echo substr($order['lid'], -4);?></div>
-<div class="order-site"><div class="lt"><?php if($order['order_type']==1):?>桌号:<?php if($siteType){echo $siteType['name'];}?><?php echo $site['serial'];?><?php else:?>订单状态<?php endif;?></div><div class="rt"><?php if($order['order_status'] < 3) echo '<button class="payOrder specialbttn bttn_orange" status="'.$order['order_status'].'">待支付</button>';elseif($order['order_status'] == 3) echo '已支付';elseif ($order['order_status']==7) echo '已取消';else echo '已完成';?></div><div class="clear"></div></div>
+<div class="order-site">
+	<div class="lt"><?php if($order['order_type']==1):?>桌号:<?php if($siteType){echo $siteType['name'];}?><?php echo $site['serial'];?><?php else:?>订单状态<?php endif;?></div>
+	<div class="rt"><?php if($order['order_status'] < 3) echo '<button class="payOrder specialbttn bttn_orange" status="'.$order['order_status'].'">待支付</button>';elseif($order['order_status'] == 3) echo '<span class="font_org">已支付</span>';elseif ($order['order_status']==7) echo '<span class="font_l">已取消</span>';else echo '<span class="font_org">已完成</span>';?></div>
+	<div class="clear"></div>
+</div>
 <?php if($address):?>
 	<?php if($order['order_type']==2):?>
 	<div class="address">
