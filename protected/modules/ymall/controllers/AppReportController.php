55c30095c7801735363cb6b354432fc9a12f5d19
dys <daiyongsheng.good@163.com>
2017-10-16 18:27:27 +0800

    dys

--- protected/modules/ymall/controllers/AppReportController.php
+++ protected/modules/ymall/controllers/AppReportController.php
@@ -10,6 +10,7 @@ class AppReportController extends Controller
 	{
 		$companyId = Yii::app()->request->getParam('companyId');
 		$this->companyId = $companyId;
+		var_dump($this->companyId);exit;
 		$this->company = WxCompany::get($this->companyId);
 	}
 	public function beforeAction($actin){
