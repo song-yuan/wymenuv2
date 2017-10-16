ad298b3ad79ef8400d6b6b050557c2be797fe723
dys <daiyongsheng.good@163.com>
2017-10-16 18:19:23 +0800

    dys

--- protected/modules/ymall/views/appReport/adminlist.php
+++ protected/modules/ymall/views/appReport/adminlist.php
@@ -1,7 +1,7 @@
 <ul class="mui-table-view">
 	<?php foreach ($admindpids as $dp):?>
     <li class="mui-table-view-cell mui-media">
-        <a href="javascript:;">
+        <a href="<?php echo $this->createUrl('appReport/index',array('coompanyId'=>$dp['lid']));?>">
             <img class="mui-media-object mui-pull-left" src="<?php echo $dp['logo'];?>">
             <div class="mui-media-body">
          		<?php echo $dp['company_name']?>
