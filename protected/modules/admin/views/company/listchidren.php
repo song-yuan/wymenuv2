9c1793b9216c39f04e107e378dc4020affd663be
dys <daiyongsheng.good@163.com>
2017-10-19 11:02:00 +0800

    dys

--- protected/modules/admin/views/company/listchidren.php
+++ protected/modules/admin/views/company/listchidren.php
@@ -94,6 +94,8 @@
 					</div>
 				</div>
 				<div class="portlet-body" id="table-manage">
+				<div class="dataTables_wrapper form-inline">
+				<div class="table-responsive">
 					<table class="table table-striped table-bordered table-hover" id="sample_1">
 						<thead>
 							<tr>
@@ -162,6 +164,7 @@
 						<?php endif;?>
 						</tbody>
 					</table>
+					</div>
 						<?php if($pages->getItemCount()):?>
 						<div class="row">
 							<div class="col-md-5 col-sm-12">
@@ -193,6 +196,7 @@
 							</div>
 						</div>
 						<?php endif;?>
+						</div>
 				</div>
 			</div>
 			<!-- END EXAMPLE TABLE PORTLET-->
