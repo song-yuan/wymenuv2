c81dfcd97a15b033aea3824544e85b4b3835aeb9
dys <daiyongsheng.good@163.com>
2017-11-20 15:35:34 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -2,15 +2,17 @@
 $dpid = Yii::app()->request->getParam('companyId');
 $orderId = Yii::app()->request->getParam('orderId');
 ?>
-<img src=""/>
+<img src="" style="width:80%"/>
 <a href="javascript:;">生成二维码</a>
 <script type="text/javascript">
 $('a').click(function(){
 	$.ajax({
 			url:'<?php echo $this->createUrl('sqbpay/precreate',array('companyId'=>$dpid,'orderId'=>$orderId));?>',
-			sucess:function(data){
-				alert(data);
-				alert(data.result);
+			success:function(data){
+				if(data.status){
+					var qrcodeUrl = data.result.biz_response.data.qr_code_image_url;
+					$('img').attr('src',qrcodeUrl);
+				}
 			},
 			dataType:'json'
 		});
