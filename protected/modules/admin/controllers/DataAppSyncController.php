07de157115d25141350704af1ba43adc9f60c048
dys <daiyongsheng.good@163.com>
2017-11-28 16:37:33 +0800

    dys

--- protected/modules/admin/controllers/DataAppSyncController.php
+++ protected/modules/admin/controllers/DataAppSyncController.php
@@ -341,11 +341,11 @@ class DataAppSyncController extends Controller
 	 *
 	 */
 	public function actionOrderToXst(){
-		$yesterDateBegain = date('Y-m-d 00:00:00',strtotime("-1 day"));
-		$yesterDateEnd = date('Y-m-d 23:59:59',strtotime("-1 day"));
+		$yesterDateBegain = date('Y-m-d 00:00:00',strtotime("-2 day"));
+		$yesterDateEnd = date('Y-m-d 23:59:59',strtotime("-2 day"));
 		$platforms = ThirdPlatform::getXstInfo();
 		foreach ($platforms as $platform){
-			$sql = 'Select lid,dpid,create_at,order_type,should_total from nb_order where dpid='.$platform['dpid'].' and create_at >= "'.$yesterDateBegain.'" and create_at <= "'.$yesterDateEnd.'" and order_status in (3,4,8)';
+			$sql = 'Select t.lid,t.dpid,t.create_at,t.order_type,t.should_total,t1.paytype,t2.is_retreat from nb_order t left join nb_order_pay t1 on t.dpid=t1.dpid and t.lid=t1.order_id left join nb_order_product t2 on t.dpid=t2.dpid and t.lid=t2.order_id where t.dpid='.$platform['dpid'].' and t.create_at >= "'.$yesterDateBegain.'" and t.create_at <= "'.$yesterDateEnd.'" and t.order_status in (3,4,8) and t1.paytype!=11 group by lid,dpid';
 			$orders = Yii::app()->db->createCommand($sql)->queryAll();
 			foreach ($orders as $order){
 				if($order['order_type']==0){
@@ -353,12 +353,22 @@ class DataAppSyncController extends Controller
 				}else{
 					$sourcetype = '网络配餐';
 				}
+				if($order['paytype']==0){
+					$payment = '现金';
+				}else{
+					$payment = '非现金';
+				}
+				if($order['is_retreat']==0){
+					$transtype = '销售';
+				}else{
+					$transtype = '退货';
+				}
 				$order = array(
 						'lid'=>$order['lid'],
 						'create_at'=>$order['create_at'],
 						'total'=>$order['should_total'],
-						'payment'=>'现金',
-						'transtype'=>'销售',
+						'payment'=>$payment,
+						'transtype'=>$transtype,
 						'sourcetype'=>$sourcetype,
 				);
 				ThirdPlatform::xst($order,$platform);
