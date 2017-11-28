c102278781aae8fdc8a33aed49b5f52efdd8bd8f
dys <daiyongsheng.good@163.com>
2017-11-28 17:08:07 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,22 +1,4 @@
 <?php
-$dpid = Yii::app()->request->getParam('companyId');
-$orderId = Yii::app()->request->getParam('orderId');
+Helper::writeLog('crontab');
 ?>
-<img src="" style="width:80%"/>
-<br/>
-<a href="javascript:;">生成二维码</a>
-<script type="text/javascript">
-$('a').click(function(){
-	$.ajax({
-			url:'<?php echo $this->createUrl('sqbpay/precreate',array('companyId'=>$dpid,'orderId'=>$orderId));?>',
-			success:function(data){
-				if(data.status){
-					var qrcodeUrl = data.result.qr_code_image_url;
-					$('img').attr('src',qrcodeUrl);
-				}
-			},
-			dataType:'json'
-		});
-});
-</script>
 
