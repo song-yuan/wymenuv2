298de4960981d0d90323e4b1c2803ee976cc7302
caofang <962460422@qq.com>
2017-12-06 17:03:09 +0800

    cf

--- protected/modules/ymall/controllers/ProductController.php
+++ protected/modules/ymall/controllers/ProductController.php
@@ -1,7 +1,6 @@
 <?php
 class ProductController extends BaseYmallController
 {
-
 	/**
 	 *
 	 * 获取分类商品
@@ -10,13 +9,14 @@ class ProductController extends BaseYmallController
 	{
 		$db = Yii::app()->db;
 		//实时库存
-		$sqls = 'SELECT `t`.material_name,SUM(stock.stock) AS stock,safe.safe_stock,safe.max_stock,unit.unit_name FROM `nb_product_material` `t`
-			LEFT JOIN `nb_product_material_stock` `stock` ON (t.lid=stock.material_id and stock.dpid=t.dpid AND stock.delete_flag=0)
+		$sqls = 'select DISTINCT(`t`.material_name),stock.stock,safe.safe_stock,safe.max_stock,unit.unit_name,mc.category_name,t.category_id FROM `nb_product_material` `t`
+			LEFT JOIN ( select material_id,dpid,SUM(stock) AS stock from `nb_product_material_stock` where dpid ='.$this->companyId.' and delete_flag=0 GROUP BY material_id) `stock` ON (t.lid=stock.material_id and stock.dpid=t.dpid )
 			LEFT JOIN (select a.* from nb_product_material_safe a where a.dpid = '.$this->companyId.' AND  UNIX_TIMESTAMP(a.create_at) in(select max(UNIX_TIMESTAMP(b.create_at)) from nb_product_material_safe b where b.dpid=a.dpid and b.material_id=a.material_id) ) `safe` ON (t.lid=safe.material_id and safe.dpid=t.dpid AND safe.delete_flag=0)
-			LEFT JOIN (SELECT u.unit_name,r.sales_unit_id FROM nb_material_unit u LEFT JOIN nb_material_unit_ratio r on(u.lid=r.sales_unit_id AND r.delete_flag=0 ) where u.delete_flag=0) `unit` ON (t.sales_unit_id=unit.sales_unit_id )
-			WHERE (t.delete_flag=0 and t.dpid='.$this->companyId.') GROUP BY t.lid';
+			LEFT JOIN (SELECT u.unit_name,r.sales_unit_id FROM nb_material_unit u LEFT JOIN nb_material_unit_ratio r on(u.lid=r.sales_unit_id AND r.delete_flag=0 ) where u.delete_flag=0) `unit` ON (t.sales_unit_id=unit.sales_unit_id )'
+			.' left join nb_material_category mc on (mc.lid=t.category_id )'
+			.' WHERE (t.delete_flag=0 and t.dpid='.$this->companyId.')';
 		$stocks = $db->createCommand($sqls)->queryAll();
-		
+		// p($stocks);
 		$company = Company::model()->find('dpid=:dpid and delete_flag=0',array(':dpid'=>$this->companyId));
 		$company_name = $company->company_name;
 		$x = mb_strlen($company_name);
