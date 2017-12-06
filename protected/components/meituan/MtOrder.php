8879eec8780c0febd7462079509cb39e4be43546
dys <daiyongsheng.good@163.com>
2017-12-06 11:26:59 +0800

    dys

--- protected/components/meituan/MtOrder.php
+++ protected/components/meituan/MtOrder.php
@@ -117,10 +117,10 @@ class MtOrder
 		return '{ "data": "ERROR"}';
 	}
 	public static function Jcbd($data){
+		Helper::writeLog('jcbd:'.$data);
 		if(empty($data)){
 			return '200';
 		}
-		Helper::writeLog('jcbd:'.$data);
 		$resArr = MtUnit::dealData($data);
 		$ePoiId = $resArr['ePoiId'];
 		$timestamp = $resArr['timestamp'];
