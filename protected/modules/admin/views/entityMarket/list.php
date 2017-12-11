59375e4acdbc2dcb66a5b89d6a8cf1a21f5c43af
caofang <962460422@qq.com>
2017-12-11 16:02:46 +0800

    cf

--- protected/modules/admin/views/entityMarket/list.php
+++ protected/modules/admin/views/entityMarket/list.php
@@ -1,94 +1,60 @@
-<style>
-
-.portlet-body {
-margin-top: 30px;
-}
-
-.col-xs-6,col-sm-3{
- text-align: center;
- margin-bottom: 50px;
-}
-
-.col-xs-6 a,col-sm-3 a,
-.col-xs-6 a:hover,col-sm-3 a:hover,
-.col-xs-6 a:visited,col-sm-3 a:visited
-{
-    text-decoration: none;
-   
-    display:block;
-    
-}
-
-.col-xs-6 .list_picture,col-sm-3 .list_picture {
-    font-size: 45px;
-    color:#3385ff;
-    
-}
-.col-xs-6 .list_text,col-sm-3 .list_text{
-    color:black; 
-    font-size: 25px;
-    margin-top: 10px;
-}
-
-  
-</style>
 <div class="page-content">
 <?php $this->widget('application.modules.admin.components.widgets.PageHeader', array('breadcrumbs'=>array(array('word'=>yii::t('app','营销活动'),'url'=>''))));?>
    
 
-                <div class="portlet purple box">
-					<div class="portlet-title">
-						<div class="caption"><i class=" fa fa-gift"></i>营销活动</div>
-					</div> 
-                    <div class="portlet-body clearfix" >
-                        <div class="panel_body">
-                             <div class="row">
-                                    <div class="col-sm-3 col-xs-6">
-                                        <a href="<?php ?>">
-                                            <span class="fa fa-cog list_picture"></span>
-                                            <div class="list_text">设置</div>
-                                        </a> 
-                                    </div>
-                                    <div class="col-xs-6  col-sm-3">
-                                        <a href="<?php echo $this->createUrl('discount/index',array('companyId' => $this->companyId))?>">
-                                            <span class="fa fa-pencil-square-o list_picture"></span>
-                                            <div class="list_text">POS折扣模板</div>
-                                        </a> 
-                                    </div>
-                                     <div class="col-xs-6  col-sm-3">
-                                        <a href="<?php echo $this->createUrl('normalpromotion/index',array('companyId' => $this->companyId))?>">
-                                            <span class="fa fa-dollar list_picture"></span>
-                                            <div class="list_text">普通优惠</div>
-                                        </a> 
-                                     </div>
-                                    <div class="col-xs-6  col-sm-3">
-                                        <a href="<?php echo $this->createUrl('fullSentPromotion/index',array('companyId' => $this->companyId))?>">
-                                            <span class="fa fa-gift list_picture"></span>
-                                            <div class="list_text">满送优惠</div>
-                                        </a> 
-                                    </div>
-                                    <div class="col-xs-6  col-sm-3">
-                                        <a href="<?php echo $this->createUrl('fullMinusPromotion/index',array('companyId' => $this->companyId))?>">
-                                            <span class="fa fa-cut list_picture"></span>
-                                            <div class="list_text">满减优惠</div>
-                                        </a> 
-                                    </div>
-                                    <div class="col-xs-6  col-sm-3">
-                                        <a href="<?php echo $this->createUrl('buysentpromotion/index',array('companyId' => $this->companyId))?>">
-                                            <span class="fa fa-leaf list_picture"></span>
-                                            <div class="list_text">买送</div>
-                                        </a> 
-                                    </div>
-                                    <div class="col-xs-6  col-sm-3">
-                                        <a href="">
-                                            <span class="fa fa-pagelines list_picture"></span>
-                                            <div class="list_text">买减</div>
-                                        </a> 
-                                    </div>
-                            </div> 
-                                  
-                    </div> 
-                </div>
-             
+        <div class="portlet purple box">
+			<div class="portlet-title">
+				<div class="caption"><i class=" fa fa-gift"></i>营销活动</div>
+			</div> 
+            <div class="portlet-body clearfix" >
+                <div class="panel_body row">
+                <p>基础设置</p>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="">
+                            <div class="list_big">设置</div>
+                            <div class="list_small"></div>
+                        </a> 
+                    </div>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="<?php echo $this->createUrl('discount/index',array('companyId' => $this->companyId))?>">
+                            <div class="list_big">POS折扣模板</div>
+                            <div class="list_small">设置POS端的结账打折</div>
+                        </a> 
+                    </div>
+                 </div>
+                <div class="panel_body row">
+                    <p>优惠活动</p>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="<?php echo $this->createUrl('normalpromotion/index',array('companyId' => $this->companyId))?>">
+                            <div class="list_big">普通优惠</div>
+                            <div class="list_small">设置商品打折活动</div>
+                        </a> 
+                    </div>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="<?php echo $this->createUrl('fullSentPromotion/index',array('companyId' => $this->companyId))?>">
+                            <div class="list_big">满送优惠</div>
+                            <div class="list_small">设置满多少送什么活动</div>
+                        </a> 
+                    </div>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="<?php echo $this->createUrl('fullMinusPromotion/index',array('companyId' => $this->companyId))?>">
+                            <div class="list_big">满减优惠</div>
+                            <div class="list_small">设置满多少减多少活动</div>
+                        </a> 
+                    </div>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="<?php echo $this->createUrl('buysentpromotion/index',array('companyId' => $this->companyId))?>">
+                            <div class="list_big">买送</div>
+                            <div class="list_small">设置买什么送什么活动</div>
+                        </a> 
+                    </div>
+                    <div style="height: 80px;" class="list col-sm-3 col-xs-12">
+                        <a href="">
+                            <div class="list_big">买减</div>
+                            <div class="list_small"></div>
+                        </a> 
+                    </div>
+                </div>   
+        </div>             
     </div>
 </div>
