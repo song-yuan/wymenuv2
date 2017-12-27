951dbbc0ea01b26b3ada01dcd97774f288e9f186
dys <daiyongsheng.good@163.com>
2017-12-27 18:07:05 +0800

    dys

--- protected/modules/admin/views/eleme/dpsq.php
+++ protected/modules/admin/views/eleme/dpsq.php
@@ -18,25 +18,18 @@
 		</div>
 		<!-- /.modal-dialog -->
 	</div>
- <?php $this->widget('application.modules.admin.components.widgets.PageHeader', array('breadcrumbs'=>array(array('word'=>yii::t('app','外卖设置'),'url'=>$this->createUrl('waimai/list' , array('companyId'=>$this->companyId,'type'=>0,))),array('word'=>yii::t('app','饿了么外卖'),'url'=>$this->createUrl('eleme/index' , array('companyId'=>$this->companyId))),array('word'=>yii::t('app','店铺授权'),'url'=>$this->createUrl('eleme/dpsq' , array('companyId'=>$this->companyId,'type'=>0)))),'back'=>array('word'=>yii::t('app','返回'),'url'=>$this->createUrl('waimai/list' , array('companyId' => $this->companyId,'type' => '0')))));?>
-	<?php if(!empty($token)){?>
-	<table cellpadding="0" cellspacing="0" width="100%" border="1" style="text-align: center;">
-		<tr>
-			<td>店铺ID</td>
-			<td>绑定时间</td>
-			<td>更新时间</td>
-			<td>授权有效时间</td>
-			<td>操作</td>
-		</tr>
-		<tr>
-			<td><?php echo $token['dpid'];?></td>
-			<td><?php echo $token['create_at'];?></td>
-			<td><?php echo $token['update_at'];?></td>
-			<td><?php echo date("Y-m-d H:i:s",$token['expires_in']);?></td>
-			<td><a href="<?php echo $this->createUrl('eleme/dpsq',array('companyId'=>$this->companyId));?>/type/1">解绑</a></td>
-		</tr>
-	</table>
-	<?php }else{?>
-	<iframe frameborder="0" width= 100% height= 500px src="<?php echo $sqUrl;?>?response_type=code&client_id=<?php echo $clientId;?>&redirect_uri=<?php echo $url;?>&state=<?php echo $companyId;?>&scope=all"></iframe>
-	<?php }?>
+ <?php $this->widget('application.modules.admin.components.widgets.PageHeader', array('breadcrumbs'=>array(array('word'=>yii::t('app','外卖设置'),'url'=>$this->createUrl('waimai/list' , array('companyId'=>$this->companyId,'type'=>0,))),array('word'=>yii::t('app','饿了么外卖'),'url'=>$this->createUrl('eleme/index' , array('companyId'=>$this->companyId))),array('word'=>yii::t('app','店铺授权'),'url'=>$this->createUrl('eleme/dpsq' , array('companyId'=>$this->companyId,'type'=>0)))),'back'=>array('word'=>yii::t('app','返回'),'url'=>$this->createUrl('eleme/index' , array('companyId' => $this->companyId)))));?>
+
+<div class="row">
+		<div class="col-md-12">
+			<div class="portlet purple box">
+				<div class="portlet-title">
+					<div class="caption"><i class="fa fa-cogs"></i><?php echo yii::t('app','饿了么授权');?></div>
+				</div>
+				<div class="portlet-body">
+					<iframe frameborder="0" width= 100% height= 500px src="<?php echo $sqUrl;?>?response_type=code&client_id=<?php echo $clientId;?>&redirect_uri=<?php echo $url;?>&state=<?php echo $companyId;?>&scope=all"></iframe>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
