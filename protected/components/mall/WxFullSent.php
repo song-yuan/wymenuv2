1f9a8704b425ed178782ad0cc2111b6488667552
dys <daiyongsheng.good@163.com>
2017-12-28 10:03:45 +0800

    dys

--- protected/components/mall/WxFullSent.php
+++ protected/components/mall/WxFullSent.php
@@ -16,19 +16,14 @@ class WxFullSent
 	 * 
 	 */
 	public static function getAllFullsent($dpid,$type){
+		$fullsentArr = array();
 		$time = date('Y-m-d H:i:s',time());
-		$sql = 'select * from nb_full_sent where dpid=:dpid and full_type=:full_type and begin_time < "'.$time.'" and end_time > "'.$time.'" and delete_flag=0 order by full_cost asc';
-		$fullsent = Yii::app()->db->createCommand($sql)
-				  ->bindValue(':dpid',$dpid)
-				  ->bindValue(':full_type',$type)
-				  ->queryAll();
+		$sql = 'select * from nb_full_sent where dpid='.$dpid.' and full_type='.$type.' and begin_time < "'.$time.'" and end_time > "'.$time.'" and delete_flag=0 order by full_cost asc';
+		$fullsent = Yii::app()->db->createCommand($sql)->queryAll();
 		if($type==0){
 			foreach ($fullsent as $key=>$sent){
-				$sql = 'select t.* from nb_full_sent_detail t,nb_product t1 where t.dpid=t1.dpid and t.product_id=t1.lid and t.dpid=:dpid and t.full_sent_id=:fullsentId and t.delete_flag=0 and t1.is_show=1 and t1.is_show_wx=1 and t1.delete_flag=0';
-				$fullsentDetail = Yii::app()->db->createCommand($sql)
-					->bindValue(':dpid',$dpid)
-					->bindValue(':fullsentId',$sent['lid'])
-					->queryAll();
+				$sql = 'select t.*,t1.product_name,t1.original_price,t1.member_price from nb_full_sent_detail t,nb_product t1 where t.dpid=t1.dpid and t.product_id=t1.lid and t.dpid='.$dpid.' and t.full_sent_id='.$sent['lid'].' and t.delete_flag=0 and t1.is_show=1 and t1.is_show_wx=1 and t1.delete_flag=0';
+				$fullsentDetail = Yii::app()->db->createCommand($sql)->queryAll();
 				if(empty($fullsentDetail)){
 					unset($fullsent[$key]);
 					continue;
@@ -51,5 +46,42 @@ class WxFullSent
 					->bindValue(':dpid',$dpid)
 					->queryRow();
 		return $fullsent;
+	}
+	/**
+	 *
+	 * 满减满送活动列表 满足条件 金额 从小到大
+	 * $type 0 满送 1 满减
+	 * $dpid 
+	 * price 订单价格
+	 *
+	 */
+	public static function getFullsentActive($dpid,$price,$type){
+		$fullsentActive = array();
+		if($type==0){
+			// 获取满送活动
+			$fullSents = self::getAllFullsent($dpid, 0); 
+			if(!empty($fullSents)){
+				foreach ($fullSents as $sent){
+					$fullcost = $sent['full_cost'];
+					if($price <= $fullcost){
+						break;
+					}
+					$fullsentActive = $sent;
+				}
+			}
+		}else{
+			// 获取满减活动
+			$fullminus = self::getAllFullsent($dpid, 1);
+			if(!empty($fullminus)){
+				foreach ($fullminus as $minus){
+					$fullcost = $minus['full_cost'];
+					if($price <= $fullcost){
+						break;
+					}
+					$fullsentActive = $minus;
+				}
+			}
+		}
+		return $fullsentActive;
 	}
 }
