69e689a7e2fcbdac06dc38c2d3cd2db4a7b1f7c2
caofang <962460422@qq.com>
2018-01-05 15:54:24 +0800

    cf

--- protected/modules/admin/views/normalpromotion/_form.php
+++ protected/modules/admin/views/normalpromotion/_form.php
@@ -126,7 +126,7 @@
 		                <div class="form-group">
 		                        <?php echo $form->label($model, yii::t('app','是否生效'),array('class' => 'col-md-3 control-label'));?>
 		                        <div class="col-md-4">
-		                                <?php echo $form->dropDownList($model, 'is_available', array('0' => yii::t('app','无效') , '1' => yii::t('app','只显示在POS机端'), '2' => yii::t('app','只显示在微信端'), '4' => yii::t('app','只显示在微信堂食'), '5' => yii::t('app','只显示在微信外卖'),'3' => yii::t('app','POS机及微信端都显示')) , array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('is_available'),'disabled'=>$a));?>
+		                                <?php echo $form->checkBoxList($model, 'is_available', array( '1' => yii::t('app','显示在POS机端'), '2' => yii::t('app','显示在微信堂食端'), '3' => yii::t('app','显示在微信外卖')) , array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('is_available'),'disabled'=>$a));?>
 		                                <?php echo $form->error($model, 'is_available' )?>
 		                        </div>
 		                </div><!-- 活动是否生效 -->
@@ -134,7 +134,7 @@
 		                <div class="form-group">
 		                        <?php echo $form->label($model, yii::t('app','是否生效'),array('class' => 'col-md-3 control-label'));?>
 		                        <div class="col-md-4">
-		                                <?php echo $form->dropDownList($model, 'is_available', array('0' => yii::t('app','无效') , '1' => yii::t('app','生效'),) , array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('is_available'),'disabled'=>$a));?>
+		                                <?php echo $form->checkBoxList($model, 'is_available', array( '1' => yii::t('app','生效'),) , array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('is_available'),'disabled'=>$a));?>
 		                                <?php echo $form->error($model, 'is_available' )?>
 		                        </div>
 		                </div><!-- 活动是否生效 -->
@@ -142,15 +142,7 @@
 						<div class="form-group">
                                 <label class="control-label col-md-3"><?php echo yii::t('app','活动有效期限');?></label>
                                 <div class="col-md-4">
-                                        <!-- <div class="input-group date form_datetime" data-date="2012-12-21T15:25:00Z">                                       
-                                                <input type="text" size="16" readonly class="form-control">
-                                                <span class="input-group-btn">
-                                                <button class="btn default date-reset" type="button"><i class="fa fa-times"></i></button>
-                                                </span>
-                                                <span class="input-group-btn">
-                                                <button class="btn default date-set" type="button"><i class="fa fa-calendar"></i></button>
-                                                </span>
-                                        </div> -->
+                                       
                                          <div class="input-group input-large date-picker input-daterange" data-date="10/11/2012" data-date-format="mm/dd/yyyy">
                                                  <?php echo $form->textField($model,'begin_time',array('class' => 'form-control ui_timepicker','readonly'=>'true','style'=>'width:160px;','placeholder'=>$model->getAttributeLabel('begin_time'),'disabled'=>$a)); ?>
                                                  <span class="input-group-addon"> ~ </span>
@@ -269,22 +261,6 @@
         });
     
 }); 
-		 
-//	 	 function aa() {
-//	 		 //alert(222);
-//	          $(".chk").click(function() {
-//	              var aa = document.getElementsByName("chk");
-//	              var ss = "";
-//	              alert(22);
-//	              for (var i = 0; i < aa.length; i++) {
-//	                  if (aa[i].checked) {
-//	                      ss += aa[i].value;
-//	                  }
-//	              }
-//	              $("#txt").val(ss);
-//	              alert(22);
-//	          });
-//	      };
 
 	     $("#su").on('click',function() {
 	         //alert(11);
@@ -343,40 +319,7 @@
 	         $("#hidden1").val(str);
 	         $("#normalpromotion-form").submit();
 	     });
-// 	   $('#category_container').on('change','.category_selecter',function(){
-// 	   		var id = $(this).val();
-// 	   		var $parent = $(this).parent();
-//                         var sid ='0000000000';
-//                         var len=$('.category_selecter').eq(1).length;
-//                         if(len > 0)
-//                         {
-//                             sid=$('.category_selecter').eq(1).val();
-//                             //alert(sid);
-//                         }
-                       
-// 	   		$(this).nextAll().remove();
-// 	   		$.ajax({
-	   			//url:'<php echo $this->createUrl('product/getChildren',array('companyId'=>$this->companyId));?>/pid/'+id,
-// 	   			type:'GET',
-// 	   			dataType:'json',
-// 	   			success:function(result){
-// 	   				if(result.data.length){
-// 	   					var str = '<select class="form-control category_selecter" tabindex="-1" name="category_id_selecter">'+
-	   					//'<option value="">--'+"<php echo yii::t('app','请选择');?>"+'--</option>';
-// 	   					$.each(result.data,function(index,value){
-// 	   						str = str + '<option value="'+value.id+'">'+value.name+'</option>';
-// 	   					});
-// 	   					str = str + '</select>';
-// 	   					$parent.append(str);
-// 	   					$('#Product_category_id').val('');
-// 	   					$parent.find('span').remove();
-// 	   				}else{
-//                                                 //if(selname == 'category_id_selecter2')
-//                                                     $('#Product_category_id').val(sid);                                                
-// 	   				}
-// 	   			}
-// 	   		});
-// 	   });
+
 	
 		function swfupload_callback(name,path,oldname)  {
 			//alert(6789);
