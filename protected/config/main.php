6244e41dc46080e33b3c1c8e6ac3bc3e726ce3a6
dys <daiyongsheng.good@163.com>
2018-01-22 14:26:29 +0800

    dys

--- protected/config/main.php
+++ protected/config/main.php
@@ -95,6 +95,7 @@ return array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+				'ymall/<controller:\w+>/<action:\w+>'=>'ymall/<controller>/<action>',
 			),
 		),
 		/*
