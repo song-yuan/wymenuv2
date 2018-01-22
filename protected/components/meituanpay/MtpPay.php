666b4c3e89b919cb27c6ef8d6477e0a5d35baace
caofang <962460422@qq.com>
2018-01-22 11:32:19 +0800

    cf

--- protected/components/meituanpay/MtpPay.php
+++ protected/components/meituanpay/MtpPay.php
@@ -365,7 +365,7 @@ class MtpPay{
     	);
 
     	$body = json_encode($datas);
-    	var_dump($body);exit;
+    	Helper::writeLog('公众号支付传输参数：'.$body);
     	$result = MtpCurl::httpPost($url, $body);
     	Helper::writeLog('公众号支付返回结果：'.$result);
     	return $result;
