9a1dbb5542dd4cf972cb001110926678e9c1a45f
caofang <962460422@qq.com>
2018-02-07 14:50:00 +0800

    cf

--- protected/controllers/MtpayController.php
+++ protected/controllers/MtpayController.php
@@ -21,104 +21,58 @@ class MtpayController extends Controller
 
 	public function actionMtwappayresult(){
 		$data=file_get_contents("php://input");
-		
-		Helper::writeLog('美团result'.$data);
-		$datas = $_POST['status'];
-		//$statu = $datas['status'];
-		//$outTradeNo = $datas['outTradeNo'];
-		$payStatus = Yii::app()->request->getParam('payStatus');
-		$status = Yii::app()->request->getParam('status');
-		$accountNo = Yii::app()->request->getParam('accountNo');
-		Helper::writeLog('美团返回参数ps:'.$payStatus);
-		Helper::writeLog('美团返回参数s:'.$status);
-		Helper::writeLog('美团返回参数an:'.$accountNo);
-		Helper::writeLog('美团返回参数st:'.$datas);
-		//Helper::writeLog('美团返回参数on:'.$outTradeNo);
-		$xml = $GLOBALS['HTTP_RAW_POST_DATA'];
-		Helper::writeLog('美团在线支付返回参数:'.$xml);
-
+		//Helper::writeLog('美团result'.$data);
+		$datas = json_decode($data, TRUE);
+		$accountno = $datas['outTradeNo'];
+		$transactionId = $datas['transactionId'];
+		$totalFee = $datas['totalFee'];
 		//订单号解析orderID和dpid
 		$account_nos = explode('-',$accountno);
 		$orderid = $account_nos[0];
 		$orderdpid = $account_nos[1];
 		//Helper::writeLog('进入方法'.$sn.';店铺:'.$companyId);
 	
-		$sql = 'select * from nb_order where dpid ='.$orderdpid.' and lid ='.$orderid;
-		$orders = Yii::app()->db->createCommand($sql)->queryRow();
-		if(!empty($orders)){
-			if($orders['order_type'] == '1' || $orders['order_type'] == '6' || $orders['order_type'] == '3' ){
-				$pay_type = '12';
-			}elseif($orders['order_type'] == '2'){
-				$pay_type = '13';
-			}else{
-				$pay_type = '1';
-			}
-			$sql = 'select * from nb_order_pay where dpid ='.$orderdpid.' and order_id ='.$orderid.' and account_no ="'.$orders['account_no'].'" and paytype ='.$pay_type;
-			$ordpays = Yii::app()->db->createCommand($sql)
-			->queryRow();
-			if(!empty($ordpays)){
-					
-			}else{
-				$se = new Sequence ( "order_pay" );
-				$orderpayId = $se->nextval();
-				$orderpayData = array (
-						'lid' => $orderpayId,
-						'dpid' => $orderdpid,
-						'create_at' => $orders['create_at'],
-						'update_at' => $orders['update_at'],
-						'order_id' => $orderid,
-						'account_no' => $orders['account_no'],
-						'pay_amount' => number_format($total_amount/100,2),
-						'paytype' => $pay_type,
-						'remark' => $accountno,
-				);
-				$result = Yii::app ()->db->createCommand ()->insert ( 'nb_order_pay', $orderpayData );
-			}
-				
-		}else{
-			Helper::writeLog('未查询到该条订单：'.$orderid);
-		}
-
-		$sql = 'select * from nb_mtpay_info where dpid ='.$orderdpid.' and account_no="'.$accountno.'"';
+		$sql = 'select * from nb_mtpay_info where dpid ='.$orderdpid.' and account_no="'.$accountno.'" and transactionId ="'.$transactionId.'"';
 		//Helper::writeLog('进入方法'.$sql);
 		$notify = Yii::app()->db->createCommand($sql)
 		->queryRow();
-		
+
+		$ords = false;$nots = false;
 		if(!empty($notify)){
 			
 		}else{
 			$results = MtpPay::query(array(
-				'outTradeNo'=>$accountno
+					'outTradeNo'=>$accountno
 			));
 			$return_code = $results['return_code'];
 			$result_code = $results['result_code'];
 			$result_msg = $results['result_msg'];
-			
+				
 			if($result_msg == 'ORDER_SUCCESS'){
 				//像微信公众号支付记录表插入记录...
 				$se = new Sequence("mtpay_info");
 				$notifyWxwapId = $se->nextval();
-				//Helper::writeLog('第一次1:['.$sn.'],插入ID：'.$notifyWxwapId);
 				$notifyWxwapData = array (
 						'lid' => $notifyWxwapId,
 						'dpid' => $orderdpid,
 						'create_at' => date ( 'Y-m-d H:i:s', time()),
 						'update_at' => date ( 'Y-m-d H:i:s', time()),
 						'accountno' => $accountno,
+						'transactionId' => $transactionId,
+						'content' => $data,
 						'pay_status' => $result_msg
 				);
 				//$data = json_encode($notifyWxwapData);
-				//Helper::writeLog('第一次2:['.$sn.'],插入数据：');
 				$result = Yii::app ()->db->createCommand ()->insert('nb_mtpay_info',$notifyWxwapData);
 				if($result){
 					//订单成功支付...
 					Helper::writeLog('支付成功!orderid:['.$orderid.'],dpid:['.$orderdpid.']');
-					//exit;
 					$sql = 'select * from nb_order where dpid ='.$orderdpid.' and lid ='.$orderid;
 					$orders = Yii::app()->db->createCommand($sql)->queryRow();
 					if(!empty($orders)){
 						$user = WxBrandUser::getFromUserId($orders['user_id']);
 						WxOrder::dealOrder($user, $orders);
+						$nots = true;
 					}
 				}
 			}else{
@@ -141,6 +95,8 @@ class MtpayController extends Controller
 								'create_at' => date ( 'Y-m-d H:i:s', time()),
 								'update_at' => date ( 'Y-m-d H:i:s', time()),
 								'accountno' => $accountno,
+								'transactionId' => $transactionId,
+								'content' => $data,
 								'pay_status' => $result_msg
 						);
 						//$data = json_encode($notifyWxwapData);
@@ -155,6 +111,7 @@ class MtpayController extends Controller
 							if(!empty($orders)){
 								$user = WxBrandUser::getFromUserId($orders['user_id']);
 								WxOrder::dealOrder($user, $orders);
+								$nots = true;
 							}
 						}
 						$j=false;
@@ -171,17 +128,64 @@ class MtpayController extends Controller
 							'create_at' => date ( 'Y-m-d H:i:s', time()),
 							'update_at' => date ( 'Y-m-d H:i:s', time()),
 							'accountno' => $accountno,
+							'transactionId' => $transactionId,
+							'content' => $data,
 							'pay_status' => $result_msg
 					);
 					//$data = json_encode($notifyWxwapData);
 					$result = Yii::app ()->db->createCommand ()->insert('nb_mtpay_info',$notifyWxwapData);
 					if($result){
+						$nots = true;
 						//订单成功支付...
 						Helper::writeLog('支付失败!orderid:['.$orderid.'],dpid:['.$orderdpid.']');
 					}
 				}
 			}
 		}
+		
+		$sql = 'select * from nb_order where dpid ='.$orderdpid.' and lid ='.$orderid;
+		$orders = Yii::app()->db->createCommand($sql)->queryRow();
+		if(!empty($orders)){
+			if($orders['order_type'] == '1' || $orders['order_type'] == '6' || $orders['order_type'] == '3' ){
+				$pay_type = '12';
+			}elseif($orders['order_type'] == '2'){
+				$pay_type = '13';
+			}else{
+				$pay_type = '1';
+			}
+			$sql = 'select * from nb_order_pay where dpid ='.$orderdpid.' and order_id ='.$orderid.' and account_no ="'.$orders['account_no'].'" and paytype ='.$pay_type;
+			$ordpays = Yii::app()->db->createCommand($sql)
+			->queryRow();
+			if(!empty($ordpays)){
+				
+			}else{
+				$se = new Sequence ( "order_pay" );
+				$orderpayId = $se->nextval();
+				$orderpayData = array (
+						'lid' => $orderpayId,
+						'dpid' => $orderdpid,
+						'create_at' => $orders['create_at'],
+						'update_at' => $orders['update_at'],
+						'order_id' => $orderid,
+						'account_no' => $orders['account_no'],
+						'pay_amount' => number_format($totalFee/100,2),
+						'paytype' => $pay_type,
+						'remark' => $accountno,
+				);
+				$result = Yii::app ()->db->createCommand ()->insert ( 'nb_order_pay', $orderpayData );
+				if($result){
+					$ords = true;
+				}
+			}
+				
+		}else{
+			Helper::writeLog('未查询到该条订单：'.$orderid);
+		}
+
+		if($nots&&$ords){
+			return $nots = '{"status":"SUCCESS"}';
+		}
+
 	}
 	public function actionMtopenidresult(){
 		$db = Yii::app()->db;
