be6bf85b6b1d7e282d53587be6173b6b13d0c4d0
dys <daiyongsheng.good@163.com>
2018-02-27 15:04:25 +0800

    dys

--- protected/controllers/CrontabController.php
+++ protected/controllers/CrontabController.php
@@ -28,11 +28,7 @@ class  CrontabController extends Controller
 			$sql = 'Select t.lid,t.dpid,t.create_at,t.order_type,t.should_total,t1.paytype,t2.is_retreat from nb_order t left join nb_order_pay t1 on t.dpid=t1.dpid and t.lid=t1.order_id left join nb_order_product t2 on t.dpid=t2.dpid and t.lid=t2.order_id where t.dpid='.$platform['dpid'].' and t.create_at >= "'.$yesterDateBegain.'" and t.create_at <= "'.$yesterDateEnd.'" and t.order_status in (3,4,8) and t1.paytype!=11 group by lid,dpid';
 			$orders = Yii::app()->db->createCommand($sql)->queryAll();
 			foreach ($orders as $order){
-				if($order['order_type']==0){
-					$sourcetype = 'POS机';
-				}else{
-					$sourcetype = '网络配餐';
-				}
+				$sourcetype = 'POS机';
 				if($order['paytype']==0){
 					$payment = '现金';
 				}else{
