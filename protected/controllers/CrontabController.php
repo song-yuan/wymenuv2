333c0f348ca0abf1056eade1bb1ed270d0497fbf
dys <daiyongsheng.good@163.com>
2018-03-28 10:41:50 +0800

    dkys

--- protected/controllers/CrontabController.php
+++ protected/controllers/CrontabController.php
@@ -58,6 +58,7 @@ class  CrontabController extends Controller
 	public function actionGetelemeToken(){
 		$time = strtotime('+1 day');
 		$sql = 'select * from nb_eleme_token where expires_in < '.$time.' and delete_flag=0';
+		echo $sql;exit;
 		$elemeTokens = Yii::app()->db->createCommand($sql)->queryAll();
 		foreach ($elemeTokens as $token){
 			$dpid = $token['dpid'];
