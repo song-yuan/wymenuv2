ab51ded769fe9f13c4ce7c902d6664c002d62617
dys <daiyongsheng.good@163.com>
2018-04-02 17:47:47 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -2,7 +2,7 @@
 	$key = 'redis_key';
 	$cache = Yii::app()->redis->get($key);
 	if($cache===false){
-		$cache = Yii::app()->redis->set($key);
+		$cache = Yii::app()->redis->set($key,'6666');
 	}
 	$cache = Yii::app()->redis->get($key);
 	var_dump($cache);
