4ad2c2b256659fa97bce959ac06cc3b2ef5d85a8
dys <daiyongsheng.good@163.com>
2018-04-02 18:00:45 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -1,13 +1,7 @@
 <?php 
-	$key = 'redis_key';
-	$cache = Yii::app()->redis->delete($key);
-	var_dump($cache);
 	$key = 'redis_key_arr';
-	Yii::app()->redis->lPush($key,'6666');
-	Yii::app()->redis->lPush($key,'7777');
-	Yii::app()->redis->lPush($key,'8888');
-	$cache = Yii::app()->redis->get($key);
-	var_dump($cache);
+	$size = Yii::app()->redis->lSize($key);
+	var_dump($size);
 ?>
 
 
