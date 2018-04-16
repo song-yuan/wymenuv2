0af53cc32502ae02031d0e0b8fa0b7e2ed7652cf
dys <daiyongsheng.good@163.com>
2018-04-16 14:07:00 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -1,6 +1,10 @@
 <?php 
 	$key = 'redis_key_arr';
 	$size = Yii::app()->redis->lSize($key);
+	for ($i=0;$i<$size;$i++){
+		Yii::app()->redis->rPop($key,'aaa');
+	}
+	$size = Yii::app()->redis->lSize($key);
 	var_dump($size);
 	$res = Yii::app()->redis->lPush($key,'aaa');
 	var_dump($res);
