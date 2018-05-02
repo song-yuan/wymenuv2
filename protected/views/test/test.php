ade1efd50c4420855e20705b0f9159c413f68548
dys <daiyongsheng.good@163.com>
2018-05-02 11:44:21 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -1,7 +1,5 @@
 <?php 
-$data = '{"requestId":"200014323281254890","type":12,"appId":11949142,"message":"{\"orderId\":\"3022682819183740026\",\"state\":\"valid\",\"shopId\":156804037,\"updateTime\":1525231066,\"role\":3}","shopId":156804037,"timestamp":1525231066767,"signature":"3E83E0524C5C624C2A04DD4A4D53B58E","userId":51321174359737349}';
-$remt = Elm::dealElmData($data);
-var_dump($remt);
+
 ?>
 
 
