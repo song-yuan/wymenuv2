ae601878cfd55b19f78ac4108fe0a9b781debe57
dys <daiyongsheng.good@163.com>
2018-05-02 10:24:42 +0800

    dys

--- protected/components/mall/WxRedis.php
+++ protected/components/mall/WxRedis.php
@@ -20,5 +20,47 @@ class WxRedis
 		$result = Yii::app()->redis->lPush($key,$data);
 		return $result;
 	}
-	
+	/**
+	 *
+	 * 饿了么 美团 还有收款机订单保存
+	 * redis 数据
+	 * type  2 同步云端    3新增会员卡 4 退款失败 5 日结
+	 *
+	 */
+	public static function dealRedisData($dpid){
+		$key = 'order_online_total_operation_'.(int)$dpid;
+		$orderKey = 'redis-order-data-'.(int)$dpid;
+		$orderSize = Yii::app()->redis->lSize($orderKey);
+		if($orderSize > 0){
+			Yii::app()->redis->set($key,true);
+			$orderData = Yii::app()->redis->rPop($orderKey);
+			$orderDataArr = json_decode($orderData,true);
+			$type = $orderDataArr['type'];
+			if($type==2){
+				$result = self::operateOrder($orderDataArr);
+			}elseif($type==3){
+				$result = self::addMemberCard($orderDataArr);
+			}elseif($type==4){
+				$result = self::retreatOrder($orderDataArr);
+			}elseif($type==5){
+				$content = $orderDataArr['data'];
+				$contentArr = explode('::', $content);
+				$rjDpid = $contentArr[0];
+				$rjUserId = $contentArr[1];
+				$rjCreateAt = $contentArr[2];
+				$rjPoscode = $contentArr[3];
+				$rjBtime = $contentArr[4];
+				$rjEtime = $contentArr[5];
+				$rjcode = $contentArr[6];
+				$result = WxRiJie::setRijieCode($rjDpid,$rjCreateAt,$rjPoscode,$rjBtime,$rjEtime,$rjcode);
+			}
+			$resObj = json_decode($result);
+			if(!$resObj->status){
+				Yii::app()->redis->lPush('redis-order-data-'.(int)$dpid,$orderData);
+			}
+			self::dealRedisData($dpid);
+		}else{
+			Yii::app()->redis->set($key,false);
+		}
+	}
 }
