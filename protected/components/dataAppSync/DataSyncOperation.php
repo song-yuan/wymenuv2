5fa41733007ececbee04c3861c73d999738e9d42
dys <daiyongsheng.good@163.com>
2018-05-28 17:10:37 +0800

    dys

--- protected/components/dataAppSync/DataSyncOperation.php
+++ protected/components/dataAppSync/DataSyncOperation.php
@@ -2092,6 +2092,7 @@ class DataSyncOperation {
 	 *
 	 */
 	public static function dealWxHykPay($data) {
+		Helper::writeLog(json_decode($data));
 		$dpid = $data['dpid'];
 		$cardId = $data['card_id'];
 		$cupons = isset($data['cupon'])?$data['cupon']:array();
