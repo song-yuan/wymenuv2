01250946822dcca03af78cea70844c15a51f22df
dys <daiyongsheng.good@163.com>
2018-06-04 15:42:33 +0800

    dys

--- protected/modules/admin/controllers/MuchprinterProdController.php
+++ protected/modules/admin/controllers/MuchprinterProdController.php
@@ -36,6 +36,7 @@ class MuchprinterProdController extends BackendController
 		
 		$sql = 'select t.dpid,t.company_name from nb_company t where t.delete_flag = 0 and t.comp_dpid = '.$this->companyId;
 		$dpids = $db->createCommand($sql)->queryAll();
+		$printerWays = PrinterWay::getPrinterWay($this->companyId);
 		$categories = $this->getCategories();
 		
 		$this->render('index',array(
