829f021699f30e59b8bf1e4d75f72877bd267c46
dys <daiyongsheng.good@163.com>
2018-06-07 11:07:46 +0800

    dys

--- protected/components/mall/WxSentMessage.php
+++ protected/components/mall/WxSentMessage.php
@@ -12,59 +12,45 @@ class WxSentMessage
 	 * 
 	 * 发送手机短信
 	 * 
-	 */
-	public static function sentMessage($mobile,$content = ''){
-		$userid = '';
+	 **/
+	public static function sentMessage($dpid,$mobile,$type,$userId,$content = ''){
 		$account = 'jksc344';
 		$password = 'wymenu6688';
-		$url = 'http://sh2.ipyy.com/smsJson.aspx?action=send&userid='.$userid.'&account='.$account.'&password='.$password.'&mobile='.trim($mobile).'&content='.$content.'&sendTime=&extno=';
+		$url = 'http://sh2.ipyy.com/smsJson.aspx?action=send&userid='.$userId.'&account='.$account.'&password='.$password.'&mobile='.trim($mobile).'&content='.$content.'&sendTime=&extno=';
 		$result = Curl::httpsRequest($url);
-		return $result;
+		var_dump($result);
+		$resObj = json_decode($result);
+		if($resObj->returnstatus=='Success'){
+			// 发送成功
+			self::insert($dpid, $mobile, $code, $type, $userId, $content);
+			$res = true;
+		}else{
+			$res = false;
+		}
+		return $res;
 	}
 	/**
 	 * 
 	 * 记录短信
 	 * 
-	 */
-	public static function insert($dpid,$mobile,$code,$type,$user_id){      
+	 **/
+	public static function insert($dpid,$mobile,$code,$type,$user_id,$content){      
 		$time = time();
 		$se = new Sequence("mobile_message");
-                $lid = $se->nextval();
+        $lid = $se->nextval();
 		$insertArr = array(
 			        	'lid'=>$lid,
 			        	'dpid'=>$dpid,
 			        	'create_at'=>date('Y-m-d H:i:s',$time),
 			        	'update_at'=>date('Y-m-d H:i:s',$time),
-                                        'user_id' => $user_id,
-                                        'type'=>$type,
+                        'user_id' => $user_id,
+                        'type'=>$type,
 			        	'mobile'=>$mobile,
 			        	'code'=>$code,
-			        	'is_sync'=>DataSync::getInitSync(),
+						'status'=>1,
+						'comment'=>$content,
 			        );
 		$result = Yii::app()->db->createCommand()->insert('nb_mobile_message', $insertArr);
 		return array('status'=>$result,'lid'=>$lid);
-	}
-	/**
-	 * 
-	 * 查找短信
-	 * 
-	 */
-        public static function update($lid,$status){  
-            $sql = 'UPDATE nb_mobile_message set status = '.$status .' WHERE  lid = ' .$lid  ;
-            $result = Yii::app()->db->createCommand($sql)->execute(); 
-        }      
-	/**
-	 * 
-	 * 查找短信
-	 * 
-	 */
-	public static function getCode($dpid,$mobile){
-		$sql = 'select * from nb_mobile_message where dpid=:dpid and mobile=:mobile order by lid desc';
-		$result = Yii::app()->db->createCommand($sql)
-				  ->bindValue(':mobile',$mobile)
-				  ->bindValue(':dpid',$dpid)
-                                 
-				  ->queryRow();
-	    return $result;
 	}
 }
