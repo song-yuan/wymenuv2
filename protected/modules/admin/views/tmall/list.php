9d84c9a5078fe9c1110301a3e3a75370d11554ac
dys <daiyongsheng.good@163.com>
2018-06-26 12:07:29 +0800

    dys

--- protected/modules/admin/views/tmall/list.php
+++ protected/modules/admin/views/tmall/list.php
@@ -10,38 +10,8 @@
             <div class="caption"><i class="fa fa-cogs"></i><?php echo yii::t('app','仓库设置');?></div>
         </div>
         <div class="portlet-body clearfix">
-            <div class="panel_body row">
-                <p>供应商 && 仓库管理员信息设置</p>
-
-                <div class="list col-sm-3 col-xs-12">
-                    <a href="<?php echo $this->createUrl('SupplierClass/index',array('companyId'=>$this->companyId));?>">
-                        <div class="list_big "><i class="fa fa-list-ol"></i> 供应商分类</div>
-                        <div class="list_small">将不同的供应商根据需要类型建立以个分类</div>
-                    </a>
-                </div>
-                <div class="list col-sm-3 col-xs-12">
-                    <a href="<?php echo $this->createUrl('Supplier/index',array('companyId'=>$this->companyId));?>">
-                        <div class="list_big "><i class="fa fa-list-ol"></i> 供应商信息</div>
-                        <div class="list_small">添加新的供应商以及填写相应的供应商信息</div>
-                    </a>
-                </div>
-                <div class="list col-sm-3 col-xs-12">
-                    <a href="<?php echo $this->createUrl('Storekeeper/index',array('companyId'=>$this->companyId));?>">
-                        <div class="list_big "><i class="fa fa-list-ol"></i> 仓库管理员</div>
-                        <div class="list_small">添加、编辑、删除仓库管理员以及填写相应的管理员信息</div>
-                    </a>
-                </div>
-            </div>
-
-            <div class="panel_body row">
-                <p>商城设置</p>
-
-                <div class="list col-sm-3 col-xs-12">
-                    <a href="<?php echo $this->createUrl('goods/index',array('companyId'=>$this->companyId));?>">
-                        <div class="list_big "><i class="fa fa-list-ol"></i> 商品录入</div>
-                        <div class="list_small">录入仓库的商品，操作商品的上下架</div>
-                    </a>
-                </div>
+        	<div class="panel_body row">
+                <p>仓库订单管理</p>
                 <div class="list col-sm-3 col-xs-12">
                     <a href="<?php echo $this->createUrl('goodstock/goodsdelivery',array('companyId'=>$this->companyId));?>">
                         <div class="list_big "><i class="fa fa-list-ol"></i> 配货单</div>
@@ -55,16 +25,48 @@
                     </a>
                 </div>
                 <div class="list col-sm-3 col-xs-12">
-                    <a href="<?php echo $this->createUrl('goodsinvoice/goodsinvoice',array('companyId'=>$this->companyId));?>">
+                    <a href="<?php echo $this->createUrl('takeawayMember/index',array('companyId'=>$this->companyId));?>">
                         <div class="list_big "><i class="fa fa-list-ol"></i> 添加配送员</div>
                         <div class="list_small">添加指定的配送员进行送货服务</div>
                     </a>
                 </div>
             </div>
+            <div class="panel_body row">
+                <p>仓库管理员设置</p>
+
+                <div class="list col-sm-3 col-xs-12">
+                    <a href="<?php echo $this->createUrl('Storekeeper/index',array('companyId'=>$this->companyId));?>">
+                        <div class="list_big "><i class="fa fa-list-ol"></i> 仓库管理员</div>
+                        <div class="list_small">添加、编辑、删除仓库管理员以及填写相应的管理员信息</div>
+                    </a>
+                </div>
+            </div>
 
             <div class="panel_body row">
-                <p>采购与入库</p>
+                <p>商品设置</p>
 
+                <div class="list col-sm-3 col-xs-12">
+                    <a href="<?php echo $this->createUrl('goods/index',array('companyId'=>$this->companyId));?>">
+                        <div class="list_big "><i class="fa fa-list-ol"></i> 商品录入</div>
+                        <div class="list_small">录入仓库的商品，操作商品的上下架</div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="panel_body row">
+                <p>仓库商品采购与入库</p>
+				<div class="list col-sm-3 col-xs-12">
+                    <a href="<?php echo $this->createUrl('SupplierClass/index',array('companyId'=>$this->companyId));?>">
+                        <div class="list_big "><i class="fa fa-list-ol"></i> 供应商分类</div>
+                        <div class="list_small">将不同的供应商根据需要类型建立以个分类</div>
+                    </a>
+                </div>
+                <div class="list col-sm-3 col-xs-12">
+                    <a href="<?php echo $this->createUrl('Supplier/index',array('companyId'=>$this->companyId));?>">
+                        <div class="list_big "><i class="fa fa-list-ol"></i> 供应商信息</div>
+                        <div class="list_small">添加新的供应商以及填写相应的供应商信息</div>
+                    </a>
+                </div>
                 <div class="list col-sm-3 col-xs-12">
                     <a href="<?php echo $this->createUrl('purchaseOrder/ckindex',array('companyId'=>$this->companyId));?>">
                         <div class="list_big "><i class="fa fa-list-ol"></i> 采购订单</div>
