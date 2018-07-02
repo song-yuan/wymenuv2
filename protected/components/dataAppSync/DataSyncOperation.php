8c724ec2b99f162403c43711856674c7d0d9fe5f
dys <daiyongsheng.good@163.com>
2018-07-02 16:42:40 +0800

    dys

--- protected/components/dataAppSync/DataSyncOperation.php
+++ protected/components/dataAppSync/DataSyncOperation.php
@@ -1543,6 +1543,7 @@ class DataSyncOperation {
 					'msg' => '不存在该管理员'
 			) );
 		}
+		Helper::writeLog('1');
 		$dpids = WxCompany::getDpids($dpid);
 		$sql = 'select * from nb_member_card where dpid in (' . $dpids . ') and rfid="' . $rfid . '" and delete_flag=0';
 		$result = Yii::app ()->db->createCommand ( $sql )->queryRow ();
@@ -1552,19 +1553,21 @@ class DataSyncOperation {
 					'msg' => '不存在该会员信息' 
 			) );
 		}
-		
+		Helper::writeLog('1');
 		if ($payPrice > $result ['all_money']) {
 			return json_encode ( array (
 					'status' => false,
 					'msg' => '余额不足' 
 			) );
 		}
+		Helper::writeLog('1');
 		if($result['haspassword'] && md5($password)!=$result['password_hash']){
 			return json_encode ( array (
 					'status' => false,
 					'msg' => '会员卡密码错误'
 			) );
 		}
+		Helper::writeLog('1');
 		$dpid = $result['dpid'];
 		
 		$transaction = Yii::app()->db->beginTransaction();
