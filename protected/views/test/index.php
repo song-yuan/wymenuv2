4be19eed3ab7519c381afb566e564aa91bacd265
dys <daiyongsheng.good@163.com>
2018-07-25 12:02:46 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,7 +1,17 @@
 <?php
-$dpid = 28;
-$keyOrder = 'redis-third-platform-'.(int)$dpid;
-$orderStr = Yii::app()->redis->rPop($keyOrder);
-var_dump(empty($orderStr));
+	$orderSize = Yii::app()->redis->lLen('redis-order-data-0');
+	var_dump($orderSize);
+	echo "<br/>";
+// 	$ckey = 'order_online_total_operation_0';
+// 	$cSize = Yii::app()->redis->set($ckey,0);
+// 	var_dump($cSize);
+// 	echo '<br>';
+	$sql = 'select dpid from nb_company where type=1 and delete_flag = 0';
+	$dpids = Yii::app()->db->createCommand($sql)->queryColumn();
+	foreach ($dpids as $dpid){
+		$orderSize = Yii::app()->redis->lLen('redis-third-platform-'.(int)$dpid);
+		var_dump($dpid.'-'.$orderSize);
+		echo "<br/>";
+	}
 ?>
 
