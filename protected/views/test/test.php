457464d5641b04d1e5d0cd79653f1912b3ef5928
dys <daiyongsheng.good@163.com>
2018-07-31 17:37:59 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -1,8 +1,8 @@
 <?php
 	$key = 'olist';
-	Yii::app()->redis->lPush($key,'abc');
-	$str = Yii::app()->redis->rPop($key);
-	var_dump($str);
+// 	Yii::app()->redis->lPush($key,'abc');
+// 	$str = Yii::app()->redis->rPop($key);
+// 	var_dump($str);
 	$len = Yii::app()->redis->lLen($key);
 	var_dump($len);
 ?>
