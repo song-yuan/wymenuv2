76ffd47a50ef2f6e00600dbbcb999869caf57800
dys <daiyongsheng.good@163.com>
2018-08-01 16:10:22 +0800

    dys

--- protected/modules/admin/controllers/StockTakingController.php
+++ protected/modules/admin/controllers/StockTakingController.php
@@ -35,7 +35,7 @@ class StockTakingController extends BackendController
 				'left join nb_material_unit ms on(t.sales_unit_id = ms.lid and t.dpid = ms.dpid and ms.delete_flag =0) '.
 				'left join (select sum(stock) as stock_all,material_id from nb_product_material_stock where dpid='.$this->companyId.' and delete_flag=0 group by material_id) ms on(t.lid = ms.material_id)'.
 				'left join (select lid,material_id,inventory_stock,inventory_sales,ratio from nb_inventory_detail where inventory_id in(select max(lid) from nb_inventory where dpid ='.$this->companyId.' and type =2 and status =0) group by material_id) inv on(inv.material_id = t.lid)'.
-				'where t.lid in(select tt.lid from nb_product_material tt where tt.delete_flag = 0 and tt.dpid ='.$this->companyId.' and tt.category_id '.$cate.') order by t.category_id asc,t.lid asc';
+				'where t.lid in(select tt.lid from nb_product_material tt where tt.delete_flag = 0 and tt.dpid ='.$this->companyId.' and tt.category_id '.$cate.') order by t.material_identifier asc,t.category_id asc,t.lid asc';
 		$models = $db->createCommand($sql)->queryAll();
 		$categories = $this->getCategories();
 		$this->render('index',array(
