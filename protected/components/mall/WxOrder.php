ec1c3569467d285d5abdf73bfc8a6ece39e8b1a5
dys <daiyongsheng.good@163.com>
2018-08-29 11:00:25 +0800

    dys

--- protected/components/mall/WxOrder.php
+++ protected/components/mall/WxOrder.php
@@ -218,7 +218,7 @@ class WxOrder
 			foreach($this->tastes as $taste){
 				$tasteArr = explode('-',$taste);
 				if(count($tasteArr)>1){
-					$productId = (int)$tasteArr[0];
+					$productId = (int)$tasteArr[0].'';
 					$this->productTastes[$productId][] = $tasteArr;
 				}
 			}
