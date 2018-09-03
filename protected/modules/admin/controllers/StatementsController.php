c4159fee2d2173584262877c7c4454d550e8b200
dys <daiyongsheng.good@163.com>
2018-09-03 16:38:55 +0800

    dys

--- protected/modules/admin/controllers/StatementsController.php
+++ protected/modules/admin/controllers/StatementsController.php
@@ -1529,11 +1529,11 @@ class StatementsController extends BackendController
 		}
 
 		if($text==1){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc) k';
 		}elseif($text==2){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc) k';
 		}elseif($text==3){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc,day(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid ='.$selectDpid.' and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc,day(create_at) asc) k';
 		}
 			//统计实付价格，客流、单数
 
@@ -5097,11 +5097,11 @@ class StatementsController extends BackendController
 
 		$db = Yii::app()->db;
 		if($text==1){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc) k';
 		}elseif($text==2){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc) k';
 		}elseif($text==3){
-			$sql = 'select k.* from(select year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc,day(create_at) asc) k';
+			$sql = 'select k.* from(select dpid,year(create_at) as y_all,month(create_at) as m_all,day(create_at) as d_all, sum(number) as all_number,count(account_no) as all_account,sum(should_total) as all_realprice,sum(reality_total) as all_originalprice from nb_order where dpid in('.$selectDpid.') and lid in('.$ords.') group by year(create_at) asc,month(create_at) asc,day(create_at) asc) k';
 		}
 			//统计实付价格，客流、单数
 		$models = Yii::app()->db->createCommand($sql)->queryAll();
