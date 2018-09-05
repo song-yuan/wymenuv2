d2ac1341f2111a59f0dc7131d385aebb998ed9ec
dys <daiyongsheng.good@163.com>
2018-09-05 14:28:55 +0800

    dys

--- protected/modules/admin/controllers/StockTakingController.php
+++ protected/modules/admin/controllers/StockTakingController.php
@@ -31,11 +31,11 @@ class StockTakingController extends BackendController
 		
 		$sql = 'select ms.stock_all,mu.unit_name,mu.lid as mu_lid,ms.lid as ms_lid,ms.unit_name as sales_name,k.category_name,inv.inventory_stock,inv.inventory_sales,inv.ratio,inv.lid as invtid,t.* from nb_product_material t '.
 				'left join nb_material_category k on(t.category_id = k.lid and t.dpid = k.dpid)'.
-				'left join nb_material_unit mu on(t.stock_unit_id = mu.lid and t.dpid = mu.dpid and mu.delete_flag =0) '.
-				'left join nb_material_unit ms on(t.sales_unit_id = ms.lid and t.dpid = ms.dpid and ms.delete_flag =0) '.
+				'left join nb_material_unit mu on(t.stock_unit_id = mu.lid and t.dpid = mu.dpid) '.
+				'left join nb_material_unit ms on(t.sales_unit_id = ms.lid and t.dpid = ms.dpid) '.
 				'left join (select sum(stock) as stock_all,material_id from nb_product_material_stock where dpid='.$this->companyId.' and delete_flag=0 group by material_id) ms on(t.lid = ms.material_id)'.
 				'left join (select lid,material_id,inventory_stock,inventory_sales,ratio from nb_inventory_detail where inventory_id in(select max(lid) from nb_inventory where dpid ='.$this->companyId.' and type =2 and status =0) group by material_id) inv on(inv.material_id = t.lid)'.
-				'where t.lid in(select tt.lid from nb_product_material tt where tt.delete_flag = 0 and tt.dpid ='.$this->companyId.' and tt.category_id '.$cate.') order by t.material_identifier asc,t.category_id asc,t.lid asc';
+				'where t.lid in(select tt.lid from nb_product_material tt where tt.delete_flag = 0 and tt.dpid ='.$this->companyId.' and tt.category_id '.$cate.') and mu.delete_flag =0 and ms.delete_flag =0 order by t.material_identifier asc,t.category_id asc,t.lid asc';
 		$models = $db->createCommand($sql)->queryAll();
 		$categories = $this->getCategories();
 		$this->render('index',array(
@@ -561,12 +561,6 @@ class StockTakingController extends BackendController
 		return $models;
 	}
 	public function getRatio($mulid,$mslid){
-		if(empty($mulid)){
-			$mulid = 0;
-		}
-		if(empty($mslid)){
-			$mslid = 0;
-		}
 		$sql = 'select unit_ratio from nb_material_unit_ratio where stock_unit_id='.$mulid.' and sales_unit_id='.$mslid;
 		$models = Yii::app()->db->createCommand($sql)->queryRow();
 		if(!empty($models)){
