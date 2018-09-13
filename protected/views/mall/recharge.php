26952c6e87fa0dba0e6e62c843af14980e9940aa
dys <daiyongsheng.good@163.com>
2018-09-13 15:07:27 +0800

    dys

--- protected/views/mall/recharge.php
+++ protected/views/mall/recharge.php
@@ -33,6 +33,7 @@
 						'getBrandWCPayRequest',
 						parameters,
 						function(res){
+							alert(res.err_msg);
 							 if(res.err_msg == "get_brand_wcpay_request:ok" ) {
 							 	// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
 							 	layer.msg('支付成功!');
