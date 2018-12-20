59cfdf7db77f8a1bc333b3485f5b58573ef52aca
dys <daiyongsheng.good@163.com>
2018-12-20 17:47:28 +0800

    dys

--- protected/components/mall/WxProduct.php
+++ protected/components/mall/WxProduct.php
@@ -40,15 +40,23 @@ class WxProduct
 			}
 			
 			if($category['cate_type']!=2){
-				$sql = 'select * from nb_product where status=0 and is_show=1 and is_show_wx=1 and delete_flag=0 and dpid=:dpid and category_id in ('.$categoryIds.') order by sort asc,lid desc';
+				$sql = 'select * from nb_product where status=0 and is_show=1 and delete_flag=0 and dpid=:dpid and category_id in ('.$categoryIds.') order by sort asc,lid desc';
 				$categoryProducts = Yii::app()->db->createCommand($sql)->bindValue(':dpid',$this->dpid)->queryAll();
 				foreach ($categoryProducts as $key=>$product){
+					if(($this->type=='6'&&$product['is_show_wx']=='4')||($this->type=='2'&&$product['is_show_wx']=='3')||$product['is_show_wx']=='2'){
+						unset($categoryProducts[$key]);
+						continue;
+					}
 					$categoryProducts[$key]['taste_groups'] = WxTaste::getProductTastes($product['lid'],$product['dpid']);
 				}
 			}else{
-				$sql = 'select * from nb_product_set where status=0 and is_show=1 and is_show_wx=1 and delete_flag=0 and dpid=:dpid and category_id in ('.$categoryIds.') order by sort asc,lid desc';
+				$sql = 'select * from nb_product_set where status=0 and is_show=1 and is_show_wx!=2 and delete_flag=0 and dpid=:dpid and category_id in ('.$categoryIds.') order by sort asc,lid desc';
 				$categoryProducts = Yii::app()->db->createCommand($sql)->bindValue(':dpid',$this->dpid)->queryAll();
 				foreach ($categoryProducts as $sk=>$set){
+					if(($this->type=='6'&&$product['is_show_wx']=='4')||($this->type=='2'&&$product['is_show_wx']=='3')||$product['is_show_wx']=='2'){
+						unset($categoryProducts[$key]);
+						continue;
+					}
 					$setDetail = self::getProductSetDetail($set['lid'], $set['dpid']);
 					if(!empty($setDetail)){
 						$categoryProducts[$sk]['detail'] = $setDetail;
@@ -81,6 +89,9 @@ class WxProduct
 	 	$productSet = Yii::app()->db->createCommand($sql)->bindValue(':dpid',$dpid)->bindValue(':lid',$productSetId)->queryRow();
 	 	return $productSet;
 	 }
+	 /**
+	  * 获取套餐详情
+	  */
 	 public static function getProductSetDetail($productSetId,$dpid){
 	 	$setDetails = array(); // 按分组号
  		$sql = 'select t.set_id,t.product_id,t.price,t.group_no,t.number,t.is_select,t1.* from nb_product_set_detail t, nb_product t1 where t.product_id=t1.lid and t.dpid=t1.dpid and t.set_id=:setId and t.dpid=:dpid and t.delete_flag=0 and t1.delete_flag=0';
