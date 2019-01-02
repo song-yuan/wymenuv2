6b15a4d257d97ef9a7f0584f9bb40cd1b965410f
dys <daiyongsheng.good@163.com>
2019-01-02 14:14:54 +0800

    dys

--- protected/modules/admin/views/posfee/_form.php
+++ protected/modules/admin/views/posfee/_form.php
@@ -23,11 +23,11 @@
 			</div>
 		</div>
 		<div class="form-actions fluid">
-		<span style="color: red;">&nbsp;提示：请慎重填写，加盟商会按照设置的金额进行续费延期。</span>
 			<div class="col-md-offset-3 col-md-9">
 				<button type="submit" class="btn blue"><?php echo yii::t('app','确定');?></button>
 			</div>
 		</div>
+		<div style="color: red;">&nbsp;提示：请慎重填写，加盟商会按照设置的金额进行续费延期。</div>
 	<?php endif;?>
 <?php $this->endWidget(); ?>
 
