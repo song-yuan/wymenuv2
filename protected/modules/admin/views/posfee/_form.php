cca530f0ba2377b17290ae5d9f5c323c94772606
dys <daiyongsheng.good@163.com>
2019-01-02 13:23:33 +0800

    dys

--- protected/modules/admin/views/posfee/_form.php
+++ protected/modules/admin/views/posfee/_form.php
@@ -11,7 +11,7 @@
 	  <div class="form-group">
 			<?php echo $form->label($model, 'years',array('class' => 'col-md-3 control-label'));?>
 			<div class="col-md-4">
-				<?php echo $form->textField($model, 'years',array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('years'),'disabled'=>$a,));?>
+				<?php echo $form->textField($model, 'years',array('class' => 'form-control','placeholder'=>$model->getAttributeLabel('years')));?>
 				<?php echo $form->error($model, 'years' )?>
 			</div>
 		</div>
@@ -25,7 +25,7 @@
 		<div class="form-actions fluid">
 		<span style="color: red;">&nbsp;提示：请慎重填写，加盟商会按照设置的金额进行续费延期。</span>
 			<div class="col-md-offset-3 col-md-9">
-				<button type="submit" class="btn blue" <?php if($a)echo 'disabled';?>><?php echo yii::t('app','确定');?></button>
+				<button type="submit" class="btn blue"><?php echo yii::t('app','确定');?></button>
 			</div>
 		</div>
 	<?php endif;?>
