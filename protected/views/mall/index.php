2529209b4a877c7f9e02a05b2e2652ad702d5ac3
dys <daiyongsheng.good@163.com>
2019-01-07 16:31:33 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -774,8 +774,8 @@
 		}
 	}
 ?>
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css?_=201901051708">
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=201901051708">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css?_=201901071708">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=201901071708">
 <style type="text/css">
 .layui-layer-setwin .layui-layer-close2 {
 	right: -12px;
