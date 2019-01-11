3429b29a76ed4fdfb5d171ec2e517c903fe4f90d
dys <daiyongsheng.good@163.com>
2019-01-11 17:16:17 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -986,6 +986,7 @@ $(document).ready(function(){
 	var topHeight = $('.top-des').height();
 	var footHeight = $('footer').height();
 	var cHeight = $('body').height()-topHeight-headHeight-footHeight;
+	alert(cHeight);
 	$(".content").css('padding-top',(topHeight+headHeight));
 	$('#nav').css('height',cHeight);
 	$('#container').css('height',cHeight);
