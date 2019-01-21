ee82abeecec8b187806debf8f5f55bef7bc613ae
dys <daiyongsheng.good@163.com>
2019-01-21 14:38:07 +0800

    dys

--- protected/views/mall/index.php
+++ protected/views/mall/index.php
@@ -51,7 +51,7 @@
 		
 ?>
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css?_=201901111705">
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=201901211432">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/index.css?_=201901211437">
 <style type="text/css">
 .layui-layer-setwin .layui-layer-close2 {
 	right: -12px;
