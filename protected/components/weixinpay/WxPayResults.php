7dc324a68958be6cdb7f498ea4f503fb67aa6c5e
dys <daiyongsheng.good@163.com>
2019-03-11 18:08:10 +0800

    dys

--- protected/components/weixinpay/WxPayResults.php
+++ protected/components/weixinpay/WxPayResults.php
@@ -16,7 +16,6 @@ class WxPayResults extends WxPayDataBase
 		if(!$this->IsSignSet()){
 			return true;
 		}
-		var_dump($this->values);exit;
 		$sign = $this->MakeSign();
 		if($this->GetSign() == $sign){
 			return true;
