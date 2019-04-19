ca8132caaf1f22f748b556ac40f437eafe216d34
dys <daiyongsheng.good@163.com>
2019-04-19 11:41:26 +0800

    dys

--- protected/models/InstructionDetail.php
+++ protected/models/InstructionDetail.php
@@ -41,11 +41,11 @@ class InstructionDetail extends CActiveRecord
 			array('instruct', 'length', 'max'=>64),
 			array('sort', 'length', 'max'=>3),
 			array('is_sync', 'length', 'max'=>50),
-			array('delete_flag', 'length', 'max'=>2),
+			array('is_waiting, delete_flag', 'length', 'max'=>2),
 			array('create_at', 'safe'),
 			// The following rule is used by search().
 			// @todo Please remove those attributes that should not be searched.
-			array('lid, dpid, create_at, update_at, instruction_id, number, instruct_name, time, instruct, sort, delete_flag, is_sync', 'safe', 'on'=>'search'),
+			array('lid, dpid, create_at, update_at, instruction_id, number, instruct_name, time, instruct, sort, is_waiting, delete_flag, is_sync', 'safe', 'on'=>'search'),
 		);
 	}
 
@@ -77,6 +77,7 @@ class InstructionDetail extends CActiveRecord
 			'time' => '执行时间',
 			'instruct' => '指令',
 			'sort' => '排序',
+			'is_waiting' => '是否等待',
 			'delete_flag' => 'Delete Flag',
 			'is_sync' => 'Is Sync',
 		);
@@ -110,6 +111,7 @@ class InstructionDetail extends CActiveRecord
 		$criteria->compare('time',$this->time,true);
 		$criteria->compare('instruct',$this->instruct,true);
 		$criteria->compare('sort',$this->sort,true);
+		$criteria->compare('is_waiting',$this->delete_flag,true);
 		$criteria->compare('delete_flag',$this->delete_flag,true);
 		$criteria->compare('is_sync',$this->is_sync,true);
 
