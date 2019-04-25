821f5dc9fb486394562395776bd02f0a6826cc35
dys <daiyongsheng.good@163.com>
2019-04-25 12:01:23 +0800

    dys

--- protected/views/test/test.php
+++ protected/views/test/test.php
@@ -11,4 +11,7 @@ $order = json_decode($message);
 $dpid = 0;
 $res = Elm::dealOrder($order,$dpid,4);
 var_dump($res);
+$keyOrder = 'redis-third-platform-'.(int)$dpid;
+$orderSize = Yii::app()->redis->lLen($keyOrder);
+var_dump($orderSize);
 ?>
