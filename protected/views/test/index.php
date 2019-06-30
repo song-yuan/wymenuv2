47c6b1ce947f642fe7a64f720fdf4e3fe24aea45
dys <daiyongsheng.good@163.com>
2019-06-30 13:26:33 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,8 +1,10 @@
 <?php 
 $appid = 4073;
 $apppoicode = 't_9uCwcElNGj';
-$orderId = '';
+$orderId = '26959143364425973';
 $reason = '售完';
 $reasonCode = 1001;
 $res = MtOpenOrder::cancel($appid, $apppoicode, $orderId, $reason, $reasonCode);
+var_dump($res);
+exit;
 ?>
