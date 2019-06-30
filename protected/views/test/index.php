420cb6339b44f98b68289c43243d3af7e32ea4b9
dys <daiyongsheng.good@163.com>
2019-06-30 13:31:03 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,10 +1,22 @@
 <?php 
-$appid = 4073;
-$apppoicode = 't_9uCwcElNGj';
-$orderId = '26959143364425973';
-$reason = '售完';
-$reasonCode = 1001;
-$res = MtOpenOrder::cancel($appid, $apppoicode, $orderId, $reason, $reasonCode);
-var_dump($res);
+function cancel(){
+	$appid = 4073;
+	$apppoicode = 't_9uCwcElNGj';
+	$orderId = '26959143364425973';
+	$reason = '售完';
+	$reasonCode = 1001;
+	$res = MtOpenOrder::cancel($appid, $apppoicode, $orderId, $reason, $reasonCode);
+	var_dump($res);
+}
+function agree(){
+	$appid = 4073;
+	$apppoicode = 't_9uCwcElNGj';
+	$orderId = '26959143173839162';
+	$reason = '售完';
+	$reasonCode = 1001;
+	$res = MtOpenOrder::agree($appid, $apppoicode, $orderId, $reason);
+	var_dump($res);
+}
+agree();
 exit;
 ?>
