932924994585464a4c4ad63428f2bc11b9d0716a
dys <daiyongsheng.good@163.com>
2019-08-01 16:15:57 +0800

    dys

--- protected/views/test/index.php
+++ protected/views/test/index.php
@@ -1,26 +1,29 @@
 <?php 
-function cancel(){
+function confirm($orderId){
 	$appid = 4190;
 	$apppoicode = 't_ADV1kPfiDx';
-	$orderId = '26964220373755333';
+	$res = MtOpenOrder::confirm($appid, $apppoicode, $orderId);
+	var_dump($res);
+}
+function cancel($orderId){
+	$appid = 4190;
+	$apppoicode = 't_ADV1kPfiDx';
 	$reason = '售完';
 	$reasonCode = 1001;
 	$res = MtOpenOrder::cancel($appid, $apppoicode, $orderId, $reason, $reasonCode);
 	var_dump($res);
 }
-function agree(){
+function agree($orderId){
 	$appid = 4190;
 	$apppoicode = 't_ADV1kPfiDx';
-	$orderId = '26959143173839162';
 	$reason = '售完';
 	$reasonCode = 1001;
 	$res = MtOpenOrder::agree($appid, $apppoicode, $orderId, $reason);
 	var_dump($res);
 }
-function reject(){
+function reject($orderId){
 	$appid = 4190;
 	$apppoicode = 't_ADV1kPfiDx';
-	$orderId = '26959140812816829';
 	$reason = '售完';
 	$reasonCode = 1001;
 	$res = MtOpenOrder::reject($appid, $apppoicode, $orderId, $reason);
@@ -32,6 +35,18 @@ function pullPhoneNumber(){
 	$res = MtOpenOrder::pullPhoneNumber($appid, $apppoicode);
 	var_dump($res);
 }
-cancel();
+$t = $_GET['t'];
+$orderId = $_GET['id'];
+if($t=='confirm'){
+	confirm($orderId);
+}elseif($t=='cancel'){
+	cancel($orderId);
+}elseif($t=='agree'){
+	agree($orderId);
+}elseif($t=='reject'){
+	reject($orderId);
+}elseif($t=='pull'){
+	pullPhoneNumber();
+}
 exit;
 ?>
