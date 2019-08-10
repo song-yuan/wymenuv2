51ced8c117a56fe832e8708830d1f45634ac41f0
dys <daiyongsheng.good@163.com>
2019-08-10 11:37:01 +0800

    dys

--- protected/modules/admin/views/instruct/tasteInstruct.php
+++ protected/modules/admin/views/instruct/tasteInstruct.php
@@ -103,6 +103,7 @@
 							<td class="center">
 								<?php if(!empty($model->productInstruct)):?>
 								<a href="<?php echo $this->createUrl('instruct/tasteInstructList',array('lid' => (int)$model->lid , 'companyId' => $model->dpid));?>"><?php echo yii::t('app','编辑');?></a>
+								<a href="<?php echo $this->createUrl('instruct/updateTasteInstruct',array('lid' => (int)$model->lid , 'companyId' => $model->dpid));?>"><?php echo yii::t('app','添加');?></a>
 								<?php else:?>
 								<a href="<?php echo $this->createUrl('instruct/updateTasteInstruct',array('lid' => (int)$model->lid , 'companyId' => $model->dpid));?>"><?php echo yii::t('app','添加');?></a>
 								<?php endif;?>
