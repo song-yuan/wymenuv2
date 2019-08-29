a50662eb08ef85e53f7582adf250bfc228394cb6
dys <daiyongsheng.good@163.com>
2019-08-29 10:24:42 +0800

    dys

--- protected/views/test/hh.php
+++ protected/views/test/hh.php
@@ -1,5 +1,7 @@
 <?php 
 	$companyId = '0000000027';
+	$key = array('productList-'.$companyId.'-2','productList-'.$companyId.'-6');
+	Yii::app()->redis->delete($key);
 	$key = 'productList-'.$companyId.'-*';
 	$keys = Yii::app()->redis->keys($key);
 	var_dump($keys);
