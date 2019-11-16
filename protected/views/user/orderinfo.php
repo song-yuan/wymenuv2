97022ed652ffd36fa3be95b399f0c4a2ae713eab
dys <daiyongsheng.good@163.com>
2019-11-16 12:46:55 +0800

    dys

--- protected/views/user/orderinfo.php
+++ protected/views/user/orderinfo.php
@@ -13,7 +13,7 @@
 <?php if($order['order_type']==1):?>
 <div class="order-title">桌号: <?php if($siteType){echo $siteType['name'];}?><?php echo $site['serial'];?></div>
 <?php else:?>
-<div class="order-title">取餐号: <?php echo substr($order['lid'], -4);?></div>
+<div class="order-title">取餐号: <?php echo $order['callno'];?></div>
 <?php endif;?>
 <div class="order-site">
 	<div class="lt">订单状态</div>
