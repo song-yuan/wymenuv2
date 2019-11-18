5cad00e80d745b154f021b080be5fae6120ce358
dys <daiyongsheng.good@163.com>
2019-11-18 14:04:22 +0800

    dys

--- protected/views/user/orderinfo.php
+++ protected/views/user/orderinfo.php
@@ -7,7 +7,7 @@
 
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/reset.css">
 <link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/user.css">
-<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/order.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/order.css?_t=20191118">
 
 
 <?php if($order['order_type']==1):?>
