92fede1a748100e6f5dd1dd72fa3fe4afc642d9d
dys <daiyongsheng.good@163.com>
2019-11-20 15:10:40 +0800

    dys

---
+++ protected/components/mall/WxZizhuOrder.php
@@ -0,0 +1,244 @@
+<?php 
+/**
+ * 
+ * 
+ * 微信端餐桌订单类
+ * 餐桌多个订单合并到最新订单中进行支付
+ *
+ * 
+ */
+class WxZizhuOrder
+{
+	public $dpid;
+	public $orderId; // nb_site_no表的lid
+	public $seatingFee = 0;
+	public $levelDiscount = 1;//会员等级折扣
+	public $user;
+	public $hasfullsent = false;
+	public $fullsent = '0-0-0';//满送满减信息
+	public $fullMinus = 0; //满减金额
+	public $fullSentProduct = array(); //满送产品
+	public $others = array();//其他参数
+	public $order = false;
+	public $orderProduct = array();
+	public $orderSuccess = false;
+	
+	public function __construct($dpid, $orderId, $user, $others){
+		$this->dpid = $dpid;
+		$this->siteId = $siteId;
+		$this->user = $user;
+		$this->others = $others;
+		$this->getSeatingFee();
+		$this->getLevelDiscount();
+		$this->getCupon();
+		$this->getFullsent();
+		$this->getOrder();
+		$this->getOrderProduct();
+	}
+	//获取餐位费
+	public function getSeatingFee(){
+		$isSeatingFee = WxCompanyFee::get(1,$this->dpid);
+		if($isSeatingFee){
+			$this->seatingFee = $isSeatingFee['fee_price'];
+		}else{
+			$this->seatingFee = 0;
+		}
+	}
+	/**
+	 *获取会员等级折扣
+	 */
+	public function getLevelDiscount(){
+		$this->levelDiscount = WxBrandUser::getUserDiscount($this->user,$this->type);
+	}
+	/**
+	 *获取优惠券信息
+	 */
+	public function getCupon(){
+		$cupoinId = $this->others['cuponId'];
+		if($cupoinId){
+			$now = date('Y-m-d H:i:s',time());
+			$cbArr = explode('-', $cupoinId);
+			$cbLid = $cbArr[0];
+			$cbDpid = $cbArr[1];
+			$sql = 'select t.lid,t.dpid,t.cupon_id,t1.cupon_money,t1.min_consumer from nb_cupon_branduser t,nb_cupon t1 where t.cupon_id=t1.lid and t.dpid=t1.dpid and  t.lid='.$cbLid.
+			' and t.dpid='.$cbDpid.' and t.valid_day <= "'.$now.'" and "'.$now.'" <= t.close_day and t1.delete_flag=0 and t1.is_available=0';
+			$this->cupon = Yii::app()->db->createCommand($sql)->queryRow();
+		}
+	}
+	/**
+	 * 处理满减满送活动
+	 */
+	public function getFullsent(){
+		if($this->others['fullsent']!='0-0-0'){
+			$now = date('Y-m-d H:i:s',time());
+			$this->hasfullsent = true;
+			$fullsentArr = explode('-', $this->fullsent);
+			$fullType = $fullsentArr[0];
+			$fullsentId = $fullsentArr[1];
+			$fullsentdetailId = $fullsentArr[2];
+			$fullsentObj = WxFullSent::checkFullsent($fullsentId,$this->dpid);
+			if(!$fullsentObj){
+				throw new Exception('满减满送活动不存在');
+			}
+			$this->fullsent = $fullsentObj;
+			if($now < $fullsentObj['begin_time']){
+				throw new Exception('满减满送活动未开始');
+			}
+			if($now > $fullsentObj['end_time']){
+				throw new Exception('满减满送活动已结束');
+			}
+			if($fullType==0){
+				$fullsentdetail = WxFullSent::checkFullsentproduct($fullsentdetailId,$fullsentId,$this->dpid);
+				if(!$fullsentdetail){
+					throw new Exception('无改满送产品');
+				}
+				$this->fullSentProduct = $fullsentdetail;
+			}else{
+				$this->fullMinus = $fullsentObj['extra_cost'];
+			}
+			
+		}
+		
+	}
+	//获取该餐桌所有订单
+	public function getOrder(){
+		$this->order = WxOrder::getOrder($this->orderId, $this->dpid);
+	}
+	public function getOrderProduct(){
+		$this->orderProduct = WxOrder::getOrderProduct($this->orderId, $this->dpid);
+	}
+	public function createOrder(){
+		$memdisprice = 0;
+		$orderPrice = 0;
+		$realityPrice = 0;
+		$number = 0;
+		$time = time();
+		$levelDiscount = $this->levelDiscount;
+		$accountNo = $this->order['account_no'];
+		$number = $this->order['number'];
+	
+		$orderProducts = $this->orderProduct;
+		foreach ($orderProducts as $product){
+			if($product['set_id'] > 0){
+				$amount = $product['zhiamount'];
+			}else{
+				$amount = $product['amount'];
+			}
+			$isdiscount = $product['is_member_discount'];
+			if($product['private_promotion_lid'] > 0){
+				$isdiscount = 0;
+			}
+			if($isdiscount){
+				$memdisprice += $amount*$product['price']*(1-$levelDiscount);
+				$orderPrice +=  $amount*$product['price']*$levelDiscount;
+			}else{
+				$orderPrice +=  $amount*$product['price'];
+			}
+			$realityPrice += $amount*$product['original_price'];
+		}
+		if(!empty($this->fullSentProduct)){
+			$se = new Sequence("order_product");
+			$orderProductId = $se->nextval();
+		
+			$orderProductData = array(
+					'lid'=>$orderProductId,
+					'dpid'=>$this->dpid,
+					'create_at'=>date('Y-m-d H:i:s',$time),
+					'update_at'=>date('Y-m-d H:i:s',$time),
+					'order_id'=>$this->orderId,
+					'set_id'=>0,
+					'product_id'=>$this->fullSentProduct['product_id'],
+					'product_name'=>$this->fullSentProduct['product_name'],
+					'product_pic'=>$this->fullSentProduct['main_picture'],
+					'price'=>$this->fullSentProduct['price'],
+					'original_price'=>$this->fullSentProduct['original_price'],
+					'amount'=>1,
+					'product_order_status'=>2,
+			);
+			Yii::app()->db->createCommand()->insert('nb_order_product',$orderProductData);
+		}
+		if($this->seatingFee > 0){
+			$se = new Sequence("order_product");
+			$orderProductId = $se->nextval();
+			$orderProductData = array(
+					'lid'=>$orderProductId,
+					'dpid'=>$this->dpid,
+					'create_at'=>date('Y-m-d H:i:s',$time),
+					'update_at'=>date('Y-m-d H:i:s',$time),
+					'order_id'=>$this->orderId,
+					'set_id'=>0,
+					'product_id'=>0,
+					'product_name'=>'餐位费',
+					'product_pic'=>'',
+					'product_type'=>1,
+					'price'=>$this->seatingFee,
+					'original_price'=>$this->seatingFee,
+					'amount'=>$this->number,
+					'product_order_status'=>9,
+			);
+			Yii::app()->db->createCommand()->insert('nb_order_product',$orderProductData);
+			$orderPrice +=  $this->seatingFee*$number;
+			$realityPrice += $this->seatingFee*$number;
+		}
+		if($memdisprice > 0){
+			$se = new Sequence("order_account_discount");
+			$orderAccountId = $se->nextval();
+			$orderAccountData = array(
+					'lid'=>$orderAccountId,
+					'dpid'=>$this->dpid,
+					'create_at'=>date('Y-m-d H:i:s',$time),
+					'update_at'=>date('Y-m-d H:i:s',$time),
+					'order_id'=>$this->orderId,
+					'account_no'=>$accountNo,
+					'discount_title'=>'会员折扣',
+					'discount_id'=>0,
+					'discount_money'=>$memdisprice,
+			);
+			Yii::app()->db->createCommand()->insert('nb_order_account_discount',$orderAccountData);
+		}
+		if($this->fullMinus > 0){
+			$se = new Sequence("order_account_discount");
+			$orderAccountId = $se->nextval();
+			$orderAccountData = array(
+					'lid'=>$orderAccountId,
+					'dpid'=>$this->dpid,
+					'create_at'=>date('Y-m-d H:i:s',$time),
+					'update_at'=>date('Y-m-d H:i:s',$time),
+					'order_id'=>$this->orderId,
+					'account_no'=>$accountNo,
+					'discount_title'=>$this->fullsent['title'],
+					'discount_id'=>0,
+					'discount_money'=>$this->fullMinus,
+			);
+			Yii::app()->db->createCommand()->insert('nb_order_account_discount',$orderAccountData);
+			$orderPrice = $orderPrice - $this->fullMinus;
+			if($orderPrice < 0){
+				$orderPrice = 0;
+			}
+		}
+		$orderArr['should_total'] = $orderPrice;
+		$payPrice = $orderPrice;
+		
+		// 现金券
+		if($this->cupon && $payPrice>0){
+			$order = $orderArr;
+			$payMoney = self::updateOrderCupon($this->cupon, $order, $payPrice, $this->user['card_id']);
+			$payPrice -= $payMoney;
+		}
+		// 使用储值
+		if($this->others['yue'] && $payPrice>0){
+			$remainMoney = WxBrandUser::getYue($this->user);
+			if($remainMoney > 0){
+				$order = $orderArr;
+				$payMoney = self::reduceYue($this->user,$order,$payPrice);
+				$payPrice -= $payMoney;
+			}
+		}
+		$sql = 'update nb_order set should_total='.$orderPrice.',reality_total='.$realityPrice.',user_id='.$this->user['lid'].' where lid='.$this->orderId.' and dpid='.$this->dpid;
+		Yii::app()->db->createCommand($sql)->execute();
+		if($payPrice <= 0){
+			$this->orderSuccess = true;
+		}
+		return $this->orderId;
+	}
+}
---
+++ protected/views/mall/zizhuorder.php
@@ -0,0 +1,444 @@
+<?php
+	$baseUrl = Yii::app()->baseUrl;
+	$this->setPageTitle('确认订单');
+	$orderTatsePrice = 0.00;
+	$fval = '0-0-0';// 满类型-满lid-送lid
+	$isCupon = false;
+	if(!empty($cupons)){
+		$isCupon = true;
+	}
+?>
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/weui.min.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/style.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/order.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $baseUrl;?>/css/mall/cart.css">
+
+<form action="<?php echo $this->createUrl('/mall/generalZizhuOrder',array('companyId'=>$this->companyId,'orderId'=>$orderId,'type'=>$this->type));?>" method="post">
+
+<div class="order-info">
+	<?php foreach($orderProducts as $product):?>
+	<div class="item">
+		<div class="lt"><?php echo $product['product_name'];?></div>
+		<div class="rt">x<?php echo $product['amount'];?> ￥<?php echo number_format($product['price'],2);?></div>
+		<div class="clear"></div>
+	</div>
+		<?php if(isset($product['taste'])&&!empty($product['taste'])):?>
+		<div class="taste">口味:
+		<?php foreach ($product['taste'] as $taste):?>
+		<span> <?php echo $taste['name'].'('.$taste['price'].')';?> </span>
+		<?php endforeach;?>
+		</div>
+		<?php endif;?>
+		
+		<?php if(isset($product['detail'])&&!empty($product['detail'])):?>
+		<div class="taste">
+		<?php foreach ($product['detail'] as $detail):?>
+		<span> <?php echo $detail['product_name'];?> </span>
+		<?php endforeach;?>
+		</div>
+		<?php endif;?>
+	<?php endforeach;?>
+</div>
+
+<div class="activity-info">
+	<?php if($memdisprice):?>
+	<div class="order-copun disabled">
+		<div class="copun-lt">会员折扣</div>
+		<div class="copun-rt"><?php echo '-￥'.number_format($memdisprice,2);?></div>
+		<div class="clear"></div>
+	</div>
+	<?php endif;?>
+	<?php if(!empty($fullsent)):?>
+		<?php if($fullsent['full_type']):
+				$fminusprice = $fullsent['extra_cost'];
+				$memdisprice += $fminusprice;
+				$fval = '1-'.$fullsent['lid'].'-0';
+		?>
+		<div class="order-copun disabled">
+			<div class="copun-lt">满减优惠</div>
+			<div class="copun-rt"><?php echo '-￥'.$fminusprice;?></div>
+			<div class="clear"></div>
+		</div>
+		<?php else:?>
+			<div class="order-copun arrowright fullsent">
+				<div class="copun-lt">满送优惠</div>
+				<div class="copun-rt">选择产品</div>
+				<div class="clear"></div>
+			</div>
+		<?php endif;?>
+	<?php endif;?>
+	<!-- 完善资料才能使用代金券  -->
+	<?php if($user['mobile_num']&&$user['user_birthday']):?>
+		<div class="order-copun arrowright cupon <?php if(!$isCupon) echo 'disabled';?>">
+			<div class="copun-lt">代金券</div>
+			<div class="copun-rt"><?php if($isCupon){echo count($cupons).'张可用';}else{echo '暂无可用';}?></div>
+			<div class="clear"></div>
+		</div>
+	<?php else:?>
+		<div class="order-copun arrowright disabled">
+			<div class="copun-lt">代金券</div>
+			<div class="copun-rt"><a href="<?php echo $this->createUrl('/user/setUserInfo',array('companyId'=>$this->companyId,'type'=>$this->type,'back'=>1));?>">去完善资料</a></div>
+			<div class="clear"></div>
+		</div>
+	<?php endif;?>
+</div>
+
+<div class="totalinfo"><span class="font_l" style="margin-right:20px;<?php if(!$memdisprice) echo 'dispaly:none';?>">优惠￥<span class="cart-discount"><?php echo number_format($memdisprice,2);?></span></span><span>实付￥<span class="cart-price"><?php echo $price;?></span></span></div>
+
+
+<div class="order-remark">
+	<textarea name="remark" placeholder="请输入备注内容"></textarea>
+</div>
+<div class="order-paytype">
+	<div class="select-type">选择支付方式</div>
+	<!-- 余额 -->
+	<div class="chooselist points" style="padding:15px;">
+		<div class="left"><img src="<?php echo $baseUrl;?>/img/wechat_img/icon-wdcz.png"/> 储值支付 <span class="small font_org">剩余￥<span id="yue" yue="<?php echo $remainMoney;?>"><?php echo $remainMoney;?></span> 可使用￥<span class="use-yue"><?php echo $remainMoney > $price?$price:$remainMoney;?></span></span></div>
+		<div class="right">
+		<?php if($remainMoney > 0):?>
+		<label><input type="checkbox" name="yue" checked="checked" class="ios-switch green  bigswitch" value="1" /><div><div></div></div></label>
+		<?php else:?>
+		<label><input type="checkbox" name="yue" class="ios-switch green  bigswitch" value="1" /><div><div></div></div></label>
+		<?php endif;?>
+		</div>
+	</div>
+	<!-- 余额 -->
+	<div class="paytype">
+		<div class="item wx on" paytype="2" style="border:none;"><img src="<?php echo $baseUrl;?>/img/mall/wxpay.png"/> 微信支付</div>
+		<!-- 
+		<div class="item zfb" paytype="1" style="border:none;"><img src="<?php echo $baseUrl;?>/img/mall/zfbpay.png"/> 支付宝支付</div>
+		-->
+		<input type="hidden" name="paytype" value="2" />
+	</div>
+</div>
+<div class="bottom"></div>
+
+<footer>
+    <div class="ft-lt">
+        <p>实付￥<span id="total" class="total" total="<?php echo $price;?>"><?php echo $price;?></span></p>
+    </div>
+    <div class="ft-rt">
+        <p><a id="payorder" href="javascript:;">去支付</a></p>
+    </div>
+    <div class="clear"></div>
+</footer>
+
+<div class="user-cupon" id="cuponList">
+	<div class="cupon-container">
+	<?php if($isCupon):?>
+	<?php foreach($cupons as $coupon):?>
+		<div class="item useCupon" user-cupon-id="<?php echo $coupon['lid'].'-'.$coupon['dpid'];?>" min-money="<?php echo $coupon['min_consumer'];?>" cupon-money="<?php echo $coupon['cupon_money'];?>">
+			<div class="item-top">
+				<div class="item-top-left"><?php echo $coupon['cupon_title'];?></div>
+				<div class="item-top-right">￥<?php echo (int)$coupon['cupon_money'];?></div>
+				<div class="clear"></div>
+			</div>
+			<div class="item-bottom">
+				<div class="item-bottom-left">有效期至<?php echo date('Y.m.d',strtotime($coupon['close_day']));?></div>
+				<div class="item-bottom-right">满<?php echo (int)$coupon['min_consumer'];?>元可用</div>
+				<div class="clear"></div>
+			</div>
+		</div>
+	<?php endforeach;?>
+		<div class="item noCupon" cupon-num="<?php echo count($cupons);?>" user-cupon-id="0" min-money="0" cupon-money="0">不使用代金券</div>
+	<?php endif;?>
+	</div>
+</div>
+<div class="user-cupon" id="sentList">
+	<div class="cupon-container">
+		<?php if(!empty($fullsent)&&$fullsent['full_type']==0):?>
+		<?php 
+			foreach ($fullsent['sent_product'] as $sent):
+			$sentProPrice = 0;
+			if($sent['is_discount']){
+				$sentProPrice = number_format($sent['original_price']*$sent['promotion_discount'],2);
+			}else{
+				$sentProPrice = number_format($sent['original_price'] - $sent['promotion_money'],2);
+			}
+		?>
+		<div class="item useSent" sent-type="0" sent-id="<?php echo $fullsent['lid'];?>" sent-product-id="<?php echo $sent['lid'];?>" sent-price="<?php echo $sentProPrice;?>">
+			<div class="item-top">
+				<div class="item-top-left"><?php echo $sent['product_name'];?></div>
+				<div class="item-top-right"><span style="font-size:15px;color:gray;"><strike>￥<?php echo $sent['original_price'];?></strike></span> <span>￥<?php echo $sentProPrice;?></span></div>
+				<div class="clear"></div>
+			</div>
+		</div>
+		<?php endforeach;?>
+		<div class="item noSent" sent-type="0" sent-id="0" sent-product-id="0" sent-price="0.00" >不选择赠送产品</div>
+		<?php endif;?>
+	</div>
+</div>
+	<input type="hidden" name="fullsent" value="<?php echo $fval;?>" />
+	<input type="hidden" name="cupon" value="0" />
+</form>
+
+<div id="dialogs">
+	<!--BEGIN dialog1-->
+	<div class="js_dialog" id="dialog1" style="display: none;">
+	    <div class="weui-mask"></div>
+	    <div class="weui-dialog">
+	         <div class="weui-dialog__hd"><strong class="weui-dialog__title">储值支付提示</strong></div>
+	         <div class="weui-dialog__bd">确定使用储值支付?</div>
+	         <div class="weui-dialog__ft">
+	         	<a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default">取消</a>
+	         	<a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary">确定</a>
+	         </div>
+	     </div>
+	</div>
+	<!--END dialog1-->
+	<!--BEGIN dialog2-->
+	<div class="js_dialog" id="dialog2" style="display: none;">
+	    <div class="weui-mask"></div>
+	    <div class="weui-dialog">
+	         <div class="weui-dialog__hd"><strong class="weui-dialog__title">储值支付提示</strong></div>
+	         <div class="weui-dialog__bd">储值余额不足,请去充值后再下单</div>
+	         <div class="weui-dialog__ft">
+	         	<a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_default">取消</a>
+	         	<a href="javascript:;" class="weui-dialog__btn weui-dialog__btn_primary">去充值</a>
+	         </div>
+	     </div>
+	</div>      
+	<!--END dialog2-->
+</div>
+<script>
+function reset_total(price){
+	var setTotal = $('#total').attr('total');
+	var total = $('#total').html();
+	var totalFee = parseFloat(total) + parseFloat(price);
+	$('#total').attr('total',parseFloat(setTotal) + parseFloat(price));
+	
+	if(totalFee > 0){
+		totalFee =  totalFee.toFixed(2);
+	}else{
+		totalFee = '0.00';
+	}
+	
+	$('#total').html(totalFee);
+}
+$(document).ready(function(){
+	var cupon_layer = 0;
+	var sent_layer = 0;
+	var isMustYue = false;
+	
+	$('.paytype .item').click(function(){
+		var paytype = $(this).attr('paytype');
+		$('.paytype .item').removeClass('on');
+		
+		$('input[name="paytype"]').val(paytype);
+		$(this).addClass('on');
+	});
+	// 点击选择代金券
+	$('.cupon').click(function(){
+	    if($(this).hasClass('disabled')){
+	      layer.msg('无可用代金券');
+	      return;
+	    }
+	    cupon_layer = layer.open({
+	      				    type: 1,
+	      				    title: false,
+	      				    shadeClose: true,
+	      				    closeBtn: 0,
+	      				    area: ['100%','100%'],
+	      				    content:$('#cuponList'),
+	      				});
+	});
+	 // 选择代金券
+	$('#cuponList .item.useCupon').click(function(){
+		var userCuponId = $(this).attr('user-cupon-id');
+		var cuponMoney = $(this).attr('cupon-money');
+		var noCuponMoney = $('.noCupon').attr('cupon-money');
+		var minMoney = $(this).attr('min-money');
+		var cartDiscount = $('.cart-discount').html();
+		var total = $('#total').html();
+		var yue = parseFloat($('#yue').attr('yue'));
+		var money = 0;
+		
+		$('#cuponList .item').removeClass('on');
+		$(this).addClass('on');
+		$('input[name="cupon"]').val(userCuponId);
+		$('.noCupon').attr('min-money',minMoney);
+		$('.noCupon').attr('cupon-money',cuponMoney);
+		
+		money = parseFloat(total) + parseFloat(noCuponMoney) - parseFloat(cuponMoney);
+		if(money > 0){
+			money = money;
+		}else{
+			money = 0;
+			$('.noCupon').attr('cupon-money',total);
+		}
+		if(yue > 0){
+			if(yue > money){
+				$('.use-yue').html(money.toFixed(2));
+			}else{
+				$('.use-yue').html(yue.toFixed(2));
+			}
+		}
+		money = money.toFixed(2);
+		cartDiscount = parseFloat(cartDiscount)-parseFloat(noCuponMoney)+parseFloat(cuponMoney);
+		cartDiscount = cartDiscount.toFixed(2);
+		$('.cart-discount').html(cartDiscount);
+		$('.cart-price').html(money);
+		$('#total').html(money);
+		$('#total').attr('total',money);
+		$('.cupon').find('.copun-rt').html('-￥'+cuponMoney);
+		layer.close(cupon_layer);
+	});
+	// 选择不使用代金券
+	$('#cuponList .item.noCupon').click(function(){
+		var cuponNum = $(this).attr('cupon-num');
+		var userCuponId = $(this).attr('user-cupon-id');
+		var cuponMoney = $(this).attr('cupon-money');
+		var minMoney = $(this).attr('min-money');
+		var cartDiscount = $('.cart-discount').html();
+		var total = $('#total').html();
+		var yue = parseFloat($('#yue').attr('yue'));
+		var money = 0;
+		
+		$('#cuponList .item').removeClass('on');
+		$('input[name="cupon"]').val(userCuponId);
+		
+		$(this).attr('min-money',0);
+		$(this).attr('cupon-money',0);
+		
+		money = parseFloat(total) + parseFloat(cuponMoney);
+		if(money > 0){
+			money = money;
+		}else{
+			money = 0;
+		}
+		if(yue > 0){
+			if(yue > money){
+				$('.use-yue').html(money.toFixed(2));
+			}else{
+				$('.use-yue').html(yue.toFixed(2));
+			}
+		}
+		money = money.toFixed(2);
+		cartDiscount = parseFloat(cartDiscount)-parseFloat(cuponMoney);
+		cartDiscount = cartDiscount.toFixed(2);
+		$('.cart-discount').html(cartDiscount);
+		$('.cart-price').html(money);
+		$('#total').html(money);
+		$('#total').attr('total',money);
+		$('.cupon').find('.copun-rt').html(cuponNum+'张可用');
+		layer.close(cupon_layer);
+	});
+	// 点击选择赠送产品
+	$('.fullsent').click(function(){
+		sent_layer = layer.open({
+		    type: 1,
+		    title: false,
+		    shadeClose: true,
+		    closeBtn: 0,
+		    area: ['100%','100%'],
+		    content:$('#sentList'),
+		});
+	});
+	$('#sentList .item.useSent').click(function(){
+		var sentType = $(this).attr('sent-type');
+		var sentId = $(this).attr('sent-id');
+		var sentProductId = $(this).attr('sent-product-id');
+		var sentPrice = $(this).attr('sent-price');
+		var noSentMoney = $('.noSent').attr('sent-price');
+		var sval = sentType+'-'+sentId+'-'+sentProductId;
+		var pName = $(this).find('.item-top-left').html();
+		var yue = parseFloat($('#yue').attr('yue'));
+		var total = $('#total').html();
+		var money = 0;
+		$('#sentList .item').removeClass('on');
+		$(this).addClass('on');
+		
+		$('.noSent').attr('sent-price',sentPrice);
+		$('input[name="fullsent"]').val(sval);
+		$('.fullsent').find('.copun-rt').html(pName+'￥'+sentPrice);
+
+		money = parseFloat(total) - parseFloat(noSentMoney) + parseFloat(sentPrice);
+
+		if(yue > 0){
+			if(yue > money){
+				$('.use-yue').html(money.toFixed(2));
+			}else{
+				$('.use-yue').html(yue.toFixed(2));
+			}
+		}
+		money = money.toFixed(2);
+		$('#total').html(money);
+		$('#total').attr('total',money);
+		
+		layer.close(sent_layer);
+	});
+	$('#sentList .item.noSent').click(function(){
+		var sval = '0-0-0';
+		var sentPrice = $(this).attr('sent-price');
+		var yue = parseFloat($('#yue').attr('yue'));
+		var total = $('#total').html();
+		var money = 0;
+		$('#sentList .item').removeClass('on');
+		$('input[name="fullsent"]').val(sval);
+		$(this).attr('sent-price','0.00');
+		money = parseFloat(total) - parseFloat(sentPrice);
+		if(yue > 0){
+			if(yue > money){
+				$('.use-yue').html(money.toFixed(2));
+			}else{
+				$('.use-yue').html(yue.toFixed(2));
+			}
+		}
+		money = money.toFixed(2);
+		$('#total').html(money);
+		$('#total').attr('total',money);
+		$('.fullsent').find('.copun-rt').html('选择产品');
+		
+		layer.close(sent_layer);
+	});	
+	
+	$('input[name="yue"]').change(function(){
+		if(isMustYue){
+			layer.msg('有储值支付活动产品<br>需使用储值支付');
+			$(this).prop('checked',true);
+			return;
+		}
+		var yue = $('#yue').attr('yue');
+		if(parseFloat(yue) == 0){
+			layer.msg('储值不足!');
+			$(this).prop('checked',false);
+		}
+	});
+	$('#payorder').click(function(){
+		if($('input[name="yue"]').is(':checked')){
+			$('#dialog1').show();
+			return;
+		}
+		layer.load(2);
+		$('form').submit();
+	});
+	$('#dialog .weui-dialog__btn_primary').click(function(){
+		$('#dialog').hide();
+		layer.load(2);
+		$('form').submit();
+	});
+	$('#dialog .weui-dialog__btn_default').click(function(){
+		$('#dialog').hide();
+	});
+	$('#dialog1 .weui-dialog__btn_primary').click(function(){
+		$('#dialog1').hide();
+		layer.load(2);
+		$('form').submit();
+	});
+	$('#dialog1 .weui-dialog__btn_default').click(function(){
+		if(isMustYue){
+			layer.msg('有储值支付活动产品<br>需使用储值支付');
+			location.href = "<?php echo $this->createUrl('/mall/index',array('companyId'=>$this->companyId,'type'=>$this->type));?>";
+		}else{
+			$('input[name="yue"]').removeAttr('checked');
+			$('#dialog1').hide();
+		}
+	});
+	$('#dialog2 .weui-dialog__btn_primary').click(function(){
+		location.href = "<?php echo $this->createUrl('/mall/reCharge',array('companyId'=>$user['dpid'],'url'=>urlencode($this->createUrl('/mall/checkOrder',array('companyId'=>$this->companyId,'type'=>$this->type)))));?>";
+	});
+	$('#dialog2 .weui-dialog__btn_default').click(function(){
+		location.href = "<?php echo $this->createUrl('/mall/index',array('companyId'=>$this->companyId,'type'=>$this->type));?>";
+	});
+});
+</script>
