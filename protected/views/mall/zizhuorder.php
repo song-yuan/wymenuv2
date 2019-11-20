9d9a416ca6c35cb961d19a645d54ba12429ab12e
dys <daiyongsheng.good@163.com>
2019-11-20 17:19:04 +0800

    dys

--- protected/views/mall/zizhuorder.php
+++ protected/views/mall/zizhuorder.php
@@ -172,7 +172,7 @@
 	<input type="hidden" name="cupon" value="0" />
 </form>
 
-<div id="dialogs">
+<div id="dialogs" tyle="font-size:22px;">
 	<!--BEGIN dialog1-->
 	<div class="js_dialog" id="dialog1" style="display: none;">
 	    <div class="weui-mask"></div>
