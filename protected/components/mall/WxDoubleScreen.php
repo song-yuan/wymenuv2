154bda31fc91dc358ce9b58537d7cf889793c0b1
dys <daiyongsheng.good@163.com>
2019-12-06 18:18:14 +0800

    dys

---
+++ protected/components/mall/WxDoubleScreen.php
@@ -0,0 +1,18 @@
+<?php 
+/**
+ * 
+ * 
+ * 副屏类
+ * 
+ * 
+ */
+class WxDoubleScreen
+{
+	public static function get($dpid){
+		$sql = 'select * from nb_double_screen_detail ds,nb_double_screen d where ds.double_screen_id=d.lid and ds.dpid=d.dpid and ds.dpid='.$dpid.' and d.is_able=1 and d.delete_flag=0';
+		$doubles = Yii::app()->db->createCommand($sql)
+				  ->bindValue(':dpid',$dpid)
+				  ->queryAll();
+	    return $doubles;
+	}
+}
