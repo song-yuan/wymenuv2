13fdb7ac264721fc02b8b71e5c8f89e461b1628a
dys <daiyongsheng.good@163.com>
2019-12-16 13:52:31 +0800

    dys

--- protected/components/mall/WxDoubleScreen.php
+++ protected/components/mall/WxDoubleScreen.php
@@ -9,7 +9,7 @@
 class WxDoubleScreen
 {
 	public static function get($dpid){
-		$sql = 'select * from nb_double_screen_detail ds,nb_double_screen d where ds.double_screen_id=d.lid and ds.dpid=d.dpid and ds.dpid='.$dpid.' and d.is_able=1 and ds.delete_flag=0 and d.delete_flag=0';
+		$sql = 'select * from nb_double_screen_detail ds,nb_double_screen d where ds.double_screen_id=d.lid and ds.dpid=d.dpid and ds.dpid='.$dpid.' and d.is_able=2 and ds.delete_flag=0 and d.delete_flag=0';
 		$doubles = Yii::app()->db->createCommand($sql)
 				  ->bindValue(':dpid',$dpid)
 				  ->queryAll();
