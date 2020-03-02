628b521f03e938f136beb947217f17ace27b8dd3
dys <daiyongsheng.good@163.com>
2020-03-02 15:53:07 +0800

    dys

--- protected/modules/mall/controllers/CuponController.php
+++ protected/modules/mall/controllers/CuponController.php
@@ -4,8 +4,19 @@
  */
 class CuponController extends Controller
 {
-	public function actionIndex()
+	public $companyId;
+	public function init()
 	{
-		
+		$companyId = Yii::app()->request->getParam('companyId');
+		$this->companyId = $companyId;
+	}
+	public function actionGetUserCupon()
+	{
+		$userId = Yii::app()->request->getPost('userId');
+		$proCodeArr = Yii::app()->request->getPost('proCodeArr');
+		$total = Yii::app()->request->getPost('total');
+		$type = 5;
+		$dpid = $this->companyId;
+		$cupon = WxCupon::getUserAvaliableCupon($proCodeArr, $total, $userId, $dpid, $type);
 	}
 }
