0c51133070f6dfe43bde318ebfbcb5db89349065
dys <daiyongsheng.good@163.com>
2020-03-02 17:37:46 +0800

    dys

--- protected/modules/inxcx/controllers/CuponController.php
+++ protected/modules/inxcx/controllers/CuponController.php
@@ -18,12 +18,7 @@ class CuponController extends Controller
 		$type = Yii::app()->request->getParam('type');
 		$dpid = $this->companyId;
 		$proCodeArr = explode(',', $proCode);
-		var_dump($proCodeArr);
-		var_dump($total);
-		var_dump($userId);
-		var_dump($dpid);
 		$cupon = WxCupon::getUserAvaliableCupon($proCodeArr, $total, $userId, $dpid, $type);
-		var_dump($cupon);
 		echo json_encode(array('cupon'=>$cupon));
 	}
 }
